58faedaf683825162895f32bbc63b8f27b0d932e
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-12-23 20:01:49 +0100

    telescope: move plugin into seperate folder

---
+++ plugins/telescope/default.nix
@@ -0,0 +1,51 @@
+{ pkgs, config, lib, ... }:
+with lib;
+let
+  cfg = config.programs.nixvim.plugins.telescope;
+  helpers = (import ../helpers.nix { inherit lib; });
+in
+{
+  # TODO:add support for aditional filetypes. This requires autocommands!
+
+  options.programs.nixvim.plugins.telescope = {
+    enable = mkEnableOption "Enable telescope.nvim";
+
+    highlightTheme = mkOption {
+      type = types.nullOr types.str;
+      description = "The colorscheme to use for syntax highlighting";
+      default = config.programs.nixvim.colorscheme;
+    };
+
+    enabledExtensions = mkOption {
+      type = types.listOf types.str;
+      description = "A list of enabled extensions. Don't use this directly";
+      default = [];
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.nixvim = {
+      extraPackages = [ pkgs.bat ];
+
+      extraPlugins = with pkgs.vimPlugins; [
+        telescope-nvim
+        plenary-nvim
+        popup-nvim
+      ];
+
+      extraConfigVim = mkIf (cfg.highlightTheme != null) ''
+        let $BAT_THEME = '${cfg.highlightTheme}'
+      '';
+
+      extraConfigLua = ''
+        local __telescopeExtensions = ${helpers.toLuaObject cfg.enabledExtensions}
+
+        require('telescope').setup{}
+
+        for i, extension in ipairs(__telescopeExtensions) do
+          require('telescope').load_extension(extension)
+        end
+        '' ;
+    };
+  };
+}
