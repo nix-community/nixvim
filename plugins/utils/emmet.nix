d8df00cab07dbe53505bf9cc33d49351e7c4ac7f
imMaturana <maturana-git.ntf91@aleeas.com>
2022-01-19 06:45:17 -0400

    emmet: remove redundant null type

--- plugins/utils/emmet.nix
+++ plugins/utils/emmet.nix
@@ -14,19 +14,19 @@ mkPlugin attrs {
 
   options = {
     mode = mkDefaultOpt {
-      type = types.nullOr (types.enum [ "i" "n" "v" "a" ]);
+      type = types.enum [ "i" "n" "v" "a" ];
       global = "user_emmet_mode";
       description = "Mode where emmet will enable";
     };
 
     leader = mkDefaultOpt {
-      type = types.nullOr types.str;
+      type = types.str;
       global = "user_emmet_leader_key";
       description = "Set leader key";
     };
 
     settings = mkDefaultOpt {
-      type = with types; nullOr (attrsOf (attrsOf eitherAttrsStrInt));
+      type = types.attrsOf (types.attrsOf eitherAttrsStrInt);
       global = "user_emmet_settings";
       description = "Emmet settings";
     };
