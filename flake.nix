35ca8a507e825136238ea9c6094aec5c5f4f14cb
Pedro Alves <pta2002@pta2002.com>
2022-06-06 01:09:46 +0100

    home-manager and nixos modules

--- flake.nix
+++ flake.nix
@@ -8,6 +8,43 @@
 
   # TODO: Use flake-utils to support all architectures
   outputs = { self, nixpkgs, nmdSrc, flake-utils, ... }@inputs:
+    with nixpkgs.lib;
+    with builtins;
+    let
+      # TODO: Support nesting
+      nixvimModules = map (f: ./modules + "/${f}") (attrNames (builtins.readDir ./modules));
+
+      modules = system: nixvimModules ++ [
+        (rec {
+          _file = ./flake.nix;
+          key = _file;
+          config = {
+            _module.args = {
+              pkgs = mkForce (import nixpkgs { inherit system; });
+              lib = nixpkgs.lib;
+              helpers = import ./plugins/helpers.nix { lib = nixpkgs.lib; };
+            };
+          };
+        })
+      ];
+
+      nixvimOption = mkOption {
+        type = types.attrs;
+        default = { enabled = false; };
+        description = "Nixvim options";
+      };
+
+      filterOptions = filterAttrs (k: _: k != "enabled");
+
+      build = system:
+        configuration:
+        let
+          eval = evalModules {
+            modules = modules system;
+          };
+        in
+        eval.config.output;
+    in
     flake-utils.lib.eachDefaultSystem
       (system: rec {
         packages.${system}.docs = import ./docs {
@@ -15,34 +52,24 @@
           lib = nixpkgs.lib;
         };
 
-        nixosModules.nixvim = import ./nixvim.nix { nixos = true; };
-        homeManagerModules.nixvim = import ./nixvim.nix { homeManager = true; };
-      }) // {
-      build = system:
-        with nixpkgs.lib;
-        with builtins;
-        configuration:
-        let
-          # TODO: Support nesting
-          nixvimModules = map (f: ./modules + "/${f}") (attrNames (builtins.readDir ./modules));
-
-          eval = evalModules {
-            modules = nixvimModules ++ [
-              (rec {
-                _file = ./flake.nix;
-                key = _file;
-                config = {
-                  _module.args = {
-                    pkgs = mkForce (import nixpkgs { inherit system; });
-                    lib = nixpkgs.lib;
-                    helpers = import ./plugins/helpers.nix { lib = nixpkgs.lib; };
-                  };
-                };
-              })
-              configuration
+        nixosModules.nixvim = { pkgs, config, lib, ... }: {
+          options.programs.nixvim = nixvimOption;
+          config = mkIf config.programs.nixvim.enabled {
+            environment.systemPackages = [
+              (build system (filterOptions config.programs.nixvim))
             ];
           };
-        in
-        eval.config.output;
+        };
+
+        homeManagerModules.nixvim = { pkgs, config, lib, ... }: {
+          options.programs.nixvim = nixvimOption;
+          config = mkIf config.programs.nixvim.enabled {
+            home.packages = [
+              (build system (filterOptions config.programs.nixvim))
+            ];
+          };
+        };
+      }) // {
+      inherit build;
     };
 }
