4d573a1e75688d75508e426243a2abaf121f6376
Alexander Nortung <alex_nortung@live.dk>
2022-07-18 21:14:41 +0000

    nvim-tree: git.enable expects a boolean (#24)

--- plugins/utils/nvim-tree.nix
+++ plugins/utils/nvim-tree.nix
@@ -116,7 +116,7 @@ in
 
     git = {
       enable = mkOption {
-        type = types.nullOr types.str;
+        type = types.nullOr types.bool;
         default = null;
         description = "Enable git integration";
       };
