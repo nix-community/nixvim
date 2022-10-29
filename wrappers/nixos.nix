fbc09c490790f6fbaa914bc2fb2b06a6a71ceffe
LightQuantum <self@lightquantum.me>
2022-10-30 02:32:11 +0800

    helpers: expose to config.nixvim.helpers (take two)

--- wrappers/nixos.nix
+++ wrappers/nixos.nix
@@ -2,18 +2,28 @@ modules:
 { pkgs, config, lib, ... }:
 
 let
-  inherit (lib) mkEnableOption mkOption mkMerge mkIf types;
+  inherit (lib) mkEnableOption mkOption mkOptionType mkMerge mkIf types;
   cfg = config.programs.nixvim;
-in {
+in
+{
   options = {
-    programs.nixvim = lib.mkOption {
+    programs.nixvim = mkOption {
       type = types.submodule ((modules pkgs) ++ [{
         options.enable = mkEnableOption "nixvim";
       }]);
     };
+    nixvim.helpers = mkOption {
+      type = mkOptionType {
+        name = "helpers";
+        description = "Helpers that can be used when writing nixvim configs";
+        check = builtins.isAttrs;
+      };
+      description = "Use this option to access the helpers";
+      default = import ../plugins/helpers.nix { inherit (pkgs) lib; };
+    };
   };
 
-  config = mkIf cfg.enable 
+  config = mkIf cfg.enable
     (mkMerge [
       { environment.systemPackages = [ cfg.finalPackage ]; }
       (mkIf (!cfg.wrapRc) {
