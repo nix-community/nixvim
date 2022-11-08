c320be04c82914933fe94f64282e646d97d342d4
Pedro Alves <pta2002@pta2002.com>
2022-11-08 20:02:17 +0000

    remove obsolete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# Creates a container and then runs the resulting neovim executable with the configuration
-set -e
-
-sudo nixos-container destroy nvim-test
-sudo nixos-container create nvim-test --flake .
-
-.tmp/sw/bin/nvim -u .tmp/etc/xdg/nvim/sysinit.vim $@
