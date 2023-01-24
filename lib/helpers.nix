eada2a76dbdf680ede974265a524c2056843f72d
traxys <quentin+dev@familleboyer.net>
2023-01-24 22:46:07 +0100

    helpers: Correctly print bool values as true/false

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -86,7 +86,7 @@ rec {
     );
 
     mkInt = default: mkNullable lib.types.int (toString default);
-    mkBool = default: mkNullable lib.types.bool (toString default);
+    mkBool = default: mkNullable lib.types.bool (if default then "true" else "false");
     mkStr = default: mkNullable lib.types.str ''"${default}"'';
   };
 
