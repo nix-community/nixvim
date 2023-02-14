6dd603a447544c1f10457a14e056c019aaf5d26d
Pedro Alves <pta2002@pta2002.com>
2023-02-14 19:50:23 +0000

    tagbar: fix typo

--- plugins/languages/tagbar.nix
+++ plugins/languages/tagbar.nix
@@ -16,7 +16,7 @@ with lib;
     package = helpers.mkPackageOption "tagbar" pkgs.vimPlugins.tagbar;
 
     extraConfig = helpers.mkNullOrOption types.attrs ''
-      The configuration options for vimtex without the 'tagbar_' prefix.
+      The configuration options for tagbar without the 'tagbar_' prefix.
       Example: To set 'tagbar_show_tag_count' to 1, write
         extraConfig = {
           show_tag_count= true;
@@ -26,10 +26,10 @@ with lib;
 
   config = mkIf cfg.enable {
 
-      extraPlugins = [ cfg.package ];
+    extraPlugins = [ cfg.package ];
 
-      extraPackages = [ pkgs.ctags ];
+    extraPackages = [ pkgs.ctags ];
 
-      globals = mapAttrs' (name: value: nameValuePair ("tagbar_" + name) value) cfg.extraConfig;
-    };
+    globals = mapAttrs' (name: value: nameValuePair ("tagbar_" + name) value) cfg.extraConfig;
+  };
 }
