db29e08aa5052cea0accfba2eef8fc2e29fd7667
Gaetan Lepage <gaetan@glepage.com>
2023-03-11 00:08:27 +0100

    lib/helpers: fix typo in mkModeMaps

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -101,7 +101,7 @@ with lib; rec {
         actionAttrs =
           if isString action
           then {inherit action;}
-          else value;
+          else action;
       in
         defaults // actionAttrs
     )
