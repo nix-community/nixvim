416c719f4759e250082668e758b8dfd9eb16ef3a
traxys <quentin+dev@familleboyer.net>
2023-03-14 23:48:27 +0100

    plugins: Fetch the plugin sources from flake inputs (#247)

--- plugins/plugin-defs.nix
+++ plugins/plugin-defs.nix
@@ -1,65 +1,31 @@
 # This is for plugins not in nixpkgs
 # e.g. intellitab.nvim
-# Ideally, in the future, this would all be specified as a flake input!
-{pkgs, ...}: {
-  intellitab-nvim = pkgs.vimUtils.buildVimPlugin rec {
-    pname = "intellitab-nvim";
-    version = "a6c1a505865f6131866d609c52440306e9914b16";
-    src = pkgs.fetchFromGitHub {
-      owner = "pta2002";
-      repo = "intellitab.nvim";
-      rev = version;
-      sha256 = "19my464jsji7cb81h0agflzb0vmmb3f5kapv0wwhpdddcfzvp4fg";
-    };
+#
+# This is generated through nvfetcher, the plugins are defined in nvfetcher.toml.
+# You can update the plugins by running `nvfetcher` in this directory
+{pkgs, ...}: let
+  sources = pkgs.callPackage ./_sources/generated.nix {};
+in {
+  intellitab-nvim = pkgs.vimUtils.buildVimPlugin {
+    inherit (sources.intellitab-nvim) pname version src;
   };
-  mark-radar = pkgs.vimUtils.buildVimPlugin rec {
-    pname = "mark-radar";
-    version = "d7fb84a670795a5b36b18a5b59afd1d3865cbec7";
-    src = pkgs.fetchFromGitHub {
-      owner = "winston0410";
-      repo = "mark-radar.nvim";
-      rev = version;
-      sha256 = "1y3l2c7h8czhw0b5m25iyjdyy0p4nqk4a3bxv583m72hn4ac8rz9";
-    };
+  mark-radar = pkgs.vimUtils.buildVimPlugin {
+    inherit (sources.mark-radar) pname version src;
   };
-  coq-nvim = pkgs.vimUtils.buildVimPlugin rec {
-    pname = "coq-nvim";
-    version = "2699492a1b8716c59ade7130dc354e53944b6a7f";
-    src = pkgs.fetchFromGitHub {
-      owner = "ms-jpq";
-      repo = "coq_nvim";
-      rev = version;
-      sha256 = "sha256-tjniIWe1V4vGuao5no+3YB9WtiNaMehEjffJyNpFgd8=";
-    };
+  coq-nvim = pkgs.vimUtils.buildVimPlugin {
+    inherit (sources.coq-nvim) pname version src;
 
     passthru.python3Dependencies = ps: [
       ps.pynvim
       ps.pyyaml
-      (ps.buildPythonPackage rec {
-        pname = "pynvim_pp";
-        version = "01dc0f58d4e71a98c388e1f37bda3d1357089fa2";
-
-        src = pkgs.fetchFromGitHub {
-          owner = "ms-jpq";
-          repo = "pynvim_pp";
-          rev = version;
-          sha256 = "sha256-/m4Paw6AvDzTMWWCWpPnrdI4gsjIDSJPvGCMV7ufbEA=";
-        };
+      (ps.buildPythonPackage {
+        inherit (sources.pynvim_pp) pname version src;
 
         propagatedBuildInputs = [pkgs.python3Packages.pynvim];
       })
-      (ps.buildPythonPackage rec {
-        pname = "std2";
-        version = "48bb39b69ed631ef64eed6123443484133fd20fc";
-
+      (ps.buildPythonPackage {
+        inherit (sources.std2) pname version src;
         doCheck = true;
-
-        src = pkgs.fetchFromGitHub {
-          owner = "ms-jpq";
-          repo = "std2";
-          rev = version;
-          sha256 = "sha256-nMwNAq15zyf9ORhFGo0sawQukOygYoVWtT7jH68MIkI=";
-        };
       })
     ];
 
@@ -72,28 +38,12 @@
     '';
   };
 
-  coq-artifacts = pkgs.vimUtils.buildVimPlugin rec {
-    pname = "coq.artifacts";
-    version = "495429564e481cafeb044456da32c10cb631f948";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "ms-jpq";
-      repo = "coq.artifacts";
-      rev = version;
-      sha256 = "sha256-AtkG2XRVZgvJzH2iLr7UT/U1+LXxenvNckdapnJV+8A=";
-    };
+  coq-artifacts = pkgs.vimUtils.buildVimPlugin {
+    inherit (sources.coq-artifacts) pname version src;
   };
 
-  magma-nvim = pkgs.vimUtils.buildVimPlugin rec {
-    pname = "magma-nvim";
-    version = "94370733757d550594fe4a1d65643949d7485989";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "WhiteBlackGoose";
-      repo = "magma-nvim-goose";
-      rev = version;
-      sha256 = "sha256-IaslJK1F2BxTvZzKGH9OKOl2RICi4d4rSgjliAIAqK4=";
-    };
+  magma-nvim = pkgs.vimUtils.buildVimPlugin {
+    inherit (sources.magma-nvim) pname version src;
 
     passthru.python3Dependencies = ps:
       with ps; [
