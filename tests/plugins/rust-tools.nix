294a6d5b0985064dc87c1f24cff74ea50ab95e87
Gaetan Lepage <gaetan@glepage.com>
2023-03-15 01:20:37 +0100

    plugins/rust-tools: refactoring + tests

---
+++ tests/plugins/rust-tools.nix
@@ -0,0 +1,52 @@
+{
+  # Empty configuration
+  empty = {
+    plugins.rust-tools.enable = true;
+  };
+
+  # All the upstream default options of rust-tools
+  defaults = {
+    plugins.rust-tools = {
+      enable = true;
+      executor = "termopen";
+      onInitialized = null;
+      reloadWorkspaceFromCargoToml = true;
+      inlayHints = {
+        auto = true;
+        onlyCurrentLine = false;
+        showParameterHints = true;
+        parameterHintsPrefix = "<- ";
+        otherHintsPrefix = "=> ";
+        maxLenAlign = false;
+        maxLenAlignPadding = 1;
+        rightAlign = false;
+        rightAlignPadding = 7;
+        highlight = "Comment";
+      };
+      hoverActions = {
+        border = [
+          ["╭" "FloatBorder"]
+          ["─" "FloatBorder"]
+          ["╮" "FloatBorder"]
+          ["│" "FloatBorder"]
+          ["╯" "FloatBorder"]
+          ["─" "FloatBorder"]
+          ["╰" "FloatBorder"]
+          ["│" "FloatBorder"]
+        ];
+        maxWidth = null;
+        maxHeight = null;
+        autoFocus = false;
+      };
+      crateGraph = {
+        backend = "x11";
+        output = null;
+        full = true;
+        enabledGraphvizBackends = null;
+      };
+      server = {
+        standalone = true;
+      };
+    };
+  };
+}
