2550c919a5acac86ccf79972273a9193c6c77869
Gaétan Lepage <33058747+GaetanLepage@users.noreply.github.com>
2023-03-22 15:00:50 +0100

    plugins/coq-thirdparty: init + tests (#282)

---
+++ plugins/completion/coq-thirdparty.nix
@@ -0,0 +1,65 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.plugins.coq-thirdparty;
+  helpers = import ../helpers.nix {inherit lib;};
+in {
+  options.plugins.coq-thirdparty = {
+    enable = mkEnableOption "coq-thirdparty";
+
+    package = helpers.mkPackageOption "coq-thirdparty" pkgs.vimPlugins.coq-thirdparty;
+
+    sources = mkOption {
+      type = types.listOf (types.submodule {
+        freeformType = types.attrs;
+
+        options = {
+          src = mkOption {
+            type = types.str;
+            description = "The name of the source";
+          };
+
+          short_name = mkOption {
+            type = types.nullOr types.str;
+            description = ''
+              A short name for the source.
+              If not specified, it is uppercase `src`.
+            '';
+            example = "nLUA";
+            default = null;
+          };
+        };
+      });
+      description = "List of sources";
+      default = [];
+      example = [
+        {
+          src = "nvimlua";
+          shortName = "nLUA";
+        }
+        {
+          src = "vimtex";
+          shortName = "vTEX";
+        }
+        {
+          src = "copilot";
+          shortName = "COP";
+          acceptKey = "<c-f>";
+        }
+        {src = "demo";}
+      ];
+    };
+  };
+
+  config = mkIf cfg.enable {
+    extraPlugins = [cfg.package];
+
+    extraConfigLua = ''
+      require('coq_3p')(${helpers.toLuaObject cfg.sources})
+    '';
+  };
+}
---
+++ tests/test-sources/plugins/completion/coq-thirdparty.nix
@@ -0,0 +1,29 @@
+{
+  # Empty configuration
+  empty = {
+    plugins.coq-thirdparty.enable = true;
+  };
+
+  example = {
+    plugins.coq-thirdparty = {
+      enable = true;
+
+      sources = [
+        {
+          src = "nvimlua";
+          short_name = "nLUA";
+        }
+        {
+          src = "vimtex";
+          short_name = "vTEX";
+        }
+        {
+          src = "copilot";
+          short_name = "COP";
+          accept_key = "<c-f>";
+        }
+        {src = "demo";}
+      ];
+    };
+  };
+}
