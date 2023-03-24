58b6c74e2fe817cbb0dd38b0c33c4dc10025d11e
Gaétan Lepage <33058747+GaetanLepage@users.noreply.github.com>
2023-03-24 08:18:45 +0100

    helpers: add globalPrefix to mkPlugin (#284)

--- plugins/languages/ledger.nix
+++ plugins/languages/ledger.nix
@@ -10,27 +10,29 @@ with import ../helpers.nix {inherit lib;};
     description = "ledger language features";
     package = pkgs.vimPlugins.vim-ledger;
 
+    globalPrefix = "ledger_";
+
     options = {
       maxWidth = mkDefaultOpt {
-        global = "ledger_maxwidth";
+        global = "maxwidth";
         description = "Number of columns to display foldtext";
         type = types.int;
       };
 
       fillString = mkDefaultOpt {
-        global = "ledger_fillstring";
+        global = "fillstring";
         description = "String used to fill the space between account name and amount in the foldtext";
         type = types.int;
       };
 
       detailedFirst = mkDefaultOpt {
-        global = "ledger_detailed_first";
+        global = "detailed_first";
         description = "Account completion sorted by depth instead of alphabetically";
         type = types.bool;
       };
 
       foldBlanks = mkDefaultOpt {
-        global = "ledger_fold_blanks";
+        global = "fold_blanks";
         description = "Hide blank lines following a transaction on a fold";
         type = types.bool;
       };
