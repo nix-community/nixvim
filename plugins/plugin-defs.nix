a261e8cdb703ba97e6249611e3744aaffcab406e
Gaetan Lepage <gaetan@glepage.com>
2023-03-26 15:53:52 +0200

    plugins/coq-nvim: remove useless local definition of the coq_nvim plugin

--- plugins/plugin-defs.nix
+++ plugins/plugin-defs.nix
@@ -5,30 +5,4 @@
 # You can update the plugins by running `nvfetcher` in this directory
 {pkgs, ...}: let
   sources = pkgs.callPackage ./_sources/generated.nix {};
-in {
-  coq-nvim = pkgs.vimUtils.buildVimPlugin {
-    inherit (sources.coq-nvim) pname version src;
-
-    passthru.python3Dependencies = ps: [
-      ps.pynvim
-      ps.pyyaml
-      (ps.buildPythonPackage {
-        inherit (sources.pynvim_pp) pname version src;
-
-        propagatedBuildInputs = [pkgs.python3Packages.pynvim];
-      })
-      (ps.buildPythonPackage {
-        inherit (sources.std2) pname version src;
-        doCheck = true;
-      })
-    ];
-
-    # We need some patches so it stops complaining about not being in a venv
-    postPatch = ''
-      substituteInPlace coq/consts.py \
-        --replace "VARS = TOP_LEVEL / \".vars\"" "VARS = Path.home() / \".cache/home/vars\"";
-      substituteInPlace coq/__main__.py \
-        --replace "_IN_VENV = _RT_PY == _EXEC_PATH" "_IN_VENV = True"
-    '';
-  };
-}
+in {}
