75a5e45b2b85a17e2c4b5f1e8d86f0552dff6abe
traxys <quentin+dev@familleboyer.net>
2023-04-16 13:08:07 +0200

    plugin/which-key: init + tests (#337)

---
+++ tests/test-sources/plugins/utils/which-key.nix
@@ -0,0 +1,90 @@
+{
+  empty = {
+    plugins.which-key.enable = true;
+  };
+
+  defaults = {
+    plugins.which-key = {
+      enable = true;
+      plugins = {
+        marks = true;
+        registers = true;
+        spelling = {
+          enabled = true;
+          suggestions = 20;
+        };
+        presets = {
+          operators = true;
+          motions = true;
+          textObjects = true;
+          windows = true;
+          nav = true;
+          z = true;
+          g = true;
+        };
+      };
+
+      operators = {gc = "Comments";};
+      keyLabels = {};
+
+      motions = {
+        count = true;
+      };
+
+      icons = {
+        breadcrumb = "»";
+        separator = "➜";
+        group = "+";
+      };
+
+      popupMappings = {
+        scrollDown = "<c-d>";
+        scrollUp = "<c-u>";
+      };
+
+      window = {
+        border = "none";
+        position = "bottom";
+        margin = {
+          top = 1;
+          right = 0;
+          bottom = 1;
+          left = 0;
+        };
+        padding = {
+          top = 1;
+          right = 2;
+          bottom = 1;
+          left = 2;
+        };
+        winblend = 0;
+      };
+      layout = {
+        height = {
+          min = 4;
+          max = 25;
+        };
+        width = {
+          min = 20;
+          max = 50;
+        };
+        spacing = 3;
+        align = "left";
+      };
+      ignoreMissing = false;
+      hidden = ["<silent>" "<cmd>" "<Cmd>" "<CR>" "^:" "^ " "^call " "^lua "];
+      showHelp = true;
+      showKeys = true;
+      triggers = "auto";
+      triggersNoWait = ["`" "'" "g`" "g'" ''"'' "<c-r>" "z="];
+      triggersBlackList = {
+        i = ["j" "k"];
+        v = ["j" "k"];
+      };
+      disable = {
+        buftypes = [];
+        filetypes = [];
+      };
+    };
+  };
+}
