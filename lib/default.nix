becd725d4420acebe54d49895c658740eefa6fc8
Alexander Nortung <alexander.nortung@oakdigital.dk>
2023-05-13 21:51:09 +0200

    Fix path in lib for testing nixvim

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,6 @@
 # Args probably only needs pkgs and lib
 args: {
   # Add all exported modules here
-  check = import ./check.nix args;
+  check = import ../tests/test-derivation.nix args;
   helpers = import ./helpers.nix args;
 }
