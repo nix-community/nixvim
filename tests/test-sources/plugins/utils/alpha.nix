a0dbcd5b3362cba7ad5fe1ff41cf6fe80fd9c5ba
CaptainEureka <captaineureka@gmail.com>
2023-05-24 19:42:58 +0200

    update: add default configuration

--- tests/test-sources/plugins/utils/alpha.nix
+++ tests/test-sources/plugins/utils/alpha.nix
@@ -2,4 +2,63 @@
   empty = {
     plugins.alpha.enable = true;
   };
+
+  default = {
+    plugins.alpha = {
+      enable = true;
+      iconsEnabled = true;
+      layout = [
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          type = "text";
+          val = [
+            "  ███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗  "
+            "  ████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║  "
+            "  ██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║  "
+            "  ██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║  "
+            "  ██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║  "
+            "  ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝  "
+          ];
+          opts = {
+            position = "center";
+            hl = "Type";
+          };
+        }
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          type = "group";
+          val = [
+            {
+              shortcut = "e";
+              desc = "  New file";
+              command = "<CMD>ene <CR>";
+            }
+            {
+              shortcut = "SPC q";
+              desc = "  Quit Neovim";
+              command = ":qa<CR>";
+            }
+          ];
+        }
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          type = "text";
+          val = "Inspiring quote here.";
+          opts = {
+            position = "center";
+            hl = "Keyword";
+          };
+        }
+      ];
+    };
+  };
 }
