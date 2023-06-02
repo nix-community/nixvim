1f285df66498952c9b1315fdc591c0c3e1d3b5f1
Gaetan Lepage <gaetan@glepage.com>
2023-06-02 13:13:27 +0200

    plugins/auto-save: init + test

---
+++ tests/test-sources/plugins/utils/auto-save.nix
@@ -0,0 +1,47 @@
+{
+  empty = {
+    plugins.auto-save.enable = true;
+  };
+
+  defaults = {
+    plugins.auto-save = {
+      enable = true;
+
+      keymaps = {
+        silent = true;
+        toggle = "<leader>s";
+      };
+      enableAutoSave = true;
+      executionMessage = {
+        message.__raw = ''
+          function()
+            return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+          end
+        '';
+        dim = 0.18;
+        cleaningInterval = 1250;
+      };
+      triggerEvents = ["InsertLeave" "TextChanged"];
+      condition = ''
+        function(buf)
+          local fn = vim.fn
+          local utils = require("auto-save.utils.data")
+
+          if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+            return true -- met condition(s), can save
+          end
+          return false -- can't save
+        end
+      '';
+      writeAllBuffers = false;
+      debounceDelay = 135;
+      callbacks = {
+        enabling = null;
+        disabling = null;
+        beforeAssertingSave = null;
+        beforeSaving = null;
+        afterSaving = null;
+      };
+    };
+  };
+}
