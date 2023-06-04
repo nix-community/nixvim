cae34a7b8f83293b21c1c0981ba810883719dfe0
traxys <quentin+dev@familleboyer.net>
2023-06-04 12:12:04 +0200

    flake: Pin nixpkgs to 23.05 (#413)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A neovim configuration system for NixOS";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
   inputs.beautysh.url = "github:lovesegfault/beautysh";
   inputs.beautysh.inputs.nixpkgs.follows = "nixpkgs";
