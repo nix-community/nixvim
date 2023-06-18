3d766c90e62be66dbc243ef82f127fa5d6bf1ee1
Wolbyte <wolbyte@gmail.com>
2023-06-19 00:16:22 +0330

    plugins/rose-pine: add colorscheme & test

---
+++ plugins/colorschemes/rose-pine.nix
@@ -0,0 +1,57 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+} @ args:
+with lib; let
+  cfg = config.colorschemes.rose-pine;
+  helpers = import ../helpers.nix args;
+in {
+  options = {
+    colorschemes.rose-pine = {
+      enable = mkEnableOption "rosepine";
+      package = helpers.mkPackageOption "rosepine" pkgs.vimPlugins.rose-pine;
+      style = helpers.defaultNullOpts.mkEnumFirstDefault ["main" "moon" "dawn"] "Theme style";
+
+      boldVerticalSplit = helpers.defaultNullOpts.mkBool false "Bolds vertical splits";
+      dimInactive = helpers.defaultNullOpts.mkBool false "Dims inactive windows";
+      disableItalics = helpers.defaultNullOpts.mkBool false "Disables italics";
+
+      groups = mkOption {
+        type = types.nullOr types.attrs;
+        default = null;
+        description = "rose-pine highlight groups";
+      };
+
+      highlightGroups = mkOption {
+        type = types.nullOr types.attrs;
+        default = null;
+        description = "Custom highlight groups";
+      };
+
+      transparentBackground = helpers.defaultNullOpts.mkBool false "Disable setting background";
+      transparentFloat = helpers.defaultNullOpts.mkBool false "Disable setting background for floating windows";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    colorscheme = "rose-pine";
+    extraPlugins = [cfg.package];
+    options = {termguicolors = true;};
+    extraConfigLuaPre = let
+      setupOptions = with cfg; {
+        dark_variant = style;
+        bold_vert_split = boldVerticalSplit;
+        disable_background = transparentBackground;
+        disable_float_background = transparentFloat;
+        disable_italics = disableItalics;
+        dim_nc_background = dimInactive;
+        groups = cfg.groups;
+        highlight_groups = highlightGroups;
+      };
+    in ''
+      require("rose-pine").setup(${helpers.toLuaObject setupOptions})
+    '';
+  };
+}
