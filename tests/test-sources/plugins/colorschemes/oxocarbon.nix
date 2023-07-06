3c32d0ef018ffa8949302133786e460f7dbe1ad0
BattleCh1cken <BattleCh1cken@larkov.de>
2023-07-06 10:23:43 -0400

    plugins/colorschems/oxocarbon: test

---
+++ tests/test-sources/plugins/colorschemes/oxocarbon.nix
@@ -0,0 +1,10 @@
+{
+  empty = {colorschemes.oxocarbon.enable = true;};
+
+  # All the upstream default options of oxocarbon
+  defaults = {
+    colorschemes.oxocarbon = {
+      enable = true;
+    };
+  };
+}
