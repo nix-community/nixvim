89bbd4b916e99aa9719d133a101bce88694a70db
Eduardo Quirós <eduardo@eduardoquiros.com>
2023-07-09 11:17:24 +0000

    plugins/nvim-lsp: add elmls (#476)
    * plugins/nvim-lsp: enable elm-language-server
    
    * Update plugins/lsp/language-servers/default.nix
    
    use suggested description
    
    Co-authored-by: Gaétan Lepage <33058747+GaetanLepage@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Gaétan Lepage <33058747+GaetanLepage@users.noreply.github.com>

--- plugins/lsp/language-servers/default.nix
+++ plugins/lsp/language-servers/default.nix
@@ -148,6 +148,11 @@ with lib; let
       package = pkgs.deno;
     }
     {
+      name = "elmls";
+      description = "Enable elmls, for Elm.";
+      package = pkgs.elmPackages.elm-language-server;
+    }
+    {
       name = "eslint";
       description = "Enable eslint";
       package = pkgs.vscode-langservers-extracted;
