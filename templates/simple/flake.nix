0778266e4499ba0bf402a726a07fbd58b11ff4f8
Pedro Alves <pta2002@pta2002.com>
2023-07-14 21:37:21 +0100

    README: change pta2002 to nix-community

--- templates/simple/flake.nix
+++ templates/simple/flake.nix
@@ -2,27 +2,30 @@
   description = "A nixvim configuration";
 
   inputs = {
-    nixvim.url = "github:pta2002/nixvim";
+    nixvim.url = "github:nix-community/nixvim";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    nixvim,
-    flake-utils,
-    ...
-  } @ inputs: let
-    config = import ./config; # import the module directly
-  in
-    flake-utils.lib.eachDefaultSystem (system: let
+  outputs =
+    { nixpkgs
+    , nixvim
+    , flake-utils
+    , ...
+    } @ inputs:
+    let
+      config = import ./config; # import the module directly
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+    let
       nixvimLib = nixvim.lib.${system};
-      pkgs = import nixpkgs {inherit system;};
+      pkgs = import nixpkgs { inherit system; };
       nixvim' = nixvim.legacyPackages.${system};
       nvim = nixvim'.makeNixvimWithModule {
         inherit pkgs;
         module = config;
       };
-    in {
+    in
+    {
       checks = {
         # Run `nix flake check .` to verify that your config is not broken
         default = nixvimLib.check.mkTestDerivationFromNvim {
