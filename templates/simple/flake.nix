82a7d666f592f43a07ba0b87e035399eeeb949ae
Pedro Alves <pta2002@pta2002.com>
2023-07-14 21:44:31 +0100

    fix: fix formatting on simple template

--- templates/simple/flake.nix
+++ templates/simple/flake.nix
@@ -6,26 +6,23 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { nixpkgs
-    , nixvim
-    , flake-utils
-    , ...
-    } @ inputs:
-    let
-      config = import ./config; # import the module directly
-    in
-    flake-utils.lib.eachDefaultSystem (system:
-    let
+  outputs = {
+    nixpkgs,
+    nixvim,
+    flake-utils,
+    ...
+  } @ inputs: let
+    config = import ./config; # import the module directly
+  in
+    flake-utils.lib.eachDefaultSystem (system: let
       nixvimLib = nixvim.lib.${system};
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {inherit system;};
       nixvim' = nixvim.legacyPackages.${system};
       nvim = nixvim'.makeNixvimWithModule {
         inherit pkgs;
         module = config;
       };
-    in
-    {
+    in {
       checks = {
         # Run `nix flake check .` to verify that your config is not broken
         default = nixvimLib.check.mkTestDerivationFromNvim {
