7161486f8565b3d41b05c3669aacf1edb83c7971
Haseeb Majid <hello@haseebmajid.dev>
2023-09-20 08:59:12 +0100

    Update ToDos

--- plugins/bufferlines/navic.nix
+++ plugins/bufferlines/navic.nix
@@ -1,14 +1,14 @@
-{ lib
-, pkgs
-, config
-, ...
+{
+  lib,
+  pkgs,
+  config,
+  ...
 }:
 with lib; let
   cfg = config.plugins.navic;
-  helpers = import ../helpers.nix { inherit lib; };
+  helpers = import ../helpers.nix {inherit lib;};
   mkListStr = helpers.defaultNullOpts.mkNullable (types.listOf types.str);
-in
-{
+in {
   options.plugins.navic =
     helpers.extraOptionsOptions
     // {
@@ -48,9 +48,9 @@ in
       lsp = with helpers.defaultNullOpts; {
         autoAttach =
           helpers.defaultNullOpts.mkBool false
-            ''
-              Enable to have nvim-navic automatically attach to every LSP for current buffer. Its disabled by default.
-            '';
+          ''
+            Enable to have nvim-navic automatically attach to every LSP for current buffer. Its disabled by default.
+          '';
 
         preference = mkListStr "[]" ''
           Table ranking lsp_servers. Lower the index, higher the priority of the server. If there are more than one server attached to a buffer. In the example below will prefer clangd over pyright
@@ -61,65 +61,64 @@ in
 
       highlight =
         helpers.defaultNullOpts.mkBool false
-          ''
-            If set to true, will add colors to icons and text as defined by highlight groups NavicIcons* (NavicIconsFile, NavicIconsModule.. etc.), NavicText and NavicSeparator.
-          '';
+        ''
+          If set to true, will add colors to icons and text as defined by highlight groups NavicIcons* (NavicIconsFile, NavicIconsModule.. etc.), NavicText and NavicSeparator.
+        '';
 
       separator =
         helpers.defaultNullOpts.mkStr " > "
-          ''
-            Icon to separate items. to use between items.
-          '';
+        ''
+          Icon to separate items. to use between items.
+        '';
 
       depthLimit =
         helpers.defaultNullOpts.mkInt 0
-          ''
-            Maximum depth of context to be shown. If the context hits this depth limit, it is truncated.
-          '';
+        ''
+          Maximum depth of context to be shown. If the context hits this depth limit, it is truncated.
+        '';
 
       depthLimitIndicator =
         helpers.defaultNullOpts.mkStr ".."
-          ''
-            Icon to indicate that depth_limit was hit and the shown context is truncated.
-          '';
+        ''
+          Icon to indicate that depth_limit was hit and the shown context is truncated.
+        '';
 
       safeOutput =
         helpers.defaultNullOpts.mkBool true
-          ''
-            Sanitize the output for use in statusline and winbar.
-          '';
+        ''
+          Sanitize the output for use in statusline and winbar.
+        '';
 
       lazyUpdateContext =
         helpers.defaultNullOpts.mkBool false
-          ''
-            If true, turns off context updates for the "CursorMoved" event.
-          '';
+        ''
+          If true, turns off context updates for the "CursorMoved" event.
+        '';
 
       click =
         helpers.defaultNullOpts.mkBool false
-          ''
-            Single click to goto element, double click to open nvim-navbuddy on the clicked element.
-          '';
+        ''
+          Single click to goto element, double click to open nvim-navbuddy on the clicked element.
+        '';
     };
 
-  config =
-    let
-      setupOptions = with cfg;
-        {
-          icons = icons;
-          lsp = with lsp; {
-            auto_attach = autoAttach;
-            preference = preference;
-          };
-          highlight = highlight;
-          separator = separator;
-          depth_limit = depthLimit;
-          safe_output = safeOutput;
-          lazy_update_context = lazyUpdateContext;
-          click = click;
-        }
-        // cfg.extraOptions;
-    in
+  config = let
+    setupOptions = with cfg;
+      {
+        icons = icons;
+        lsp = with lsp; {
+          auto_attach = autoAttach;
+          preference = preference;
+        };
+        highlight = highlight;
+        separator = separator;
+        depth_limit = depthLimit;
+        safe_output = safeOutput;
+        lazy_update_context = lazyUpdateContext;
+        click = click;
+      }
+      // cfg.extraOptions;
+  in
     mkIf cfg.enable {
       extraPlugins = with pkgs.vimPlugins; [
         cfg.package
