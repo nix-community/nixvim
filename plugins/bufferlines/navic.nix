ea5af0e48d1702498a47a043199b0a6043afda6a
Haseeb Majid <haseeb.majid@imaginecurve.com>
2023-09-20 14:03:13 +0100

    update tests

--- plugins/bufferlines/navic.nix
+++ plugins/bufferlines/navic.nix
@@ -52,11 +52,9 @@ in {
         };
 
       lsp = {
-        autoAttach =
-          helpers.defaultNullOpts.mkBool false
-          ''
-            Enable to have nvim-navic automatically attach to every LSP for current buffer. Its disabled by default.
-          '';
+        autoAttach = helpers.defaultNullOpts.mkBool false ''
+          Enable to have nvim-navic automatically attach to every LSP for current buffer. Its disabled by default.
+        '';
 
         preference = helpers.defaultNullOpts.mkListStr "[]" ''
           Table ranking lsp_servers. Lower the index, higher the priority of the server. If there are more than one server attached to a buffer. In the example below will prefer clangd over pyright
@@ -65,23 +63,17 @@ in {
         '';
       };
 
-      highlight =
-        helpers.defaultNullOpts.mkBool false
-        ''
-          If set to true, will add colors to icons and text as defined by highlight groups NavicIcons* (NavicIconsFile, NavicIconsModule.. etc.), NavicText and NavicSeparator.
-        '';
+      highlight = helpers.defaultNullOpts.mkBool false ''
+        If set to true, will add colors to icons and text as defined by highlight groups NavicIcons* (NavicIconsFile, NavicIconsModule.. etc.), NavicText and NavicSeparator.
+      '';
 
-      separator =
-        " > "
-        ''
-          Icon to separate items. to use between items.
-        '';
+      separator = " > " ''
+        Icon to separate items. to use between items.
+      '';
 
-      depthLimit =
-        helpers.defaultNullOpts.mkInt 0
-        ''
-          Maximum depth of context to be shown. If the context hits this depth limit, it is truncated.
-        '';
+      depthLimit = helpers.defaultNullOpts.mkInt 0 ''
+        Maximum depth of context to be shown. If the context hits this depth limit, it is truncated.
+      '';
 
       depthLimitIndicator =
         ".."
@@ -89,23 +81,17 @@ in {
           Icon to indicate that depth_limit was hit and the shown context is truncated.
         '';
 
-      safeOutput =
-        helpers.defaultNullOpts.mkBool true
-        ''
-          Sanitize the output for use in statusline and winbar.
-        '';
+      safeOutput = helpers.defaultNullOpts.mkBool true ''
+        Sanitize the output for use in statusline and winbar.
+      '';
 
-      lazyUpdateContext =
-        helpers.defaultNullOpts.mkBool false
-        ''
-          If true, turns off context updates for the "CursorMoved" event.
-        '';
+      lazyUpdateContext = helpers.defaultNullOpts.mkBool false ''
+        If true, turns off context updates for the "CursorMoved" event.
+      '';
 
-      click =
-        helpers.defaultNullOpts.mkBool false
-        ''
-          Single click to goto element, double click to open nvim-navbuddy on the clicked element.
-        '';
+      click = helpers.defaultNullOpts.mkBool false ''
+        Single click to goto element, double click to open nvim-navbuddy on the clicked element.
+      '';
     };
 
   config = let
--- tests/test-sources/plugins/bufferlines/navic.nix
+++ tests/test-sources/plugins/bufferlines/navic.nix
@@ -6,10 +6,18 @@
   defaults = {
     plugins.navic = {
       enable = true;
+      icons = {
+        File = "󰆧 ";
+      };
       lsp = {
         autoAttach = true;
+        preference = ["clangd" "pyright"];
       };
+      highlight = true;
       separator = " | ";
+      depthLimit = 10;
+      safeOutput = false;
+
       click = true;
     };
   };
