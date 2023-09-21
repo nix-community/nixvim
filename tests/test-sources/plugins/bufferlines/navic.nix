32a64af2318beac3c89d4d1fa0b73ce237c5cf1e
Haseeb Majid <hello@haseebmajid.dev>
2023-09-21 13:48:05 +0100

    plugins/navic: init + tests (#597)
    Co-authored-by: Haseeb Majid <haseeb.majid@imaginecurve.com>
    Co-authored-by: Gaétan Lepage <33058747+GaetanLepage@users.noreply.github.com>

---
+++ tests/test-sources/plugins/bufferlines/navic.nix
@@ -0,0 +1,49 @@
+{
+  empty = {
+    plugins.navic.enable = true;
+  };
+
+  defaults = {
+    plugins.navic = {
+      enable = true;
+
+      icons = {
+        File = "󰆧 ";
+        Module = " ";
+        Namespace = "󰌗 ";
+        Package = " ";
+        Class = "󰌗 ";
+        Method = "󰆧 ";
+        Property = " ";
+        Field = " ";
+        Constructor = " ";
+        Enum = "󰕘";
+        Interface = "󰕘";
+        Function = "󰊕 ";
+        Variable = "󰆧 ";
+        Constant = "󰏿 ";
+        String = "󰀬 ";
+        Number = "󰎠 ";
+        Boolean = "◩ ";
+        Array = "󰅪 ";
+        Object = "󰅩 ";
+        Key = "󰌋 ";
+        Null = "󰟢 ";
+        EnumMember = " ";
+        Struct = "󰌗 ";
+        Event = " ";
+        Operator = "󰆕 ";
+        TypeParameter = "󰊄 ";
+      };
+      lsp = {
+        autoAttach = true;
+        preference = ["clangd" "pyright"];
+      };
+      highlight = true;
+      separator = " | ";
+      depthLimit = 10;
+      safeOutput = false;
+      click = true;
+    };
+  };
+}
