798d13b4a467d018eeea168dbeb9085cfc028292
Haseeb Majid <hello@haseebmajid.dev>
2023-09-22 23:45:33 +0100

    update mappings

--- plugins/utils/navbuddy.nix
+++ plugins/utils/navbuddy.nix
@@ -1,16 +1,16 @@
-{ lib
-, pkgs
-, config
-, ...
+{
+  lib,
+  pkgs,
+  config,
+  ...
 }:
 with lib; let
   cfg = config.plugins.navic;
-  helpers = import ../helpers.nix { inherit lib; };
+  helpers = import ../helpers.nix {inherit lib;};
   mkListStr = helpers.defaultNullOpts.mkNullable (types.listOf types.str);
   percentageType = types.ints.between 0 100;
   mkPercentageOpt = default: helpers.defaultNullOpts.mkNullable percentageType (toString default);
-in
-{
+in {
   options.plugins.navic =
     helpers.extraOptionsOptions
     // {
@@ -26,36 +26,36 @@ in
 
         size =
           helpers.defaultNullOpts.mkNullable
-            (
-              with types;
+          (
+            with types;
               either
-                percentageType
-                (submodule {
-                  options = {
-                    height = mkPercentageOpt 40 "The height size (in %).";
+              percentageType
+              (submodule {
+                options = {
+                  height = mkPercentageOpt 40 "The height size (in %).";
 
-                    width = mkPercentageOpt 100 "The width size (in %).";
-                  };
-                })
-            )
-            "60"
-            "The size of the window.";
+                  width = mkPercentageOpt 100 "The width size (in %).";
+                };
+              })
+          )
+          "60"
+          "The size of the window.";
 
         position =
           helpers.defaultNullOpts.mkNullable
-            (
-              with types;
+          (
+            with types;
               either
-                percentageType
-                (submodule {
-                  options = {
-                    height = mkPercentageOpt 40 "The height size (in %).";
+              percentageType
+              (submodule {
+                options = {
+                  height = mkPercentageOpt 40 "The height size (in %).";
 
-                    width = mkPercentageOpt 100 "The width size (in %).";
-                  };
-                })
-            )
-            "50";
+                  width = mkPercentageOpt 100 "The width size (in %).";
+                };
+              })
+          )
+          "50";
 
         scrolloff = helpers.defaultNullOpts.mkInt null ''
           scrolloff value within navbuddy window
@@ -83,7 +83,7 @@ in
               "rounded", "double", "solid", "none"  or an array with eight chars building up the border in a clockwise fashion
               starting with the top-left corner. eg: { "╔", "═" ,"╗", "║", "╝", "═", "╚", "║" }.
             '';
-            preview = helpers.defaultNullOpts.mkEnum [ "leaf" "always" "never" ] "leaf" ''
+            preview = helpers.defaultNullOpts.mkEnum ["leaf" "always" "never"] "leaf" ''
               Right section can show previews too.
                     Options: "leaf", "always" or "never"
             '';
@@ -109,38 +109,38 @@ in
 
       icons =
         mapAttrs
-          (
-            name: default:
-              default "icon for ${name}."
-          )
-          {
-            File = "󰈙 ";
-            Module = " ";
-            Namespace = "󰌗 ";
-            Package = " ";
-            Class = "󰌗 ";
-            Method = "󰆧 ";
-            Property = " ";
-            Field = " ";
-            Constructor = " ";
-            Enum = "󰕘";
-            Interface = "󰕘";
-            Function = "󰊕 ";
-            Variable = "󰆧 ";
-            Constant = "󰏿 ";
-            String = "󰀬 ";
-            Number = "󰎠 ";
-            Boolean = "◩ ";
-            Array = "󰅪 ";
-            Object = "󰅩 ";
-            Key = "󰌋 ";
-            Null = "󰟢 ";
-            EnumMember = " ";
-            Struct = "󰌗 ";
-            Event = " ";
-            Operator = "󰆕 ";
-            TypeParameter = "󰊄 ";
-          };
+        (
+          name: default:
+            default "icon for ${name}."
+        )
+        {
+          File = "󰈙 ";
+          Module = " ";
+          Namespace = "󰌗 ";
+          Package = " ";
+          Class = "󰌗 ";
+          Method = "󰆧 ";
+          Property = " ";
+          Field = " ";
+          Constructor = " ";
+          Enum = "󰕘";
+          Interface = "󰕘";
+          Function = "󰊕 ";
+          Variable = "󰆧 ";
+          Constant = "󰏿 ";
+          String = "󰀬 ";
+          Number = "󰎠 ";
+          Boolean = "◩ ";
+          Array = "󰅪 ";
+          Object = "󰅩 ";
+          Key = "󰌋 ";
+          Null = "󰟢 ";
+          EnumMember = " ";
+          Struct = "󰌗 ";
+          Event = " ";
+          Operator = "󰆕 ";
+          TypeParameter = "󰊄 ";
+        };
 
       useDefaultMapping = helpers.defaultNullOpts.mkBool true ''
         If set to false, only mappings set by user are set. Else default mappings are used for keys that are not set by user
@@ -188,7 +188,6 @@ in
 
         "<C-v>" = "vsplit";
         "<C-s>" = "hsplit";
-
       };
 
       lsp = {
@@ -204,7 +203,7 @@ in
       sourceBuffer = {
         followNode = helpers.defaultNullOpts.mkBool true "Keep the current node in focus on the source buffer";
         highlight = helpers.defaultNullOpts.mkBool true "Highlight the currently focused node";
-        reorient = helpers.defaultNullOpts.mkEnum [ "smart" "top" "mid" "none" ] "smart" ''
+        reorient = helpers.defaultNullOpts.mkEnum ["smart" "top" "mid" "none"] "smart" ''
           Right section can show previews too.
                 Options: "leaf", "always" or "never"
         '';
@@ -214,27 +213,26 @@ in
       };
     };
 
-  config =
-    let
-      setupOptions = with cfg;
-        {
-          inherit
-            window
-            icons
-            mapping
-            ;
-          node_markers = nodeMarkers;
-          use_default_mapping = useDefaultMapping;
-          lsp = with lsp; {
-            auto_attach = autoAttach;
-            inherit preference;
-          };
-          source_buffer = sourceBuffer;
-        }
-        // cfg.extraOptions;
-    in
+  config = let
+    setupOptions = with cfg;
+      {
+        inherit
+          window
+          icons
+          mapping
+          ;
+        node_markers = nodeMarkers;
+        use_default_mapping = useDefaultMapping;
+        lsp = with lsp; {
+          auto_attach = autoAttach;
+          inherit preference;
+        };
+        source_buffer = sourceBuffer;
+      }
+      // cfg.extraOptions;
+  in
     mkIf cfg.enable {
-      extraPlugins = [ cfg.package ];
+      extraPlugins = [cfg.package];
 
       extraConfigLua = ''
         require('nvim-navbuddy').setup(${helpers.toLuaObject setupOptions})
