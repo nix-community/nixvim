fe69a0ac5ec59ee62c57255fc42382400bce71b7
Haseeb Majid <hello@haseebmajid.dev>
2023-09-30 11:07:06 +0100

    Update tests/test-sources/plugins/utils/navbuddy.nix
    Co-authored-by: Gaétan Lepage <33058747+GaetanLepage@users.noreply.github.com>

--- tests/test-sources/plugins/utils/navbuddy.nix
+++ tests/test-sources/plugins/utils/navbuddy.nix
@@ -6,22 +6,19 @@
   defaults = {
     plugins.navic = {
       enable = true;
+
       window = {
         border = "rounded";
       };
-
       size = {
         height = 50;
         width = 50;
       };
-
       position = {
         height = 50;
         width = 50;
       };
-
       scrolloff = 8;
-
       sections = {
         left = {
           size = 50;
@@ -36,18 +33,14 @@
           border = "rounded";
         };
       };
-
       nodeMarkers = {
         enabled = true;
         icons = {
           leaf = " ... ";
-
           leaf_selected = "  ";
-
           branch = " 󰆧 ";
         };
       };
-
       icons = {
         File = "󰆧 ";
         Module = " ";
@@ -76,51 +69,36 @@
         Operator = "󰆕 ";
         TypeParameter = "󰊄 ";
       };
-
       useDefaultMapping = false;
       mappings = {
         "<esc>" = "close";
         "q" = "close";
         "j" = "next_sibling";
         "k" = "previous_sibling";
-
         "h" = "parent";
         "l" = "children";
         "0" = "root";
-
         "v" = "visual_name";
         "V" = "visual_scope";
-
         "y" = "yank_name";
         "Y" = "yank_scope";
-
         "i" = "insert_name";
         "I" = "insert_scope";
-
         "a" = "append_name";
         "A" = "append_scope";
-
         "r" = "rename";
-
         "d" = "delete";
-
         "f" = "fold_create";
         "F" = "fold_delete";
-
         "c" = "comment";
-
         "<enter>" = "select";
         "o" = "select";
-
         "J" = "move_down";
         "K" = "move_up";
-
         "s" = "toggle_preview";
-
         "<C-v>" = "vsplit";
         "<C-s>" = "hsplit";
       };
-
       lsp = {
         autoAttach = true;
         preference = [
@@ -128,7 +106,6 @@
           "pyright"
         ];
       };
-
       sourceBuffer = {
         followNode = true;
         highlight = true;
