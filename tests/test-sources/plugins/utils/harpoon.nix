9db422d15fe72235deb9c5877e1e159628a82662
Gaetan Lepage <gaetan@glepage.com>
2023-10-03 16:09:56 +0200

    plugins/harpoon: fix keymaps + test

---
+++ tests/test-sources/plugins/utils/harpoon.nix
@@ -0,0 +1,62 @@
+{
+  empty = {
+    # Harpoon expects to access `~/.local/share/nvim/harpoon.json` which is not available in the
+    # test environment
+    tests.dontRun = true;
+
+    plugins.harpoon.enable = true;
+  };
+
+  example = {
+    # Harpoon expects to access `~/.local/share/nvim/harpoon.json` which is not available in the
+    # test environment
+    tests.dontRun = true;
+
+    plugins.harpoon = {
+      enable = true;
+
+      keymapsSilent = true;
+      keymaps = {
+        addFile = "<leader>a";
+        navFile = {
+          "1" = "<C-j>";
+          "2" = "<C-k>";
+          "3" = "<C-l>";
+          "4" = "<C-m>";
+        };
+        navNext = "<leader>b";
+        navPrev = "<leader>c";
+        gotoTerminal = {
+          "1" = "J";
+          "2" = "K";
+          "3" = "L";
+          "4" = "M";
+        };
+        cmdToggleQuickMenu = "<leader>d";
+        tmuxGotoTerminal = {
+          "1" = "<C-1>";
+          "2" = "<C-2>";
+          "{down-of}" = "<leader>g";
+        };
+      };
+      saveOnToggle = false;
+      saveOnChange = true;
+      enterOnSendcmd = false;
+      tmuxAutocloseWindows = false;
+      excludedFiletypes = ["harpoon"];
+      markBranch = false;
+      projects = {
+        "$HOME/personal/vim-with-me/server" = {
+          termCommands = [
+            "./env && npx ts-node src/index.ts"
+          ];
+        };
+      };
+      menu = {
+        width = 60;
+        height = 10;
+        borderChars = ["─" "│" "─" "│" "╭" "╮" "╯" "╰"];
+      };
+    };
+  };
+}
