ae54ba7c7af73ad6cfd65b603b759620b29bc1e0
Gaetan Lepage <gaetan@glepage.com>
2023-10-17 10:11:13 +0200

    plugins/indent-blankline: update options

--- tests/test-sources/plugins/utils/indent-blankline.nix
+++ tests/test-sources/plugins/utils/indent-blankline.nix
@@ -7,60 +7,62 @@
     plugins.indent-blankline = {
       enable = true;
 
-      char = "│";
-      charBlankline = "";
-      charList = [];
-      charListBlankline = [];
-      charHighlightList = [];
-      spaceCharBlankline = " ";
-      spaceCharHighlightList = [];
-      spaceCharBlanklineHighlightList = [];
-      useTreesitter = false;
-      indentLevel = 10;
-      maxIndentIncrease = 10;
-      showFirstIndentLevel = true;
-      showTrailingBlanklineIndent = true;
-      showEndOfLine = false;
-      showFoldtext = true;
-      disableWithNolist = false;
-      filetype = [];
-      filetypeExclude = ["lspinfo" "packer" "checkhealth" "help" "man" ""];
-      buftypeExclude = ["terminal" "nofile" "quickfix" "prompt"];
-      bufnameExclude = [];
-      strictTabs = false;
-      showCurrentContext = false;
-      showCurrentContextStart = false;
-      showCurrentContextStartOnCurrentLine = true;
-      contextChar = "│";
-      contextCharBlankline = "";
-      contextCharList = [];
-      contextCharListBlankline = [];
-      contextHighlightList = [];
-      charPriority = 1;
-      contextStartPriority = 10000;
-      contextPatterns = [
-        "class"
-        "^func"
-        "method"
-        "^if"
-        "while"
-        "for"
-        "with"
-        "try"
-        "except"
-        "arguments"
-        "argument_list"
-        "object"
-        "dictionary"
-        "element"
-        "table"
-        "tuple"
-        "do_block"
-      ];
-      useTreesitterScope = false;
-      contextPatternHighlight = {};
-      viewportBuffer = 10;
-      disableWarningMessage = false;
+      debounce = 200;
+      viewportBuffer = {
+        min = 30;
+        max = 500;
+      };
+      indent = {
+        char = "▎";
+        tabChar = null;
+        highlight = null;
+        smartIndentCap = true;
+        priority = 1;
+      };
+      whitespace = {
+        highlight = null;
+        removeBlanklineTrail = true;
+      };
+      scope = {
+        enabled = true;
+        char = null;
+        showStart = true;
+        showEnd = true;
+        showExactScope = false;
+        injectedLanguages = true;
+        highlight = null;
+        priority = 1024;
+        include = {
+          nodeType = {};
+        };
+        exclude = {
+          language = [];
+          nodeType = {
+            "*" = ["source_file" "program"];
+            lua = ["chunk"];
+            python = ["module"];
+          };
+        };
+      };
+      exclude = {
+        filetypes = [
+          "lspinfo"
+          "packer"
+          "checkhealth"
+          "help"
+          "man"
+          "gitcommit"
+          "TelescopePrompt"
+          "TelescopeResults"
+          "\'\'"
+        ];
+        buftypes = [
+          "terminal"
+          "nofile"
+          "quickfix"
+          "prompt"
+        ];
+      };
     };
   };
 }
