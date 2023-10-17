fa61174d101db430803a906d2b52bd79c4ec6234
Haseeb Majid <hello@haseebmajid.dev>
2023-10-17 21:49:04 +0100

    making changes

--- plugins/utils/nix-develop.nix
+++ plugins/utils/nix-develop.nix
@@ -24,21 +24,18 @@ in {
       };
     };
 
-  config = let
-    inherit (cfg) ignoredVariables separatedVariables;
-  in
-    mkIf cfg.enable {
-      extraPlugins = [cfg.package];
-      extraConfigLua = ''
-        local ignored_variables = ${helpers.toLuaObject ignoredVariables}
-        for ignoredVariable, shouldIgnore in ipairs(ignored_variables) do
-        	require("nix-develop").ignored_variables[ignoredVariable] = shouldIgnore
-        end
+  config = mkIf cfg.enable {
+    extraPlugins = [cfg.package];
+    extraConfigLua = ''
+      local __ignored_variables = ${helpers.toLuaObject cfg.ignoredVariables}
+      for ignoredVariable, shouldIgnore in ipairs(__ignored_variables) do
+      	require("nix-develop").ignored_variables[ignoredVariable] = shouldIgnore
+      end
 
-        local separated_variables = ${helpers.toLuaObject separatedVariables}
-        for variable, separator in ipairs(separated_variables) do
-        	require("nix-develop").separated_variables[variable] = separator
-        end
-      '';
-    };
+      local __separated_variables = ${helpers.toLuaObject cfg.separatedVariables}
+      for variable, separator in ipairs(__separated_variables) do
+      	require("nix-develop").separated_variables[variable] = separator
+      end
+    '';
+  };
 }
