f6a0ee2de545e27980d2ec59274b3e8e1ea1dbea
Voreck Lukas <voreck-lukas@tutanota.com>
2023-11-28 10:49:59 +0100

    plugins/formatter-nvim: Other options

--- plugins/utils/formatter-nvim.nix
+++ plugins/utils/formatter-nvim.nix
@@ -12,6 +12,8 @@ in
     options.plugins.formatter-nvim = {
       enable = mkEnableOption "formatter-nvim";
       package = helpers.mkPackageOption "formatter-nvim" pkgs.vimPlugins.formatter-nvim;
+      logging = helpers.defaultNullOpts.mkBool true "If logging should be enabled";
+      logLevel = helpers.defaultNullOpts.mkEnum ["DEBUG" "ERROR" "INFO" "TRACE" "WARN" "OFF"] "WARN" "The level to log at";
       filetype =
         helpers.defaultNullOpts.mkNullable
         (
@@ -37,18 +39,25 @@ in
     };
 
     config = let
-      setupOptions.filetype =
-        mapAttrs (
-          filetype: formatters:
-            map (
-              formatter:
-                if (builtins.typeOf formatter) == "set"
-                then formatter
-                else {__raw = "require(\"formatter.filetypes.${elemAt (strings.splitString "." formatter) 0}\").${elemAt (strings.splitString "." formatter) 1}";}
-            )
-            formatters
-        )
-        cfg.filetype;
+      setupOptions = {
+        filetype =
+          mapAttrs (
+            filetype: formatters:
+              map (
+                formatter:
+                  if (builtins.typeOf formatter) == "set"
+                  then formatter
+                  else {__raw = "require(\"formatter.filetypes.${elemAt (strings.splitString "." formatter) 0}\").${elemAt (strings.splitString "." formatter) 1}";}
+              )
+              formatters
+          )
+          cfg.filetype;
+        inherit (cfg) logging;
+        log_level.__raw =
+          if (cfg.log_level == null)
+          then null
+          else "vim.log.levels.${cfg.log_level}";
+      };
       packages = lists.unique (
         concatMap (
           formatters:
