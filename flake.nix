4d497915bba1985622cd7eae324e69540e5ff9be
Gaetan Lepage <gaetan@glepage.com>
2023-11-29 22:56:02 +0100

    flake.nix: follow the nixos-23.11 channel

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     beautysh = {
       url = "github:lovesegfault/beautysh";
