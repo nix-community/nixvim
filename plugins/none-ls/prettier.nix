07ff3e1b1a226f8e5adf6a59d73c0eceadd2c4c8
Gaetan Lepage <gaetan@glepage.com>
2023-12-02 17:59:09 +0100

    plugins/none-ls: add disableTsServerFormatter option

---
+++ plugins/none-ls/prettier.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.plugins.none-ls.sources.formatting.prettier;
+  tsserver-cfg = config.plugins.lsp.servers.tsserver;
+in {
+  options.plugins.none-ls.sources.formatting.prettier = {
+    disableTsServerFormatter = mkOption {
+      type = with types; nullOr bool;
+      description = ''
+        Disables the formatting capability of the `tsserver` language server if it is enabled.
+      '';
+      default = null;
+      example = true;
+    };
+  };
+
+  config = {
+    warnings =
+      optional
+      (
+        (cfg.disableTsServerFormatter == null)
+        && tsserver-cfg.enable
+      )
+      ''
+        You have enabled the `prettier` formatter in none-ls.
+        You have also enabled the `tsserver` language server which also brings a formatting feature.
+
+        - To disable the formatter built-in the `tsserver` language server, set
+          `plugins.none-ls.sources.formatting.prettier.disableTsServerFormatter` to `true`.
+        - Else, to silence this warning, explicitly set the option to `false`.
+      '';
+
+    plugins.lsp.servers.tsserver =
+      mkIf
+      (
+        cfg.enable
+        && tsserver-cfg.enable
+        && (isBool cfg.disableTsServerFormatter)
+        && cfg.disableTsServerFormatter
+      )
+      {
+        onAttach.function = ''
+          client.server_capabilities.documentFormattingProvider = false
+        '';
+      };
+  };
+}
