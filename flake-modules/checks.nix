31284ddabe11097c965ad9e49ff6302bdc11dca5
Gaetan Lepage <gaetan@glepage.com>
2023-12-06 13:09:26 +0100

    flake.nix: refactoring using flake-parts

---
+++ flake-modules/checks.nix
@@ -0,0 +1,31 @@
+{
+  perSystem = {
+    pkgs,
+    makeNixvimWithModuleUnfree,
+    makeNixvimWithModule,
+    ...
+  }: {
+    checks = {
+      tests = import ../tests {
+        inherit pkgs;
+        inherit (pkgs) lib;
+        makeNixvim = configuration:
+          makeNixvimWithModuleUnfree {
+            module = {
+              config = configuration;
+            };
+          };
+      };
+
+      extra-args-tests = import ../tests/extra-args.nix {
+        inherit pkgs;
+        inherit makeNixvimWithModule;
+      };
+
+      lib-tests = import ../tests/lib-tests.nix {
+        inherit pkgs;
+        inherit (pkgs) lib;
+      };
+    };
+  };
+}
