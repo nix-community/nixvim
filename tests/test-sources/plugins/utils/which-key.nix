97eccf981b221b7833f57e33a8d291766cb9c65e
phieash <yaoshiu@qq.com>
2023-12-11 18:30:28 +0800

    plugins/which-key: relax the restrition on the type of registrations and more tests have been added

--- tests/test-sources/plugins/utils/which-key.nix
+++ tests/test-sources/plugins/utils/which-key.nix
@@ -7,11 +7,23 @@
     plugins.which-key = {
       enable = true;
 
-      # Simple mapping with only Description
-      registrations."ff" = "Test";
-
+      # Testing for registrations
       registrations."f" = {
+        prefix = "<leader>";
+        mode = ["n" "v" "i" "t" "c" "x" "s" "o"];
         name = "Group Test";
+        f = "Label Test";
+        "1" = [
+          {
+            __raw = ''
+              function()
+                print("Raw Lua Code and List KeyMapping Test")
+              end
+            '';
+          }
+          "Raw Lua Code and List KeyMapping Test"
+        ];
+        "oo" = "Label Test 2";
       };
 
       plugins = {
