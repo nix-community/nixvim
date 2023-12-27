02149dbb011a3fffd616b44ee3bb3f1685778f13
Gaetan Lepage <gaetan@glepage.com>
2023-12-27 17:02:05 +0100

    modules/options: add localOptions and globalOptions

--- modules/options.nix
+++ modules/options.nix
@@ -12,6 +12,18 @@ with lib; {
       description = "The configuration options, e.g. line numbers";
     };
 
+    globalOptions = mkOption {
+      type = types.attrsOf types.anything;
+      default = {};
+      description = "The configuration global options";
+    };
+
+    localOptions = mkOption {
+      type = types.attrsOf types.anything;
+      default = {};
+      description = "The configuration local options";
+    };
+
     globals = mkOption {
       type = types.attrsOf types.anything;
       default = {};
@@ -39,6 +51,28 @@ with lib; {
 
           for k,v in pairs(nixvim_options) do
             vim.opt[k] = v
+          end
+        end
+        -- }}}
+      ''
+      + optionalString (config.localOptions != {}) ''
+        -- Set up local options {{{
+        do
+          local nixvim_local_options = ${helpers.toLuaObject config.localOptions}
+
+          for k,v in pairs(nixvim_local_options) do
+            vim.opt_local[k] = v
+          end
+        end
+        -- }}}
+      ''
+      + optionalString (config.globalOptions != {}) ''
+        -- Set up global options {{{
+        do
+          local nixvim_global_options = ${helpers.toLuaObject config.globalOptions}
+
+          for k,v in pairs(nixvim_global_options) do
+            vim.opt_global[k] = v
           end
         end
         -- }}}
