f2e808a519d9526cf7df9047f20f71c323c21ac9
Gaetan Lepage <gaetan@glepage.com>
2023-12-30 23:50:12 +0100

    plugins: add a sensible template for modern plugins

---
+++ plugins/TEMPLATE.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  helpers,
+  config,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.plugins.my-plugin; # TODO replace
+in {
+  meta.maintainers = [maintainers.MyName]; # TODO replace with your name
+
+  # TODO replace
+  options.plugins.my-plugin =
+    helpers.extraOptionsOptions
+    // {
+      enable = mkEnableOption "my-plugin.nvim"; # TODO replace
+
+      package = helpers.mkPackageOption "my-plugin.nvim" pkgs.vimPlugins.my-plugin-nvim; # TODO replace
+    };
+
+  config = mkIf cfg.enable {
+    extraPlugins = [cfg.package];
+
+    extraConfigLua = let
+      setupOptions = with cfg;
+        {
+        }
+        // cfg.extraOptions;
+    in ''
+      require('my-plugin').setup(${helpers.toLuaObject setupOptions})
+    '';
+  };
+}
