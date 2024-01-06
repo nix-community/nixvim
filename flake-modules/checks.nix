782f2d6150c98f52533d261bc009fa76ac33251c
Gaetan Lepage <gaetan@glepage.com>
2024-01-06 15:39:36 +0100

    flake/packages: exclude docs from packages on darwin

--- flake-modules/checks.nix
+++ flake-modules/checks.nix
@@ -6,33 +6,28 @@
     makeNixvimWithModule,
     ...
   }: {
-    checks =
-      {
-        tests = import ../tests {
-          inherit pkgs;
-          inherit (pkgs) lib;
-          makeNixvim = configuration:
-            makeNixvimWithModuleUnfree {
-              module = {
-                config = configuration;
-              };
+    checks = {
+      tests = import ../tests {
+        inherit pkgs;
+        inherit (pkgs) lib;
+        makeNixvim = configuration:
+          makeNixvimWithModuleUnfree {
+            module = {
+              config = configuration;
             };
-        };
+          };
+      };
 
-        extra-args-tests = import ../tests/extra-args.nix {
-          inherit pkgs;
-          inherit makeNixvimWithModule;
-        };
+      extra-args-tests = import ../tests/extra-args.nix {
+        inherit pkgs;
+        inherit makeNixvimWithModule;
+      };
 
-        lib-tests = import ../tests/lib-tests.nix {
-          inherit pkgs;
-          inherit (pkgs) lib;
-        };
-      }
-      # Do not check if documentation builds fine on darwin as it fails:
-      # > sandbox-exec: pattern serialization length 69298 exceeds maximum (65535)
-      // (pkgs.lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
-        inherit (config.packages) docs;
-      });
+      lib-tests = import ../tests/lib-tests.nix {
+        inherit pkgs;
+        inherit (pkgs) lib;
+      };
+      docs = config.packages.docs ? null;
+    };
   };
 }
