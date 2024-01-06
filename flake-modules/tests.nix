245930d735a1fa391837d1fa648cfbfb9faf73f3
traxys <quentin+dev@familleboyer.net>
2024-01-06 17:53:31 +0100

    tests: Add a test for the nixos module (#907)

--- flake-modules/tests.nix
+++ flake-modules/tests.nix
@@ -6,39 +6,53 @@
   perSystem = {
     pkgs,
     config,
+    system,
     makeNixvimWithModuleUnfree,
     makeNixvimWithModule,
     ...
   }: {
-    checks = {
-      tests = import ../tests {
-        inherit pkgs;
-        inherit (pkgs) lib;
-        makeNixvim = configuration:
-          makeNixvimWithModuleUnfree {
-            module = {
-              config = configuration;
-            };
-          };
-      };
-
-      extra-args-tests = import ../tests/extra-args.nix {
-        inherit pkgs;
-        inherit makeNixvimWithModule;
-      };
-
-      lib-tests = import ../tests/lib-tests.nix {
-        inherit pkgs;
-        inherit (pkgs) lib;
-      };
-
-      home-manager =
-        (import ../tests/modules/hm.nix {
+    checks =
+      {
+        tests = import ../tests {
           inherit pkgs;
-          inherit (inputs) home-manager;
-          nixvim = self;
-        })
-        .activationPackage;
-    };
+          inherit (pkgs) lib;
+          makeNixvim = configuration:
+            makeNixvimWithModuleUnfree {
+              module = {
+                config = configuration;
+              };
+            };
+        };
+
+        extra-args-tests = import ../tests/extra-args.nix {
+          inherit pkgs;
+          inherit makeNixvimWithModule;
+        };
+
+        lib-tests = import ../tests/lib-tests.nix {
+          inherit pkgs;
+          inherit (pkgs) lib;
+        };
+
+        home-manager-module =
+          (import ../tests/modules/hm.nix {
+            inherit pkgs;
+            inherit (inputs) home-manager;
+            nixvim = self;
+          })
+          .activationPackage;
+      }
+      // pkgs.lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+        nixos-module =
+          (import ../tests/modules/nixos.nix {
+            inherit system;
+            inherit (inputs) nixpkgs;
+            nixvim = self;
+          })
+          .config
+          .system
+          .build
+          .toplevel;
+      };
   };
 }
