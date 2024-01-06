fb45e36f5eb0228fa09428180e2792e3af7e4b55
traxys <quentin+dev@familleboyer.net>
2024-01-06 17:31:35 +0100

    tests: Add a test for the nixos module

---
+++ tests/modules/nixos.nix
@@ -0,0 +1,21 @@
+{
+  nixvim,
+  nixpkgs,
+  system,
+}:
+nixpkgs.lib.nixosSystem {
+  inherit system;
+
+  modules = [
+    {
+      system.stateVersion = "23.11";
+      boot.loader.systemd-boot.enable = true;
+      fileSystems."/" = {device = "/non/existent/device";};
+
+      programs.nixvim = {
+        enable = true;
+      };
+    }
+    nixvim.nixosModules.nixvim
+  ];
+}
