b96b52068a7bf827e9a9158066ae9e7a54e29162
Gaetan Lepage <gaetan@glepage.com>
2024-01-08 08:30:29 +0100

    plugins/nvim-cmp: fix example for mappings option

--- plugins/completion/nvim-cmp/default.nix
+++ plugins/completion/nvim-cmp/default.nix
@@ -82,25 +82,19 @@ in {
             )
           );
         example = {
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.close()";
           "<Tab>" = {
             modes = ["i" "s"];
-            action = ''
-              function(fallback)
-                if cmp.visible() then
-                  cmp.select_next_item()
-                elseif luasnip.expandable() then
-                  luasnip.expand()
-                elseif luasnip.expand_or_jumpable() then
-                  luasnip.expand_or_jump()
-                elseif check_backspace() then
-                  fallback()
-                else
-                  fallback()
-                end
-              end
-            '';
+            action = "cmp.mapping.select_next_item()";
           };
+          "<S-Tab>" = {
+            modes = ["i" "s"];
+            action = "cmp.mapping.select_prev_item()";
+          };
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
         };
       };
 
