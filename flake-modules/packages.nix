6ffa7059ebe2d2919e554f5f07eb74a904e273c4
traxys <quentin+dev@familleboyer.net>
2024-01-12 00:03:35 +0100

    docs: Correctly generate documentation for types `either a (submodule ...)`
    This requires pulling quite a lot of code from nixpkgs in order to
    make the `either` type work correctly, the effects can be seen for
    example in the documentation of `ollama.promps` and `ollama.actions`

--- flake-modules/packages.nix
+++ flake-modules/packages.nix
@@ -1,27 +1,14 @@
 {
   perSystem = {
     pkgs,
-    pkgsUnfree,
     config,
     modules,
     ...
   }: {
-    packages = let
-      # Do not check if documentation builds fine on darwin as it fails:
-      # > sandbox-exec: pattern serialization length 69298 exceeds maximum (65535)
-      docs = pkgs.lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
-        docs = pkgsUnfree.callPackage (import ../docs) {
-          inherit modules;
-        };
-      };
-
-      man-docs = import ../man-docs {
-        pkgs = pkgsUnfree;
-        inherit modules;
-      };
-    in
-      docs
-      // man-docs;
+    packages = import ../docs {
+      inherit modules pkgs;
+      inherit (pkgs) lib;
+    };
 
     # Test that all packages build fine when running `nix flake check`.
     checks = config.packages;
