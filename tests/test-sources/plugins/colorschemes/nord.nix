ae57ceaaf1a4db456b5019020ae2e7b162a7b603
Gaetan Lepage <gaetan@glepage.com>
2024-01-16 00:17:28 +0100

    colorschemes/nord: fix

---
+++ tests/test-sources/plugins/colorschemes/nord.nix
@@ -0,0 +1,20 @@
+{
+  empty = {
+    colorschemes.nord.enable = true;
+  };
+
+  defaults = {
+    colorschemes.nord = {
+      enable = true;
+
+      contrast = false;
+      borders = false;
+      disableBackground = false;
+      cursorlineTransparent = false;
+      enableSidebarBackground = false;
+      italic = true;
+      uniformDiffBackground = false;
+      extraConfig = {};
+    };
+  };
+}
