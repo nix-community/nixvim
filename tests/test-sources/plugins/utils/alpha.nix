66a2fb66b29aae0b0c70bddd4bdea291efac64b1
Gaetan Lepage <gaetan@glepage.com>
2024-01-18 00:04:49 +0100

    plugins/alpha: fix example (for buttons)

--- tests/test-sources/plugins/utils/alpha.nix
+++ tests/test-sources/plugins/utils/alpha.nix
@@ -46,12 +46,16 @@
           type = "group";
           val = [
             {
+              type = "button";
               val = "  New file";
               on_press.__raw = "function() vim.cmd[[ene]] end";
+              opts.shortcut = "n";
             }
             {
+              type = "button";
               val = " Quit Neovim";
               on_press.__raw = "function() vim.cmd[[qa]] end";
+              opts.shortcut = "q";
             }
           ];
         }
