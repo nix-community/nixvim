7643a0d4f3530f02da29c8373075ec69e034c7c8
Gaetan Lepage <gaetan@glepage.com>
2024-01-25 16:17:36 +0100

    lib/helpers: init neovim-plugin.mkSetupOptionsOption

--- lib/neovim-plugin.nix
+++ lib/neovim-plugin.nix
@@ -3,6 +3,27 @@
   nixvimOptions,
 }:
 with lib; {
+  mkSetupOptionsOption = pluginName: options: {
+    type = with types;
+      submodule {
+        freeformType = with types; attrsOf anything;
+        inherit options;
+      };
+    description = ''
+      Options provided to the `require('${pluginName}').setup` function.
+    '';
+    default = {};
+    example = {
+      foo_bar = 42;
+      hostname = "localhost:8080";
+      callback.__raw = ''
+        function()
+          print('nixvim')
+        end
+      '';
+    };
+  };
+
   extraOptionsOptions = {
     extraOptions = mkOption {
       default = {};
