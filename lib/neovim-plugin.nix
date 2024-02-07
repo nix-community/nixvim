2e18333dd2c696241cde0f6aa0632738dff10c15
Gaetan Lepage <gaetan@glepage.com>
2024-02-07 14:21:11 +0100

    helpers/neovim-plugin/mkSettingsOption: fix typo

--- lib/neovim-plugin.nix
+++ lib/neovim-plugin.nix
@@ -3,26 +3,27 @@
   nixvimOptions,
 }:
 with lib; {
-  mkSettingsOption = pluginName: options: {
-    type = with types;
-      submodule {
-        freeformType = with types; attrsOf anything;
-        inherit options;
-      };
-    description = ''
-      Options provided to the `require('${pluginName}').setup` function.
-    '';
-    default = {};
-    example = {
-      foo_bar = 42;
-      hostname = "localhost:8080";
-      callback.__raw = ''
-        function()
-          print('nixvim')
-        end
+  mkSettingsOption = pluginName: options:
+    mkOption {
+      type = with types;
+        submodule {
+          freeformType = with types; attrsOf anything;
+          inherit options;
+        };
+      description = ''
+        Options provided to the `require('${pluginName}').setup` function.
       '';
+      default = {};
+      example = {
+        foo_bar = 42;
+        hostname = "localhost:8080";
+        callback.__raw = ''
+          function()
+            print('nixvim')
+          end
+        '';
+      };
     };
-  };
 
   extraOptionsOptions = {
     extraOptions = mkOption {
