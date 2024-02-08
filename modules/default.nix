eec8d6b1b2eea3d492c3df6ce90ada1d4adf2ae8
Gaetan Lepage <gaetan@glepage.com>
2024-02-08 14:14:25 +0100

    misc: import nixvim modules in a simpler way

---
+++ modules/default.nix
@@ -0,0 +1,18 @@
+{
+  imports = [
+    ./autocmd.nix
+    ./clipboard.nix
+    ./colorscheme.nix
+    ./commands.nix
+    ./doc.nix
+    ./editorconfig.nix
+    ./filetype.nix
+    ./highlights.nix
+    ./keymaps.nix
+    ./lua-loader.nix
+    ./options.nix
+    ./output.nix
+    ./plugins.nix
+    ./warnings.nix
+  ];
+}
