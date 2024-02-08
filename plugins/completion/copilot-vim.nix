b30e8a1eda5803e46dd86d75d21d2a6e2c7b1004
Gaetan Lepage <gaetan@glepage.com>
2024-02-08 16:18:40 +0100

    plugins/copilot-vim: remove deprecation warning

--- plugins/completion/copilot-vim.nix
+++ plugins/completion/copilot-vim.nix
@@ -3,46 +3,40 @@
   pkgs,
   ...
 } @ args:
-with lib;
-  (
-    with (import ../helpers.nix {inherit lib;}).vim-plugin;
-      mkVimPlugin args {
-        name = "copilot-vim";
-        description = "copilot.vim";
-        package = pkgs.vimPlugins.copilot-vim;
-        globalPrefix = "copilot_";
+with lib; (
+  with (import ../helpers.nix {inherit lib;}).vim-plugin;
+    mkVimPlugin args {
+      name = "copilot-vim";
+      description = "copilot.vim";
+      package = pkgs.vimPlugins.copilot-vim;
+      globalPrefix = "copilot_";
 
-        options = {
-          nodeCommand = mkDefaultOpt {
-            global = "node_command";
-            type = types.str;
-            default = "${pkgs.nodejs-18_x}/bin/node";
-            description = "Tell Copilot what `node` binary to use.";
-          };
+      options = {
+        nodeCommand = mkDefaultOpt {
+          global = "node_command";
+          type = types.str;
+          default = "${pkgs.nodejs-18_x}/bin/node";
+          description = "Tell Copilot what `node` binary to use.";
+        };
 
-          filetypes = mkDefaultOpt {
-            type = with types; attrsOf bool;
-            description = ''
-              A dictionary mapping file types to their enabled status
+        filetypes = mkDefaultOpt {
+          type = with types; attrsOf bool;
+          description = ''
+            A dictionary mapping file types to their enabled status
 
-              Default: `{}`
-            '';
-            example = {
-              "*" = false;
-              python = true;
-            };
-          };
-
-          proxy = mkDefaultOpt {
-            type = types.str;
-            description = "Tell Copilot what proxy server to use.";
-            example = "localhost:3128";
+            Default: `{}`
+          '';
+          example = {
+            "*" = false;
+            python = true;
           };
         };
-      }
-  )
-  // {
-    imports = [
-      (lib.mkRenamedOptionModule ["plugins" "copilot"] ["plugins" "copilot-vim"])
-    ];
-  }
+
+        proxy = mkDefaultOpt {
+          type = types.str;
+          description = "Tell Copilot what proxy server to use.";
+          example = "localhost:3128";
+        };
+      };
+    }
+)
