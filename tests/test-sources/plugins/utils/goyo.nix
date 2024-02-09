a5dd578a2925b0adc5e4e8bc729272c1dd595b03
Gaetan Lepage <gaetan@glepage.com>
2024-02-09 15:23:45 +0100

    tests/goyo: init

---
+++ tests/test-sources/plugins/utils/goyo.nix
@@ -0,0 +1,15 @@
+{
+  empty = {
+    plugins.goyo.enable = true;
+  };
+
+  defaults = {
+    plugins.goyo = {
+      enable = true;
+
+      width = 80;
+      height = 85;
+      showLineNumbers = false;
+    };
+  };
+}
