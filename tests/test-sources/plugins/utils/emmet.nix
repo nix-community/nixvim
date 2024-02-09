e2bd02afd4e420b134b208363a9c1a1eebfc873a
Gaetan Lepage <gaetan@glepage.com>
2024-02-09 15:16:17 +0100

    tests/emmet: init

---
+++ tests/test-sources/plugins/utils/emmet.nix
@@ -0,0 +1,43 @@
+{
+  empty = {
+    plugins.emmet.enable = true;
+  };
+
+  example = {
+    plugins.emmet = {
+      enable = true;
+
+      mode = "inv";
+      leader = "<C-Z>";
+      settings = {
+        variables = {
+          lang = "ja";
+        };
+        html = {
+          default_attributes = {
+            option = {value = null;};
+            textarea = {
+              id = null;
+              name = null;
+              cols = 10;
+              rows = 10;
+            };
+          };
+          snippets = {
+            "html:5" = ''
+              <!DOCTYPE html>
+              <html lang=\"$\{lang}\">
+              <head>
+              \t<meta charset=\"$\{charset}\">
+              \t<title></title>
+              \t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+              </head>
+              <body>\n\t$\{child}|\n</body>
+              </html>
+            '';
+          };
+        };
+      };
+    };
+  };
+}
