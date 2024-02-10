3f12558d495ac34ee053b6c778f60f921de95dd8
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-02-10 17:04:22 +0000

    plugins/committia: init

---
+++ tests/test-sources/plugins/git/committia.nix
@@ -0,0 +1,22 @@
+{
+  empty = {
+    plugins.committia.enable = true;
+  };
+
+  default = {
+    plugins.committia = {
+      enable = true;
+
+      settings = {
+        open_only_vim_starting = true;
+        use_singlecolumn = "always";
+        min_window_width = 160;
+        status_window_opencmd = "belowright split";
+        diff_window_opencmd = "botright vsplit";
+        singlecolumn_diff_window_opencmd = "belowright split";
+        edit_window_width = 80;
+        status_window_min_height = 0;
+      };
+    };
+  };
+}
