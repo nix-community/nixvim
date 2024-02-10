f3af01a1544fe095f752b1a3d4bf73a55cddc809
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-02-10 17:03:54 +0000

    Add maintainers entry for Alison Jenkins

--- lib/maintainers.nix
+++ lib/maintainers.nix
@@ -5,4 +5,11 @@
 # expected to follow the same format as that list.
 #
 # Nixpkgs maintainers: https://github.com/NixOS/nixpkgs/blob/0212bde005b3335b2665c1476c36b3936e113b15/maintainers/maintainer-list.nix
-{}
+{
+  alisonjenkins = {
+    email = "alison.juliet.jenkins@gmail.com";
+    github = "alisonjenkins";
+    githubId = 1176328;
+    name = "Alison Jenkins";
+  };
+}
