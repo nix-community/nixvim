0f3a542a981feefd386acfec37c32f883856b5c1
Gaetan Lepage <gaetan@glepage.com>
2024-02-14 11:04:31 +0100

    colorschemes/gruvbox: use simpler syntax

--- plugins/colorschemes/gruvbox.nix
+++ plugins/colorschemes/gruvbox.nix
@@ -4,63 +4,57 @@
   config,
   pkgs,
   ...
-}: {
+}:
+helpers.neovim-plugin.mkNeovimPlugin config {
+  name = "gruvbox";
+  namespace = "colorschemes";
+  originalName = "gruvbox.nvim";
+  defaultPackage = pkgs.vimPlugins.gruvbox-nvim;
+
+  maintainers = [lib.maintainers.GaetanLepage];
+
+  # Introduced January 31 2024
+  # TODO remove in early March 2024.
   imports =
+    map
+    (
+      optionName:
+        lib.mkRemovedOptionModule
+        ["colorschemes" "gruvbox" optionName]
+        "Please use `colorschemes.gruvbox.settings.${helpers.toSnakeCase optionName}` instead."
+    )
     [
-      (
-        helpers.neovim-plugin.mkNeovimPlugin config {
-          name = "gruvbox";
-          namespace = "colorschemes";
-          originalName = "gruvbox.nvim";
-          defaultPackage = pkgs.vimPlugins.gruvbox-nvim;
+      "italics"
+      "bold"
+      "underline"
+      "undercurl"
+      "contrastDark"
+      "contrastLight"
+      "highlightSearchCursor"
+      "numberColumn"
+      "signColumn"
+      "colorColumn"
+      "vertSplitColor"
+      "italicizeComments"
+      "italicizeStrings"
+      "invertSelection"
+      "invertSigns"
+      "invertIndentGuides"
+      "invertTabline"
+      "improvedStrings"
+      "improvedWarnings"
+      "transparentBg"
+      "trueColor"
+    ];
 
-          settingsExample = {
-            terminal_colors = true;
-            palette_overrides = {
-              dark1 = "#323232";
-              dark2 = "#383330";
-              dark3 = "#323232";
-              bright_blue = "#5476b2";
-              bright_purple = "#fb4934";
-            };
-          };
-
-          maintainers = [lib.maintainers.GaetanLepage];
-        }
-      )
-    ]
-    # Introduced January 31 2024
-    # TODO remove in early March 2024.
-    ++ (
-      map
-      (
-        optionName:
-          lib.mkRemovedOptionModule
-          ["colorschemes" "gruvbox" optionName]
-          "Please use `colorschemes.gruvbox.settings.${helpers.toSnakeCase optionName}` instead."
-      )
-      [
-        "italics"
-        "bold"
-        "underline"
-        "undercurl"
-        "contrastDark"
-        "contrastLight"
-        "highlightSearchCursor"
-        "numberColumn"
-        "signColumn"
-        "colorColumn"
-        "vertSplitColor"
-        "italicizeComments"
-        "italicizeStrings"
-        "invertSelection"
-        "invertSigns"
-        "invertIndentGuides"
-        "invertTabline"
-        "improvedStrings"
-        "improvedWarnings"
-        "transparentBg"
-        "trueColor"
-      ]
-    );
+  settingsExample = {
+    terminal_colors = true;
+    palette_overrides = {
+      dark1 = "#323232";
+      dark2 = "#383330";
+      dark3 = "#323232";
+      bright_blue = "#5476b2";
+      bright_purple = "#fb4934";
+    };
+  };
 }
