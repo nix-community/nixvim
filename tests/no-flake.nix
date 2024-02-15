dcbe894bea41567e4d751367666b249cfd489704
traxys <quentin+dev@familleboyer.net>
2024-02-15 22:51:26 +0100

    Fix issue with non flake import (#1102)
    * Update flake compat
    
    * tests: Add a test without flakes

---
+++ tests/no-flake.nix
@@ -0,0 +1,32 @@
+{
+  system,
+  nixvim,
+  mkTestDerivationFromNvim,
+}: let
+  # This simulates the `default.nix`, but in a pure way so because we are currently in a flake.
+  nixvim' =
+    (
+      import
+      (
+        let
+          lock = builtins.fromJSON (builtins.readFile ../flake.lock);
+        in
+          fetchTarball {
+            url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+            sha256 = lock.nodes.flake-compat.locked.narHash;
+          }
+      )
+      {src = nixvim;}
+    )
+    .defaultNix;
+
+  config = {
+    colorschemes.gruvbox.enable = true;
+  };
+
+  nvim = nixvim'.legacyPackages."${system}".makeNixvim config;
+in
+  mkTestDerivationFromNvim {
+    name = "no-flakes";
+    inherit nvim;
+  }
