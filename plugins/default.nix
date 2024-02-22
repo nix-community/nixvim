b871d2a827c06a09eed6d5a8dbcbb0e96954c213
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-02-22 07:55:43 +0000

    plugins/virt-column: init

--- plugins/default.nix
+++ plugins/default.nix
@@ -109,6 +109,7 @@
     ./ui/image.nix
     ./ui/noice.nix
     ./ui/transparent.nix
+    ./ui/virt-column.nix
 
     ./utils/alpha.nix
     ./utils/auto-save.nix
---
+++ tests/test-sources/plugins/ui/virt-column.nix
@@ -0,0 +1,47 @@
+{
+  empty = {
+    plugins.virt-column.enable = true;
+  };
+
+  defaults = {
+    plugins.virt-column = {
+      enable = true;
+
+      settings = {
+        enabled = true;
+        char = "┃";
+        virtcolumn = "";
+        highlight = "NonText";
+        exclude = {
+          filetypes = [
+            "lspinfo"
+            "packer"
+            "checkhealth"
+            "help"
+            "man"
+            "TelescopePrompt"
+            "TelescopeResults"
+          ];
+          buftypes = [
+            "nofile"
+            "quickfix"
+            "terminal"
+            "prompt"
+          ];
+        };
+      };
+    };
+  };
+
+  example = {
+    plugins.virt-column = {
+      enable = true;
+
+      settings = {
+        char = ["#" "!"];
+        virtcolumn = "80,90,100";
+        highlight = "NonText";
+      };
+    };
+  };
+}
