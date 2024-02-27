db0dcc6ee32708b2a391d1d5d5b855b5c6ed5b06
Gaétan Lepage <33058747+GaetanLepage@users.noreply.github.com>
2024-02-27 18:13:04 +0100

    plugins/bacon: formatting (#1164)

--- plugins/utils/bacon.nix
+++ plugins/utils/bacon.nix
@@ -9,10 +9,16 @@ helpers.neovim-plugin.mkNeovimPlugin config
   name = "bacon";
   defaultPackage = pkgs.vimPlugins.nvim-bacon;
   maintainers = [helpers.maintainers.alisonjenkins];
+
   settingsOptions = {
     quickfix = {
-      enabled = helpers.defaultNullOpts.mkBool true "true to populate the quickfix list with bacon errors and warnings.";
-      event_trigger = helpers.defaultNullOpts.mkBool true "triggers the QuickFixCmdPost event after populating the quickfix list";
+      enabled = helpers.defaultNullOpts.mkBool true ''
+        Whether to populate the quickfix list with bacon errors and warnings.
+      '';
+
+      event_trigger = helpers.defaultNullOpts.mkBool true ''
+        Triggers the `QuickFixCmdPost` event after populating the quickfix list.
+      '';
     };
   };
 
