9d6381e12efbb329c277766c9f545782c0a1a711
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2024-03-01 15:56:37 -0800

    Fix withArgs type
    This was previously null or string, but was changed to null or Lua in
    nix-community/nixvim#1169. Internally, `withArgs` is expected to be a
    string, but `mkNullOrLua` applies `mkRaw`, leading to a type error when
    generating the output, as it is attempting to interpolate a set rather
    than a string.
    
    Tested locally, and verified that it fixes my setup.

--- plugins/none-ls/servers.nix
+++ plugins/none-ls/servers.nix
@@ -331,7 +331,7 @@ in {
         (source: _:
           {
             enable = mkEnableOption "the ${source} ${sourceType} source for none-ls";
-            withArgs = helpers.mkNullOrLua ''
+            withArgs = helpers.mkNullOrStr ''
               Raw Lua code passed as an argument to the source's `with` method.
             '';
           }
