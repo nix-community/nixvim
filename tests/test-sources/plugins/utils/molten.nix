d452c45aba630003bc5fcf2aec2cc74cef2e2cb3
Gaetan Lepage <gaetan@glepage.com>
2024-03-01 21:26:23 +0100

    plugins/molten: switch to settings option

--- tests/test-sources/plugins/utils/molten.nix
+++ tests/test-sources/plugins/utils/molten.nix
@@ -7,24 +7,61 @@
     plugins.molten = {
       enable = true;
 
-      autoOpenOutput = true;
-      copyOutput = false;
-      enterOutputBehavior = "open_then_enter";
-      imageProvider = "none";
-      outputCropBorder = true;
-      outputShowMore = false;
-      outputVirtLines = false;
-      outputWinBorder = ["" "━" "" ""];
-      outputWinCoverGutter = true;
-      outputWinHideOnLeave = true;
-      outputWinMaxHeight = 999999;
-      outputWinMaxWidth = 999999;
-      outputWinStyle = false;
-      savePath.__raw = "vim.fn.stdpath('data')..'/molten'";
-      useBorderHighlights = false;
-      virtLinesOffBy1 = false;
-      wrapOutput = false;
-      showMimetypeDebug = false;
+      settings = {
+        auto_image_popup = false;
+        auto_init_behavior = "init";
+        auto_open_html_in_browser = false;
+        auto_open_output = true;
+        cover_empty_lines = false;
+        cover_lines_starting_with = [];
+        copy_output = false;
+        enter_output_behavior = "open_then_enter";
+        image_provider = "none";
+        open_cmd = null;
+        output_crop_border = true;
+        output_show_more = false;
+        output_virt_lines = false;
+        output_win_border = ["" "━" "" ""];
+        output_win_cover_gutter = true;
+        output_win_hide_on_leave = true;
+        output_win_max_height = 999999;
+        output_win_max_width = 999999;
+        output_win_style = false;
+        save_path.__raw = "vim.fn.stdpath('data')..'/molten'";
+        tick_rate = 500;
+        use_border_highlights = false;
+        limit_output_chars = 1000000;
+        virt_lines_off_by_1 = false;
+        virt_text_output = false;
+        virt_text_max_lines = 12;
+        wrap_output = false;
+        show_mimetype_debug = false;
+      };
+    };
+  };
+
+  example = {
+    plugins.molten = {
+      enable = true;
+
+      settings = {
+        auto_open_output = true;
+        copy_output = false;
+        enter_output_behavior = "open_then_enter";
+        image_provider = "none";
+        output_crop_border = true;
+        output_show_more = false;
+        output_virt_lines = false;
+        output_win_border = ["" "━" "" ""];
+        output_win_cover_gutter = true;
+        output_win_hide_on_leave = true;
+        output_win_style = false;
+        save_path.__raw = "vim.fn.stdpath('data')..'/molten'";
+        use_border_highlights = false;
+        virt_lines_off_by1 = false;
+        wrap_output = false;
+        show_mimetype_debug = false;
+      };
     };
   };
 }
