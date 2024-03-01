10fa9872502d7eaadbbee3906f635b66e4ecd824
Gaetan Lepage <gaetan@glepage.com>
2024-03-01 17:23:00 +0100

    plugins/startify: add tests

---
+++ tests/test-sources/plugins/utils/startify.nix
@@ -0,0 +1,91 @@
+{
+  empty = {
+    plugins.startify.enable = true;
+  };
+
+  example = {
+    plugins.startify = {
+      enable = true;
+
+      settings = {
+        session_dir = "~/.vim/session";
+        lists = [
+          {
+            type = "files";
+            header = ["   MRU"];
+          }
+          {
+            type = "dir";
+            header = [{__raw = "'   MRU' .. vim.loop.cwd()";}];
+          }
+          {
+            type = "sessions";
+            header = ["   Sessions"];
+          }
+          {
+            type = "bookmarks";
+            header = ["   Bookmarks"];
+          }
+          {
+            type = "commands";
+            header = ["   Commands"];
+          }
+        ];
+        commands = [
+          ":help reference"
+          ["Vim Reference" "h ref"]
+          {h = "h ref";}
+          {m = ["My magical function" "call Magic()"];}
+        ];
+        files_number = 10;
+        update_oldfiles = false;
+        session_autoload = false;
+        session_before_save = ["silent! tabdo NERDTreeClose"];
+        session_persistence = false;
+        session_delete_buffers = true;
+        change_to_dir = true;
+        change_to_vcs_root = false;
+        change_cmd = "lcd";
+        skiplist = [
+          "\.vimgolf"
+          "^/tmp"
+          "/project/.*/documentation"
+        ];
+        fortune_use_unicode = false;
+        padding_left = 3;
+        skiplist_server = ["GVIM"];
+        enable_special = true;
+        enable_unsafe = false;
+        session_remove_lines = ["setlocal" "winheight"];
+        session_savevars = [
+          "g:startify_session_savevars"
+          "g:startify_session_savecmds"
+          "g:random_plugin_use_feature"
+        ];
+        session_savecmds = [
+          "silent !pdfreader ~/latexproject/main.pdf &"
+        ];
+        session_number = 999;
+        session_sort = false;
+        custom_indices = ["f" "g" "h"];
+        custom_header = [
+          ""
+          "     ███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗"
+          "     ████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║"
+          "     ██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║"
+          "     ██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║"
+          "     ██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║"
+          "     ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝"
+        ];
+        custom_header_quotes = [
+          ["quote #1"]
+          ["quote #2" "using" "three lines"]
+        ];
+        custom_footer = null;
+        disable_at_vimenter = false;
+        relative_path = false;
+        use_env = false;
+      };
+    };
+  };
+}
