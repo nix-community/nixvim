60c69121260cca724e511725f8e1234b9c32be41
Gaetan Lepage <gaetan@glepage.com>
2024-03-02 23:41:38 +0100

    plugins/zig: switch to settings option

--- plugins/languages/zig.nix
+++ plugins/languages/zig.nix
@@ -12,14 +12,25 @@ with helpers.vim-plugin;
     originalName = "zig.vim";
     defaultPackage = pkgs.vimPlugins.zig-vim;
     globalPrefix = "zig_";
-    deprecateExtraConfig = true;
 
-    # Possibly add option to disable Treesitter highlighting if this is installed
-    options = {
-      formatOnSave = mkDefaultOpt {
-        type = types.bool;
-        global = "fmt_autosave";
-        description = "Run zig fmt on save";
-      };
+    # TODO introduced 2024-03-02: remove 2024-05-02
+    deprecateExtraConfig = true;
+    imports = [
+      (
+        mkRenamedOptionModule
+        ["plugins" "zig" "formatOnSave"]
+        ["plugins" "zig" "settings" "fmt_autosave"]
+      )
+    ];
+
+    settingsOptions = {
+      fmt_autosave = helpers.defaultNullOpts.mkBool true ''
+        This plugin enables automatic code formatting on save by default using zig fmt.
+        To disable it, you can set this option to `false`.
+      '';
+    };
+
+    settingsExample = {
+      fmt_autosave = false;
     };
   }
