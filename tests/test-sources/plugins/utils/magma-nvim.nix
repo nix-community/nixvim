51765fedbe15ee5920025b7b6772f24aa644e50b
Gaetan Lepage <gaetan@glepage.com>
2024-03-02 19:11:05 +0100

    plugins/magma-nvim: switch to settings option

--- tests/test-sources/plugins/utils/magma-nvim.nix
+++ tests/test-sources/plugins/utils/magma-nvim.nix
@@ -7,13 +7,15 @@
     plugins.magma-nvim = {
       enable = true;
 
-      imageProvider = "none";
-      automaticallyOpenOutput = true;
-      wrapOutput = true;
-      outputWindowBorders = true;
-      cellHighlightGroup = "CursorLine";
-      savePath = null;
-      showMimetypeDebug = false;
+      settings = {
+        image_provider = "none";
+        automatically_open_output = true;
+        wrap_output = true;
+        output_window_borders = true;
+        cell_highlight_group = "CursorLine";
+        save_path.__raw = "vim.fn.stdpath('data') .. '/magma'";
+        show_mimetype_debug = false;
+      };
     };
   };
 }
