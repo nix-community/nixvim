a6bf19966d03b9eb85d06a6f3ccfd447c501c39d
Gaetan Lepage <gaetan@glepage.com>
2024-03-02 22:58:22 +0100

    plugins/tagbar: add settingsExample

--- plugins/languages/tagbar.nix
+++ plugins/languages/tagbar.nix
@@ -8,6 +8,22 @@ helpers.vim-plugin.mkVimPlugin config {
   name = "tagbar";
   defaultPackage = pkgs.vimPlugins.tagbar;
   globalPrefix = "tagbar_";
-  deprecateExtraConfig = true;
   extraPackages = [pkgs.ctags];
+
+  # TODO introduced 2024-02-12: remove 2024-04-12
+  deprecateExtraConfig = true;
+
+  settingsExample = {
+    position = "right";
+    autoclose = false;
+    autofocus = false;
+    foldlevel = 2;
+    autoshowtag = true;
+    iconchars = ["" ""];
+    visibility_symbols = {
+      public = "󰡭 ";
+      protected = "󱗤 ";
+      private = "󰛑 ";
+    };
+  };
 }
