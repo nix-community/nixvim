fe3b2d5ff6e1ed1c8002dc8a1a03457e566f8ac5
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-03-03 18:30:10 +0000

    plugins/lightline: Improve types for active option of lightline

--- plugins/statuslines/lightline.nix
+++ plugins/statuslines/lightline.nix
@@ -55,19 +55,11 @@ in {
         default = null;
         type = types.nullOr (types.submodule {
           options = let
-            listType = with types; nullOr (listOf (listOf str));
+            listType = with helpers.nixvimTypes; maybeRaw (listOf (listOf str));
           in {
-            left = mkOption {
-              type = listType;
-              description = "List of components that will show up on the left side of the bar";
-              default = null;
-            };
+            left = helpers.mkNullOrOption listType "List of components that will show up on the left side of the bar";
 
-            right = mkOption {
-              type = listType;
-              description = "List of components that will show up on the right side of the bar";
-              default = null;
-            };
+            right = helpers.mkNullOrOption listType "List of components that will show up on the right side of the bar";
           };
         });
       };
