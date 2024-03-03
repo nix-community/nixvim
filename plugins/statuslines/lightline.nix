7dcd217ff37c2acd3c81fd21d75810e036c52721
Dyson <dyson.dyson@icloud.com>
2024-03-03 18:21:19 +0000

    plugins/lightline: Add option for inactive components (#1170)

--- plugins/statuslines/lightline.nix
+++ plugins/statuslines/lightline.nix
@@ -72,6 +72,19 @@ in {
         });
       };
 
+      inactive = mkOption {
+        default = null;
+        type = types.nullOr (types.submodule {
+          options = let
+            listType = with helpers.nixvimTypes; maybeRaw (listOf (listOf str));
+          in {
+            left = helpers.mkNullOrOption listType "List of components that will show up on the left side of the bar";
+
+            right = helpers.mkNullOrOption listType "List of components that will show up on the right side of the bar";
+          };
+        });
+      };
+
       modeMap = mkOption {
         type = with types; nullOr (attrsOf str);
         description = "Mode name mappings";
@@ -82,7 +95,7 @@ in {
 
   config = let
     configAttrs = filterAttrs (_: v: v != null) {
-      inherit (cfg) colorscheme active component componentFunction modeMap;
+      inherit (cfg) colorscheme active inactive component componentFunction modeMap;
     };
   in
     mkIf cfg.enable {
