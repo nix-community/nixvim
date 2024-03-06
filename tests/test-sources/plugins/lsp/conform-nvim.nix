2053b6da76874a8f3c5ec0a649b07f66c9bbfbd9
jonboh <jon.bosque.hernando@gmail.com>
2024-03-06 22:14:52 +0100

    plugins/conform: add str type for formatAfterSave

--- tests/test-sources/plugins/lsp/conform-nvim.nix
+++ tests/test-sources/plugins/lsp/conform-nvim.nix
@@ -94,4 +94,30 @@
       '';
     };
   };
+
+  custom_format_after_save_function = {
+    plugins.conform-nvim = {
+      enable = true;
+
+      formattersByFt = {
+        lua = ["stylua"];
+        python = ["isort" "black"];
+        javascript = [["prettierd" "prettier"]];
+        "*" = ["codespell"];
+        "_" = ["trimWhitespace"];
+      };
+
+      formatAfterSave = ''
+        function(bufnr)
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          if not _conform_slow_format_filetypes[vim.bo[bufnr].filetype] then
+            return
+          end
+          return { lsp_fallback = true }
+        end
+      '';
+    };
+  };
 }
