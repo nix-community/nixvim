e7b13db0d55aaf814eaa926474322a4881479b6c
Gaetan Lepage <gaetan@glepage.com>
2024-03-06 10:42:36 +0100

    plugins/TEMPLATE.nix: improve the template

--- plugins/TEMPLATE.nix
+++ plugins/TEMPLATE.nix
@@ -7,14 +7,25 @@
 }:
 helpers.neovim-plugin.mkNeovimPlugin config {
   name = "my-plugin";
-  originalName = "my-plugin.nvim";
+  originalName = "my-plugin.nvim"; # TODO replace (or remove entirely if it is the same as `name`)
   defaultPackage = pkgs.vimPlugins.my-plugin-nvim; # TODO replace
 
-  # Optionnally provide an example for the `settings` option.
+  maintainers = [lib.maintainers.MyName]; # TODO replace with your name
+
+  # Optionnally, explicitly declare some options. You don't have to.
+  settingsOptions = {
+    foo = helpers.defaultNullOpts.mkUnsignedInt 97 ''
+      The best birth year.
+    '';
+
+    great_feature = helpers.defaultNullOpts.mkBool false ''
+      Whether to enable the great feature.
+    '';
+  };
+
+  # Optionnally, provide an example for the `settings` option.
   settingsExample = {
     foo = 42;
     bar.__raw = "function() print('hello') end";
   };
-
-  maintainers = [lib.maintainers.MyName]; # TODO replace with your name
 }
