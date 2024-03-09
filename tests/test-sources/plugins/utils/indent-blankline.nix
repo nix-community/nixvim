cd3af8544e2e41bb945c414286e7f9655fdb7541
Gaetan Lepage <gaetan@glepage.com>
2024-03-10 00:44:02 +0100

    plugins/indent-blankline: swith to mkNeovimPlugin

--- tests/test-sources/plugins/utils/indent-blankline.nix
+++ tests/test-sources/plugins/utils/indent-blankline.nix
@@ -3,65 +3,97 @@
     plugins.indent-blankline.enable = true;
   };
 
+  example = {
+    plugins.indent-blankline = {
+      enable = true;
+
+      settings = {
+        indent = {
+          char = "│";
+        };
+        scope = {
+          show_start = false;
+          show_end = false;
+          show_exact_scope = true;
+        };
+        exclude = {
+          filetypes = [
+            ""
+            "checkhealth"
+            "help"
+            "lspinfo"
+            "packer"
+            "TelescopePrompt"
+            "TelescopeResults"
+            "yaml"
+          ];
+          buftypes = ["terminal" "quickfix"];
+        };
+      };
+    };
+  };
+
   defaults = {
     plugins.indent-blankline = {
       enable = true;
 
-      debounce = 200;
-      viewportBuffer = {
-        min = 30;
-        max = 500;
-      };
-      indent = {
-        char = "▎";
-        tabChar = null;
-        highlight = null;
-        smartIndentCap = true;
-        priority = 1;
-      };
-      whitespace = {
-        highlight = null;
-        removeBlanklineTrail = true;
-      };
-      scope = {
-        enabled = true;
-        char = null;
-        showStart = true;
-        showEnd = true;
-        showExactScope = false;
-        injectedLanguages = true;
-        highlight = null;
-        priority = 1024;
-        include = {
-          nodeType = {};
+      settings = {
+        debounce = 200;
+        viewport_buffer = {
+          min = 30;
+          max = 500;
         };
-        exclude = {
-          language = [];
-          nodeType = {
-            "*" = ["source_file" "program"];
-            lua = ["chunk"];
-            python = ["module"];
+        indent = {
+          char = "▎";
+          tab_char = null;
+          highlight = null;
+          smart_indent_cap = true;
+          priority = 1;
+        };
+        whitespace = {
+          highlight = null;
+          remove_blankline_trail = true;
+        };
+        scope = {
+          enabled = true;
+          char = null;
+          show_start = true;
+          show_end = true;
+          show_exact_scope = false;
+          injected_languages = true;
+          highlight = null;
+          priority = 1024;
+          include = {
+            node_type = {};
+          };
+          exclude = {
+            language = [];
+            node_type = {
+              "*" = ["source_file" "program"];
+              lua = ["chunk"];
+              python = ["module"];
+            };
           };
         };
-      };
-      exclude = {
-        filetypes = [
-          "lspinfo"
-          "packer"
-          "checkhealth"
-          "help"
-          "man"
-          "gitcommit"
-          "TelescopePrompt"
-          "TelescopeResults"
-          "\'\'"
-        ];
-        buftypes = [
-          "terminal"
-          "nofile"
-          "quickfix"
-          "prompt"
-        ];
+        exclude = {
+          filetypes = [
+            "lspinfo"
+            "packer"
+            "checkhealth"
+            "help"
+            "man"
+            "gitcommit"
+            "TelescopePrompt"
+            "TelescopeResults"
+            "\'\'"
+          ];
+          buftypes = [
+            "terminal"
+            "nofile"
+            "quickfix"
+            "prompt"
+          ];
+        };
       };
     };
   };
