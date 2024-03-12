42edf42cef7094112414c3751d8faa383af02ec7
traxys <quentin+dev@familleboyer.net>
2024-03-12 22:59:57 +0100

    plugin/coq-thirdparty: Clarify documentation of freeform sources

--- plugins/completion/coq-thirdparty.nix
+++ plugins/completion/coq-thirdparty.nix
@@ -34,7 +34,10 @@ in {
           };
         };
       });
-      description = "List of sources";
+      description = ''
+        List of sources. Each source is a free form type, so additional settings like `accept_key`
+        may be specified even if they are not declared by nixvim
+      '';
       default = [];
       example = [
         {
