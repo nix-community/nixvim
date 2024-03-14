bb10fface40bb2c5b3876e1e47add1fd84f4ce36
Loïc Reynier <88983487+loicreynier@users.noreply.github.com>
2024-03-14 14:55:40 +0100

    plugins/vscode: init (#1263)

---
+++ plugins/colorschemes/vscode.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  helpers,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+  helpers.neovim-plugin.mkNeovimPlugin config {
+    name = "vscode";
+    isColorscheme = true;
+    originalName = "vscode-nvim";
+    defaultPackage = pkgs.vimPlugins.vscode-nvim;
+    colorscheme = null; # Color scheme is set by `require.("vscode").load()`
+    callSetup = false;
+
+    maintainers = [maintainers.loicreynier];
+
+    settingsOptions = {
+      transparent = helpers.defaultNullOpts.mkBool false "Whether to enable transparent background";
+      italic_comments = helpers.defaultNullOpts.mkBool false "Whether to enable italic comments";
+      underline_links = helpers.defaultNullOpts.mkBool false "Whether to underline links";
+      disable_nvimtree_bg = helpers.defaultNullOpts.mkBool true "Whether to disable nvim-tree background";
+      color_overrides =
+        helpers.defaultNullOpts.mkAttrsOf types.str
+        "{}"
+        ''
+          A dictionary of color overrides.
+          See https://github.com/Mofiqul/vscode.nvim/blob/main/lua/vscode/colors.lua for color names.
+        '';
+      group_overrides = with helpers;
+        defaultNullOpts.mkAttrsOf nixvimTypes.highlight
+        "{}"
+        ''
+          A dictionary of group names, each associated with a dictionary of parameters
+          (`bg`, `fg`, `sp` and `style`) and colors in hex.
+        '';
+    };
+
+    extraConfig = cfg: {
+      extraConfigLuaPre = ''
+        local _vscode = require("vscode")
+        _vscode.setup(${helpers.toLuaObject cfg.settings})
+        _vscode.load()
+      '';
+    };
+  }
