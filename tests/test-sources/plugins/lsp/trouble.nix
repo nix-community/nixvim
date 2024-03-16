ab3a8a28402574ee03fc104cb1db145b334b9c88
Loïc Reynier <loic@loicreynier.fr>
2024-03-16 15:30:02 +0000

    plugins/trouble: switch to settingsOptions

--- tests/test-sources/plugins/lsp/trouble.nix
+++ tests/test-sources/plugins/lsp/trouble.nix
@@ -1,5 +1,4 @@
 {
-  # Empty configuration
   empty = {
     plugins.trouble.enable = true;
   };
@@ -13,53 +12,55 @@
     plugins.trouble.enable = true;
   };
 
-  # All the upstream default options of trouble
   defaults = {
     plugins.trouble = {
       enable = true;
 
-      position = "bottom";
-      height = 10;
-      width = 50;
-      icons = true;
-      mode = "workspace_diagnostics";
-      foldOpen = "";
-      foldClosed = "";
-      group = true;
-      padding = true;
-      actionKeys = {
-        close = "q";
-        cancel = "<esc>";
-        refresh = "r";
-        jump = ["<cr>" "<tab>"];
-        openSplit = ["<c-x>"];
-        openVsplit = ["<c-v>"];
-        openTab = ["<c-t>"];
-        jumpClose = ["o"];
-        toggleMode = "m";
-        togglePreview = "P";
-        hover = "K";
-        preview = "p";
-        closeFolds = ["zM" "zm"];
-        openFolds = ["zR" "zr"];
-        toggleFold = ["zA" "za"];
-        previous = "k";
-        next = "j";
+      settings = {
+        position = "bottom";
+        height = 10;
+        width = 50;
+        icons = true;
+        mode = "workspace_diagnostics";
+        fold_open = "";
+        fold_closed = "";
+        group = true;
+        padding = true;
+        action_keys = {
+          close = "q";
+          cancel = "<esc>";
+          refresh = "r";
+          jump = ["<cr>" "<tab>"];
+          open_split = ["<c-x>"];
+          open_vsplit = ["<c-v>"];
+          open_tab = ["<c-t>"];
+          jump_close = ["o"];
+          toggle_mode = "m";
+          toggle_preview = "P";
+          hover = "K";
+          preview = "p";
+          close_folds = ["zM" "zm"];
+          open_folds = ["zR" "zr"];
+          toggle_fold = ["zA" "za"];
+          previous = "k";
+          next = "j";
+        };
+        indent_lines = true;
+        win_config = {border = "single";};
+        auto_open = false;
+        auto_close = false;
+        auto_preview = true;
+        auto_fold = false;
+        auto_jump = ["lsp_definitions"];
+        signs = {
+          error = "";
+          warning = "";
+          hint = "";
+          information = "";
+          other = "﫠";
+        };
+        use_diagnostic_signs = false;
       };
-      indentLines = true;
-      autoOpen = false;
-      autoClose = false;
-      autoPreview = true;
-      autoFold = false;
-      autoJump = ["lsp_definitions"];
-      signs = {
-        error = "";
-        warning = "";
-        hint = "";
-        information = "";
-        other = "﫠";
-      };
-      useDiagnosticSigns = false;
     };
   };
 }
