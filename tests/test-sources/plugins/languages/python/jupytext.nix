b658169f114bd38d29617f01e1fe4dd587a31199
Gaetan Lepage <gaetan@glepage.com>
2024-03-23 15:46:55 +0100

    plugins/jupytext: init

---
+++ tests/test-sources/plugins/languages/python/jupytext.nix
@@ -0,0 +1,35 @@
+{
+  empty = {
+    plugins.jupytext.enable = true;
+  };
+
+  defaults = {
+    plugins.jupytext = {
+      enable = true;
+
+      settings = {
+        style = "hydrogen";
+        output_extension = "auto";
+        force_ft = null;
+        custom_language_formatting = {};
+      };
+    };
+  };
+
+  example = {
+    plugins.jupytext = {
+      enable = true;
+
+      settings = {
+        style = "light";
+        output_extension = "auto";
+        force_ft = null;
+        custom_language_formatting.python = {
+          extension = "md";
+          style = "markdown";
+          force_ft = "markdown";
+        };
+      };
+    };
+  };
+}
