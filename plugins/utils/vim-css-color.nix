5fb9f0bb86edf08043ebf1cc3d16388469390c0a
Bodleum <60107449+Bodleum@users.noreply.github.com>
2024-03-27 20:55:24 +0000

    plugins/vim-css-color: init (#1335)

---
+++ plugins/utils/vim-css-color.nix
@@ -0,0 +1,12 @@
+{
+  config,
+  helpers,
+  pkgs,
+  ...
+}:
+helpers.vim-plugin.mkVimPlugin config {
+  name = "vim-css-color";
+  defaultPackage = pkgs.vimPlugins.vim-css-color;
+
+  maintainers = [helpers.maintainers.DanielLaing];
+}
