ad046c976d0a52512f75cae6154c39dac84dbc65
Gaetan Lepage <gaetan@glepage.com>
2024-03-27 11:53:27 +0100

    colorschemes/catpuccin: switch to mkNeovimPlugin

--- tests/test-sources/plugins/colorschemes/catppuccin.nix
+++ tests/test-sources/plugins/colorschemes/catppuccin.nix
@@ -6,45 +6,101 @@
   defaults = {
     colorschemes.catppuccin = {
       enable = true;
-      flavour = "mocha";
-      background = {
-        light = "latte";
-        dark = "mocha";
-      };
-      transparentBackground = false;
-      terminalColors = false;
-      showBufferEnd = false;
-      dimInactive = {
-        enabled = true;
-        shade = "dark";
-        percentage = 0.15;
-      };
-      styles = {
-        comments = ["italic"];
-        conditionals = ["italic"];
-        loops = [];
-        functions = [];
-        keywords = [];
-        strings = [];
-        variables = [];
-        numbers = [];
-        booleans = [];
-        properties = [];
-        types = [];
-        operators = [];
-      };
-      colorOverrides = {};
-      customHighlights = {};
-      integrations = {
-        cmp = true;
-        gitsigns = true;
-        nvimtree = true;
-        telescope = {
-          enabled = true;
+
+      settings = {
+        compile_path.__raw = "vim.fn.stdpath 'cache' .. '/catppuccin'";
+        flavour = null;
+        background = {
+          light = "latte";
+          dark = "mocha";
         };
-        notify = false;
-        mini = {
+        transparent_background = false;
+        show_end_of_buffer = false;
+        term_colors = false;
+        dim_inactive = {
           enabled = false;
+          shade = "dark";
+          percentage = 0.15;
+        };
+        no_italic = false;
+        no_bold = false;
+        no_underline = false;
+        styles = {
+          comments = ["italic"];
+          conditionals = ["italic"];
+          loops = [];
+          functions = [];
+          keywords = [];
+          strings = [];
+          variables = [];
+          numbers = [];
+          booleans = [];
+          properties = [];
+          types = [];
+          operators = [];
+        };
+        color_overrides = {};
+        custom_highlights = {};
+        default_integrations = true;
+        integrations = {
+          alpha = true;
+          cmp = true;
+          dap = true;
+          dap_ui = true;
+          dashboard = true;
+          flash = true;
+          gitsigns = true;
+          markdown = true;
+          neogit = true;
+          neotree = true;
+          nvimtree = true;
+          ufo = true;
+          rainbow_delimiters = true;
+          semantic_tokens.__raw = "not (vim.fn.has 'nvim' ~= 1)";
+          telescope.enabled = true;
+          treesitter.__raw = "not (vim.fn.has 'nvim' ~= 1)";
+          treesitter_context = true;
+          barbecue = {
+            dim_dirname = true;
+            bold_basename = true;
+            dim_context = false;
+            alt_background = false;
+          };
+          illuminate = {
+            enabled = true;
+            lsp = false;
+          };
+          indent_blankline = {
+            enabled = true;
+            scope_color = "";
+            colored_indent_levels = false;
+          };
+          native_lsp = {
+            enabled = true;
+            virtual_text = {
+              errors = ["italic"];
+              hints = ["italic"];
+              warnings = ["italic"];
+              information = ["italic"];
+            };
+            underlines = {
+              errors = ["underline"];
+              hints = ["underline"];
+              warnings = ["underline"];
+              information = ["underline"];
+            };
+            inlay_hints = {
+              background = true;
+            };
+          };
+          navic = {
+            enabled = false;
+            custom_bg = "NONE";
+          };
+          dropbar = {
+            enabled = true;
+            color_mode = false;
+          };
         };
       };
     };
@@ -54,49 +110,43 @@
     colorschemes.catppuccin = {
       enable = true;
 
-      flavour = "mocha";
-      terminalColors = true;
-      colorOverrides.mocha.base = "#1e1e2f";
-
-      disableItalic = true;
-      disableBold = true;
-      disableUnderline = true;
-
-      integrations = {
-        barbar = true;
-        fidget = true;
-        gitsigns = true;
-        illuminate = {
-          enabled = true;
+      settings = {
+        flavour = "mocha";
+        disable_italic = true;
+        disable_bold = true;
+        disable_underline = true;
+        term_colors = true;
+        color_overrides = {
+          all.text = "#ffffff";
+          latte = {
+            base = "#ff0000";
+            mantle = "#242424";
+            crust = "#474747";
+          };
+          frappe = {};
+          macchiato = {};
+          mocha = {};
         };
-        indent_blankline = {
-          enabled = true;
-          colored_indent_levels = true;
+        styles = {
+          booleans = ["bold" "italic"];
+          conditionals = ["bold"];
+          functions = ["bold"];
+          keywords = ["italic"];
+          loops = ["bold"];
+          operators = ["bold"];
+          properties = ["italic"];
         };
-        lsp_trouble = true;
-        mini = {
-          enabled = true;
+        integrations = {
+          cmp = true;
+          gitsigns = true;
+          nvimtree = true;
+          treesitter = true;
+          notify = false;
+          mini = {
+            enabled = true;
+            indentscope_color = "";
+          };
         };
-        native_lsp.enabled = true;
-        navic.enabled = true;
-        nvimtree = true;
-        overseer = true;
-        telescope = {
-          enabled = true;
-          style = "nvchad";
-        };
-        treesitter = true;
-        treesitter_context = true;
-        ts_rainbow2 = true;
-      };
-      styles = {
-        booleans = ["bold" "italic"];
-        conditionals = ["bold"];
-        functions = ["bold"];
-        keywords = ["italic"];
-        loops = ["bold"];
-        operators = ["bold"];
-        properties = ["italic"];
       };
     };
   };
