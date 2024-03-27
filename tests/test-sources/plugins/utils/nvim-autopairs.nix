38a4fc7709343eb6e15ebb483e141fd95be28122
Gaetan Lepage <gaetan@glepage.com>
2024-03-27 15:46:56 +0100

    plugins/nvim-autopairs: switch to mkNeovimPlugin

--- tests/test-sources/plugins/utils/nvim-autopairs.nix
+++ tests/test-sources/plugins/utils/nvim-autopairs.nix
@@ -7,27 +7,49 @@
     plugins.nvim-autopairs = {
       enable = true;
 
-      pairs = null;
-      disabledFiletypes = ["TelescopePrompt" "spectre_panel"];
-      disableInMacro = false;
-      disableInVisualblock = false;
-      disableInReplaceMode = true;
-      ignoredNextChar = "[=[[%w%%%'%[%\"%.%`%$]]=]";
-      enableMoveright = true;
-      enableAfterQuote = true;
-      enableCheckBracketLine = true;
-      enableBracketInQuote = true;
-      enableAbbr = false;
-      breakUndo = true;
-      checkTs = false;
-      tsConfig = {
-        lua = ["string" "source"];
-        javascript = ["string" "template_string"];
+      settings = {
+        disable_filetype = ["TelescopePrompt" "spectre_panel"];
+        disable_in_macro = false;
+        disable_in_visualblock = false;
+        disable_in_replace_mode = true;
+        ignored_next_char = "[=[[%w%%%'%[%\"%.%`%$]]=]";
+        enable_moveright = true;
+        enable_afterquote = true;
+        enable_check_bracket_line = true;
+        enable_bracket_in_quote = true;
+        enable_abbr = false;
+        break_undo = true;
+        check_ts = false;
+        ts_config = {
+          lua = [
+            "string"
+            "source"
+            "string_content"
+          ];
+          javascript = [
+            "string"
+            "template_string"
+          ];
+        };
+        map_cr = true;
+        map_bs = true;
+        map_c_h = false;
+        map_c_w = false;
+        fast_wrap = {
+          map = "<M-e>";
+          chars = ["{" "[" "(" "\"" "'"];
+          pattern = ''[=[[%'%"%>%]%)%}%,%`]]=]'';
+          end_key = "$";
+          before_key = "h";
+          after_key = "l";
+          cursor_pos_before = true;
+          keys = "qwertyuiopzxcvbnmasdfghjkl";
+          highlight = "Search";
+          highlight_grey = "Comment";
+          manual_position = true;
+          use_virt_lines = true;
+        };
       };
-      mapCr = true;
-      mapBs = true;
-      mapCH = false;
-      mapCW = false;
     };
   };
 }
