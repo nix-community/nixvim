31f802e39e064ed3ccd65fdbce9cde59c8f2c9bb
Gaetan Lepage <gaetan@glepage.com>
2024-03-30 13:54:48 +0100

    wrappers/hm: add vimdiffAlias option

--- wrappers/modules/hm.nix
+++ wrappers/modules/hm.nix
@@ -4,5 +4,13 @@ with lib;
   options = {
     enable = mkEnableOption "nixvim";
     defaultEditor = mkEnableOption "nixvim as the default editor";
+
+    vimdiffAlias = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Alias `vimdiff` to `nvim -d`.
+      '';
+    };
   };
 }
