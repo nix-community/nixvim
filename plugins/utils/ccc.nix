8eb6c1dbbe983401950c6f389317dc01b8365f34
Jan Kremer <mail@jankremer.eu>
2024-04-01 12:04:40 +0200

    plugins: Add ccc

---
+++ plugins/utils/ccc.nix
@@ -0,0 +1,44 @@
+{
+  helpers,
+  config,
+  pkgs,
+  ...
+}:
+helpers.neovim-plugin.mkNeovimPlugin config {
+  name = "ccc";
+  originalName = "ccc.nvim";
+  defaultPackage = pkgs.vimPlugins.ccc-nvim;
+
+  maintainers = [helpers.maintainers.JanKremer];
+
+  settingsOptions = {
+    default_color = helpers.defaultNullOpts.mkString "#000000" ''
+      The default color used when a color cannot be picked. It must be HEX format.
+    '';
+
+    highlight_mode = helpers.defaultNullOpts.mkString "bg" ''
+      Option to highlight text foreground or background. It is used to output_line and highlighter.
+      Options: "fg" | "bg" | "foreground" | "background"
+    '';
+
+    lsp = helpers.defaultNullOpts.mkBool true ''
+      Whether to enable nvim-lsp support. The color information is updated in the background and the result is used by |:CccPick| and highlighter.
+    '';
+
+    highlighter.auto_enable = helpers.defaultNullOpts.mkBool false ''
+      Whether to enable automatically on BufEnter.
+    '';
+
+    highlighter.lsp = helpers.defaultNullOpts.mkBool true ''
+      If true, highlight using nvim-lsp. If LS with the color provider is not attached to a buffer, it falls back to highlight with pickers. See also |ccc-option-lsp|.
+    '';
+  };
+
+  settingsExample = {
+    default_color = "#FFFFFF";
+    highlight_mode = "fg";
+    lsp = false;
+    highlighter.auto_enable = true;
+    highlighter.lsp = false;
+  };
+}
---
+++ tests/test-sources/plugins/utils/ccc.nix
@@ -0,0 +1,5 @@
+{
+  empty = {
+    plugins.ccc.enable = true;
+  };
+}
