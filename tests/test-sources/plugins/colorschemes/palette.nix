9bf3f1b6f3f731660ce77e15ab94dfb82b5a796e
Gaetan Lepage <gaetan@glepage.com>
2024-04-07 18:18:49 +0200

    colorschemes/palette: switch to mkNeovimPlugin

--- tests/test-sources/plugins/colorschemes/palette.nix
+++ tests/test-sources/plugins/colorschemes/palette.nix
@@ -7,20 +7,22 @@
     colorschemes.palette = {
       enable = true;
 
-      palettes = {
-        main = "dark";
-        accent = "pastel";
-        state = "pastel";
+      settings = {
+        palettes = {
+          main = "dark";
+          accent = "pastel";
+          state = "pastel";
+        };
+        custom_palettes = {
+          main = {};
+          accent = {};
+          state = {};
+        };
+        italics = true;
+        transparent_background = false;
+        caching = true;
+        cache_dir.__raw = "vim.fn.stdpath('cache') .. '/palette'";
       };
-      customPalettes = {
-        main = {};
-        accent = {};
-        state = {};
-      };
-      italics = true;
-      transparentBackground = false;
-      caching = true;
-      cacheDir.__raw = "vim.fn.stdpath('cache') .. '/palette'";
     };
   };
 
@@ -28,28 +30,30 @@
     colorschemes.palette = {
       enable = true;
 
-      palettes = {
-        main = "dust_dusk";
-      };
+      settings = {
+        palettes = {
+          main = "dust_dusk";
+        };
 
-      customPalettes = {
-        main = {
-          dust_dusk = {
-            color0 = "#121527";
-            color1 = "#1A1E39";
-            color2 = "#232A4D";
-            color3 = "#3E4D89";
-            color4 = "#687BBA";
-            color5 = "#A4B1D6";
-            color6 = "#bdbfc9";
-            color7 = "#DFE5F1";
-            color8 = "#e9e9ed";
+        custom_palettes = {
+          main = {
+            dust_dusk = {
+              color0 = "#121527";
+              color1 = "#1A1E39";
+              color2 = "#232A4D";
+              color3 = "#3E4D89";
+              color4 = "#687BBA";
+              color5 = "#A4B1D6";
+              color6 = "#bdbfc9";
+              color7 = "#DFE5F1";
+              color8 = "#e9e9ed";
+            };
           };
         };
-      };
 
-      italics = true;
-      transparentBackground = false;
+        italics = true;
+        transparent_background = false;
+      };
     };
   };
 }
