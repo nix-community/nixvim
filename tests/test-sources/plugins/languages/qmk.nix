17d3f77cd5b876cb29e1f6f7c26f0b7dd3617500
Gaetan Lepage <gaetan@glepage.com>
2024-04-13 14:03:28 +0200

    plugins/qmk: init

---
+++ tests/test-sources/plugins/languages/qmk.nix
@@ -0,0 +1,49 @@
+{
+  minimal = {
+    plugins.qmk = {
+      enable = true;
+      settings = {
+        name = "LAYOUT_preonic_grid";
+        layout = [
+          "x x"
+          "x^x"
+        ];
+      };
+    };
+  };
+
+  example = {
+    plugins.qmk = {
+      enable = true;
+
+      settings = {
+        name = "LAYOUT_preonic_grid";
+        layout = [
+          "x x"
+          "x^x"
+        ];
+        variant = "qmk";
+        timeout = 5000;
+        auto_format_pattern = "*keymap.c";
+        comment_preview = {
+          position = "top";
+          keymap_overrides = {};
+          symbols = {
+            space = " ";
+            horz = "─";
+            vert = "│";
+            tl = "┌";
+            tm = "┬";
+            tr = "┐";
+            ml = "├";
+            mm = "┼";
+            mr = "┤";
+            bl = "└";
+            bm = "┴";
+            br = "┘";
+          };
+        };
+      };
+    };
+  };
+}
