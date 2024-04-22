6ebd538ede5a0de49253620828ba28dc57988305
Gaetan Lepage <gaetan@glepage.com>
2024-04-22 09:55:02 +0200

    plugins/treesitter-context: switch to mkNeovimPlugin

--- tests/test-sources/plugins/languages/treesitter/treesitter-context.nix
+++ tests/test-sources/plugins/languages/treesitter/treesitter-context.nix
@@ -12,15 +12,18 @@
       treesitter-context = {
         enable = true;
 
-        maxLines = 0;
-        minWindowHeight = 0;
-        lineNumbers = true;
-        multilineThreshold = 20;
-        trimScope = "outer";
-        mode = "cursor";
-        separator = null;
-        zindex = 20;
-        onAttach = null;
+        settings = {
+          enable = true;
+          max_lines = 0;
+          min_window_height = 0;
+          line_numbers = true;
+          multiline_threshold = 20;
+          trim_scope = "outer";
+          mode = "cursor";
+          separator = null;
+          zindex = 20;
+          on_attach = null;
+        };
       };
     };
   };
