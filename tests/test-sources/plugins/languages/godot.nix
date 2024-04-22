c826d146c65bfa8164f31931cf54278b99f5a3a0
Gaetan Lepage <gaetan@glepage.com>
2024-04-22 10:05:55 +0200

    plugins/godot: init

---
+++ tests/test-sources/plugins/languages/godot.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}: {
+  empty = {
+    # Godot is only available on Linux
+    plugins.godot.enable = pkgs.stdenv.isLinux;
+  };
+
+  defaults = {
+    plugins.godot = {
+      # Godot is only available on Linux
+      enable = pkgs.stdenv.isLinux;
+
+      settings = {
+        executable = "godot";
+      };
+    };
+  };
+}
