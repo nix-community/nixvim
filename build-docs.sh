81b626f92e1b64d77b90962998633bce1f093f49
traxys <quentin+dev@familleboyer.net>
2024-05-21 23:12:58 +0200

    docs: Fix the path to the unstable docs

--- build-docs.sh
+++ build-docs.sh
@@ -4,6 +4,6 @@ set -ex
 
 mkdir -p docs-build
 nix build github:nix-community/nixvim#docs
-cp -r result/share/doc docs-build
+cp -r result/share/doc/* docs-build
 nix build github:nix-community/nixvim/nixos-23.11#docs
 cp -r result/share/doc docs-build/stable
