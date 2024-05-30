bcfef991f8aba211ca81a6413be2f46695c28de1
Gaetan Lepage <gaetan@glepage.com>
2024-05-30 14:56:05 +0200

    flake: add the nix-community binary cache

--- flake.nix
+++ flake.nix
@@ -36,6 +36,13 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
