1d00efbb00c8859841d1b8cc876612e149402c5d
Gaetan Lepage <gaetan@glepage.com>
2024-06-01 00:56:14 +0200

    plugins/copilot-chat: init

---
+++ plugins/ai/copilot-chat.nix
@@ -0,0 +1,322 @@
+{
+  lib,
+  helpers,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+helpers.neovim-plugin.mkNeovimPlugin config {
+  name = "copilot-chat";
+  originalName = "CopilotChat.nvim";
+  luaName = "CopilotChat";
+  defaultPackage = pkgs.vimPlugins.CopilotChat-nvim;
+
+  maintainers = [ maintainers.GaetanLepage ];
+
+  settingsOptions = {
+    debug = helpers.defaultNullOpts.mkBool false ''
+      Enable debug logging.
+    '';
+
+    proxy = helpers.defaultNullOpts.mkStr null ''
+      Custom proxy to use, formatted as `[protocol://]host[:port]`.
+    '';
+
+    allow_insecure = helpers.defaultNullOpts.mkBool false ''
+      Allow insecure server connections.
+    '';
+
+    system_prompt = helpers.defaultNullOpts.mkLua "require('CopilotChat.prompts').COPILOT_INSTRUCTIONS" ''
+      System prompt to use.
+    '';
+
+    model = helpers.defaultNullOpts.mkStr "gpt-4" ''
+      GPT model to use, 'gpt-3.5-turbo' or 'gpt-4'.
+    '';
+
+    temperature = helpers.defaultNullOpts.mkNullableWithRaw (types.numbers.between 0.0 1.0) 0.1 ''
+      GPT temperature.
+    '';
+
+    question_header = helpers.defaultNullOpts.mkStr "## User " ''
+      Header to use for user questions.
+    '';
+
+    answer_header = helpers.defaultNullOpts.mkStr "## Copilot " ''
+      Header to use for AI answers.
+    '';
+
+    error_header = helpers.defaultNullOpts.mkStr "## Error " ''
+      Header to use for errors.
+    '';
+
+    separator = helpers.defaultNullOpts.mkStr "───" ''
+      Separator to use in chat.
+    '';
+
+    show_folds = helpers.defaultNullOpts.mkBool true ''
+      Shows folds for sections in chat.
+    '';
+
+    show_help = helpers.defaultNullOpts.mkBool true ''
+      Shows help message as virtual lines when waiting for user input.
+    '';
+
+    auto_follow_cursor = helpers.defaultNullOpts.mkBool true ''
+      Auto-follow cursor in chat.
+    '';
+
+    auto_insert_mode = helpers.defaultNullOpts.mkBool false ''
+      Automatically enter insert mode when opening window and if auto follow cursor is enabled on new prompt.
+    '';
+
+    clear_chat_on_new_prompt = helpers.defaultNullOpts.mkBool false ''
+      Clears chat on every new prompt.
+    '';
+
+    highlight_selection = helpers.defaultNullOpts.mkBool true ''
+      Highlight selection.
+    '';
+
+    context =
+      helpers.defaultNullOpts.mkEnum
+        [
+          "buffers"
+          "buffer"
+        ]
+        null
+        ''
+          Default context to use, `"buffers"`, `"buffer"` or `null` (can be specified manually in prompt via @).
+        '';
+
+    history_path = helpers.defaultNullOpts.mkStr (helpers.mkRaw "vim.fn.stdpath('data') .. '/copilotchat_history'") ''
+      Default path to stored history.
+    '';
+
+    callback = helpers.defaultNullOpts.mkLuaFn null ''
+      Callback to use when ask response is received.
+
+      `fun(response: string, source: CopilotChat.config.source)`
+    '';
+
+    selection =
+      helpers.defaultNullOpts.mkLuaFn
+        ''
+          function(source)
+            local select = require('CopilotChat.select')
+            return select.visual(source) or select.line(source)
+          end
+        ''
+        ''
+          Default selection (visual or line).
+          `fun(source: CopilotChat.config.source):CopilotChat.config.selection`
+        '';
+
+    prompts =
+      let
+        promptType = types.submodule {
+          freeformType = with types; attrsOf anything;
+          options = {
+            prompt = helpers.mkNullOrStr "Prompt text.";
+
+            description = helpers.mkNullOrStr "Description for this prompt.";
+
+            kind = helpers.mkNullOrStr "Kind of this prompt.";
+
+            mapping = helpers.mkNullOrStr "A key to bind to this prompt.";
+
+            system_prompt = helpers.mkNullOrStr "System prompt to use.";
+
+            callback = helpers.mkNullOrLuaFn ''
+              Callback to trigger when this prompt is executed.
+              `fun(response: string, source: CopilotChat.config.source)`
+            '';
+
+            selection = helpers.mkNullOrLuaFn ''
+              Selection for this prompt.
+              `fun(source: CopilotChat.config.source):CopilotChat.config.selection`
+            '';
+          };
+        };
+
+        default = {
+          Explain.prompt = "/COPILOT_EXPLAIN Write an explanation for the active selection as paragraphs of text.";
+          Review = {
+            prompt = "/COPILOT_REVIEW Review the selected code.";
+            callback = ''
+              function(response, source)
+                -- see config.lua for implementation
+              end
+            '';
+          };
+          Fix.prompt = "/COPILOT_GENERATE There is a problem in this code. Rewrite the code to show it with the bug fixed.";
+          Optimize.prompt = "/COPILOT_GENERATE Optimize the selected code to improve performance and readablilty.";
+          Docs.prompt = "/COPILOT_GENERATE Please add documentation comment for the selection.";
+          Tests.prompt = "/COPILOT_GENERATE Please generate tests for my code.";
+          FixDiagnostic = {
+            prompt = "Please assist with the following diagnostic issue in file:";
+            selection = "require('CopilotChat.select').diagnostics";
+          };
+          Commit = {
+            prompt = "Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.";
+            selection = "require('CopilotChat.select').gitdiff";
+          };
+          CommitStaged = {
+            prompt = "Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.";
+            selection = ''
+              function(source)
+                return select.gitdiff(source, true)
+              end
+            '';
+          };
+        };
+      in
+      helpers.defaultNullOpts.mkAttrsOf (with types; either str promptType) default ''
+        Default prompts.
+      '';
+
+    window = {
+      layout =
+        helpers.defaultNullOpts.mkEnumFirstDefault
+          [
+            "vertical"
+            "horizontal"
+            "float"
+            "replace"
+          ]
+          ''
+            Layout for the window.
+          '';
+
+      width =
+        helpers.defaultNullOpts.mkNullableWithRaw
+          (with types; either (numbers.between 0.0 1.0) ints.positive)
+          0.5
+          ''
+            Fractional width of parent, or absolute width in columns when > 1.
+          '';
+
+      height =
+        helpers.defaultNullOpts.mkNullableWithRaw
+          (with types; either (numbers.between 0.0 1.0) ints.positive)
+          0.5
+          ''
+            Fractional height of parent, or absolute height in rows when > 1.
+          '';
+
+      relative =
+        helpers.defaultNullOpts.mkEnumFirstDefault
+          [
+            "editor"
+            "win"
+            "cursor"
+            "mouse"
+          ]
+          ''
+            Relative position.
+            (Only for floating windows.)
+          '';
+
+      border =
+        helpers.defaultNullOpts.mkEnum
+          [
+            "none"
+            "single"
+            "double"
+            "rounded"
+            "solid"
+            "shadow"
+          ]
+          "single"
+          ''
+            Border for this window.
+            (Only for floating windows.)
+          '';
+
+      row = helpers.defaultNullOpts.mkUnsignedInt null ''
+        Row position of the window, default is centered.
+        (Only for floating windows.)
+      '';
+
+      col = helpers.defaultNullOpts.mkUnsignedInt null ''
+        Column position of the window, default is centered.
+        (Only for floating windows.)
+      '';
+
+      title = helpers.defaultNullOpts.mkStr "Copilot Chat" ''
+        Title of chat window.
+      '';
+
+      footer = helpers.defaultNullOpts.mkStr null ''
+        Footer of chat window.
+      '';
+
+      zindex = helpers.defaultNullOpts.mkUnsignedInt 1 ''
+        Determines if window is on top or below other floating windows.
+      '';
+    };
+
+    mappings =
+      helpers.defaultNullOpts.mkAttrsOf
+        (types.submodule {
+          options = {
+            normal = helpers.mkNullOrStr "Key for normal mode.";
+
+            insert = helpers.mkNullOrStr "Key for insert mode.";
+
+            detail = helpers.mkNullOrStr "A description for this binding.";
+          };
+        })
+        {
+          complete = {
+            detail = "Use @<Tab> or /<Tab> for options.";
+            insert = "<Tab>";
+          };
+          close = {
+            normal = "q";
+            insert = "<C-c>";
+          };
+          reset = {
+            normal = "<C-l>";
+            insert = "<C-l>";
+          };
+          submit_prompt = {
+            normal = "<CR>";
+            insert = "<C-m>";
+          };
+          accept_diff = {
+            normal = "<C-y>";
+            insert = "<C-y>";
+          };
+          yank_diff.normal = "gy";
+          show_diff.normal = "gd";
+          show_system_prompt.normal = "gp";
+          show_user_selection.normal = "gs";
+        }
+        "Mappings for CopilotChat.";
+  };
+
+  settingsExample = {
+    question_header = "## User ";
+    answer_header = "## Copilot ";
+    error_header = "## Error ";
+    prompts = {
+      Explain = "Please explain how the following code works.";
+      Review = "Please review the following code and provide suggestions for improvement.";
+      Tests = "Please explain how the selected code works, then generate unit tests for it.";
+    };
+    auto_follow_cursor = false;
+    show_help = false;
+    mappings = {
+      complete = {
+        detail = "Use @<Tab> or /<Tab> for options.";
+        insert = "<Tab>";
+      };
+      close = {
+        normal = "q";
+        insert = "<C-c>";
+      };
+    };
+  };
+}
---
+++ tests/test-sources/plugins/ai/copilot-chat.nix
@@ -0,0 +1,167 @@
+{
+
+  empty = {
+    plugins.copilot-chat.enable = true;
+  };
+
+  defaults = {
+    plugins.copilot-chat = {
+      enable = true;
+
+      settings = {
+        debug = false;
+        proxy = null;
+        allow_insecure = false;
+        system_prompt = "require('CopilotChat.prompts').COPILOT_INSTRUCTIONS";
+        model = "gpt-4";
+        temperature = 0.1;
+        question_header = "## User ";
+        answer_header = "## Copilot ";
+        error_header = "## Error ";
+        separator = "───";
+        show_folds = true;
+        show_help = true;
+        auto_follow_cursor = true;
+        auto_insert_mode = false;
+        clear_chat_on_new_prompt = false;
+        highlight_selection = true;
+        context = null;
+        history_path.__raw = "vim.fn.stdpath('data') .. '/copilotchat_history'";
+        callback = null;
+        selection = ''
+          function(source)
+            local select = require('CopilotChat.select')
+            return select.visual(source) or select.line(source)
+          end
+        '';
+        prompts = {
+          Explain.prompt = "/COPILOT_EXPLAIN Write an explanation for the active selection as paragraphs of text.";
+          Review = {
+            prompt = "/COPILOT_REVIEW Review the selected code.";
+            callback = ''
+              function(response, source)
+                -- see config.lua for implementation
+              end
+            '';
+          };
+          Fix.prompt = "/COPILOT_GENERATE There is a problem in this code. Rewrite the code to show it with the bug fixed.";
+          Optimize.prompt = "/COPILOT_GENERATE Optimize the selected code to improve performance and readablilty.";
+          Docs.prompt = "/COPILOT_GENERATE Please add documentation comment for the selection.";
+          Tests.prompt = "/COPILOT_GENERATE Please generate tests for my code.";
+          FixDiagnostic = {
+            prompt = "Please assist with the following diagnostic issue in file:";
+            selection = "require('CopilotChat.select').diagnostics";
+          };
+          Commit = {
+            prompt = "Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.";
+            selection = "require('CopilotChat.select').gitdiff";
+          };
+          CommitStaged = {
+            prompt = "Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.";
+            selection = ''
+              function(source)
+                return select.gitdiff(source, true)
+              end
+            '';
+          };
+        };
+
+        window = {
+          layout = "vertical";
+          width = 0.5;
+          height = 0.5;
+          relative = "editor";
+          border = "single";
+          row = null;
+          col = null;
+          title = "Copilot Chat";
+          footer = null;
+          zindex = 1;
+        };
+        mappings = {
+          complete = {
+            detail = "Use @<Tab> or /<Tab> for options.";
+            insert = "<Tab>";
+          };
+          close = {
+            normal = "q";
+            insert = "<C-c>";
+          };
+          reset = {
+            normal = "<C-l>";
+            insert = "<C-l>";
+          };
+          submit_prompt = {
+            normal = "<CR>";
+            insert = "<C-m>";
+          };
+          accept_diff = {
+            normal = "<C-y>";
+            insert = "<C-y>";
+          };
+          yank_diff.normal = "gy";
+          show_diff.normal = "gd";
+          show_system_prompt.normal = "gp";
+          show_user_selection.normal = "gs";
+        };
+      };
+    };
+  };
+
+  example = {
+    plugins.copilot-chat = {
+      enable = true;
+
+      settings = {
+        question_header = "## User ";
+        answer_header = "## Copilot ";
+        error_header = "## Error ";
+        prompts = {
+          Explain = "Please explain how the following code works.";
+          Review = "Please review the following code and provide suggestions for improvement.";
+          Tests = "Please explain how the selected code works, then generate unit tests for it.";
+          Refactor = "Please refactor the following code to improve its clarity and readability.";
+          FixCode = "Please fix the following code to make it work as intended.";
+          FixError = "Please explain the error in the following text and provide a solution.";
+          BetterNamings = "Please provide better names for the following variables and functions.";
+          Documentation = "Please provide documentation for the following code.";
+          SwaggerApiDocs = "Please provide documentation for the following API using Swagger.";
+          SwaggerJsDocs = "Please write JSDoc for the following API using Swagger.";
+          Summarize = "Please summarize the following text.";
+          Spelling = "Please correct any grammar and spelling errors in the following text.";
+          Wording = "Please improve the grammar and wording of the following text.";
+          Concise = "Please rewrite the following text to make it more concise.";
+        };
+
+        auto_follow_cursor = false;
+        show_help = false;
+        mappings = {
+          complete = {
+            detail = "Use @<Tab> or /<Tab> for options.";
+            insert = "<Tab>";
+          };
+          close = {
+            normal = "q";
+            insert = "<C-c>";
+          };
+          reset = {
+            normal = "<C-x>";
+            insert = "<C-x>";
+          };
+          submit_prompt = {
+            normal = "<CR>";
+            insert = "<C-CR>";
+          };
+          accept_diff = {
+            normal = "<C-y>";
+            insert = "<C-y>";
+          };
+          yank_diff.normal = "gmy";
+          show_diff.normal = "gmd";
+          show_system_prompt.normal = "gmp";
+          show_user_selection.normal = "gms";
+        };
+      };
+    };
+  };
+}
