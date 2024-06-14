7a2a25af02be25987aa43cd681312f4b5ba12317
Matt Sturgeon <matt@sturgeon.me.uk>
2024-06-14 15:27:30 +0100

    ci: simplify docs-build workflow

--- build-docs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-set -ex
-
-mkdir -p docs-build
-nix build github:nix-community/nixvim#docs
-cp -r result/share/doc/* docs-build
-nix build github:nix-community/nixvim/nixos-24.05#docs
-cp -r result/share/doc docs-build/stable
