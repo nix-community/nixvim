22f1c8a246c88825e2365715c4e6a9558415c72c
Matt Sturgeon <matt@sturgeon.me.uk>
2024-06-20 22:07:29 +0100

    flake: use 24.05 home-manager branch

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
