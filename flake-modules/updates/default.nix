f4ea0dcd6a38ec998f27b88688caaba0bea9e968
traxys <quentin+dev@familleboyer.net>
2024-07-04 19:34:35 +0200

    github: Re-generate the files on updates

--- flake-modules/updates/default.nix
+++ flake-modules/updates/default.nix
@@ -2,6 +2,21 @@
   perSystem =
     { pkgs, ... }:
     {
+      apps.generate-files.program = pkgs.writeShellApplication {
+        name = "generate-files";
+
+        text = ''
+          repo_root=$(git rev-parse --show-toplevel)
+          generated_dir=$repo_root/generated
+
+          # Rust-Analyzer
+          nix build .#rust-analyzer-options
+          cat ./result >"$generated_dir"/rust-analyzer.nix
+
+          nix fmt
+        '';
+      };
+
       packages = {
         rust-analyzer-options = pkgs.callPackage ./rust-analyzer.nix { };
       };
