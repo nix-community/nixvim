b59fa976d0f42eba35bf89c8fbc4107de7ef1db2
Matt Sturgeon <matt@sturgeon.me.uk>
2024-07-05 15:35:57 +0100

    plugins/nvim-osc52: hide warnings in tests

--- tests/test-sources/plugins/utils/nvim-osc52.nix
+++ tests/test-sources/plugins/utils/nvim-osc52.nix
@@ -1,10 +1,16 @@
+{ pkgs, ... }:
 {
   empty = {
     plugins.nvim-osc52.enable = true;
+
+    # Hide warnings, since this plugin is deprecated
+    warnings = pkgs.lib.mkForce [ ];
   };
 
   defaults = {
     plugins.nvim-osc52 = {
+      enable = true;
+
       maxLength = 0;
       silent = false;
       trim = false;
@@ -14,5 +20,8 @@
         enable = true;
       };
     };
+
+    # Hide warnings, since this plugin is deprecated
+    warnings = pkgs.lib.mkForce [ ];
   };
 }
