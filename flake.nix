f38400d95978fba42adc8609ad4aa381f05a7535
Matt Sturgeon <matt@sturgeon.me.uk>
2024-07-07 01:49:24 +0100

    flake: disable `allow-import-from-derivation`
    Unfortunately this will print a confirmation when first using the flake:
    
    ```
    do you want to allow configuration setting 'allow-import-from-derivation' to be set to 'false' (y/N)?
    ```
    
    This is no different to the cachix configs though, so probably no big
    deal.

--- flake.nix
+++ flake.nix
@@ -71,6 +71,7 @@
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    allow-import-from-derivation = false;
   };
 
   outputs =
