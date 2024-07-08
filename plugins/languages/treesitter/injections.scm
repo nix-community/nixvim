023dc1c93a17181502269609924950acaa9091d3
traxys <quentin+dev@familleboyer.net>
2024-07-08 18:57:12 +0200

    plugins/treesitter: Add a treesitter injection query for raw lua

--- plugins/languages/treesitter/injections.scm
+++ plugins/languages/treesitter/injections.scm
@@ -11,7 +11,20 @@
       ((string_fragment) @injection.content
         (#set! injection.language "lua")))
   ]
-  (#match? @_path "(^extraConfigLua(Pre|Post)?)$"))
+  (#match? @_path "(^(extraConfigLua(Pre|Post)?|__raw))$"))
+
+(apply_expression
+  function: (_) @_func
+  argument: [
+    (string_expression
+      ((string_fragment) @injection.content
+        (#set! injection.language "lua")))
+    (indented_string_expression
+      ((string_fragment) @injection.content
+        (#set! injection.language "lua")))
+  ]
+  (#match? @_func "(^|\\.)mkRaw$")
+  (#set! injection.combined))
 
 (binding
   attrpath: (attrpath
