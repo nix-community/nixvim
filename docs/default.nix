d10534b61f8ace0b0916c654c87019e41bd159e0
Matt Sturgeon <matt@sturgeon.me.uk>
2024-07-08 18:43:31 +0100

    DEBUG: add some traces to the docs' lib extensions

--- docs/default.nix
+++ docs/default.nix
@@ -4,20 +4,72 @@
 let
   # Extend nixpkg's lib, so that we can handle recursive leaf types such as `either`
   lib = pkgs.lib.extend (
-    final: prev: {
+    final: prev:
+    let
+      prettyEval =
+        value:
+        builtins.tryEval (
+          lib.generators.toPretty { } (
+            lib.generators.withRecursion {
+              depthLimit = 10;
+              throwOnDepthLimit = false;
+            } value
+          )
+        );
+      show =
+        value:
+        let
+          res = prettyEval value;
+        in
+        if res.success then res.value else "<FAILED>";
+    in
+    {
       types = prev.types // {
         either =
           t1: t2:
           (prev.types.either t1 t2)
           // {
-            getSubOptions = prefix: (t1.getSubOptions prefix) // (t2.getSubOptions prefix);
+            getSubOptions =
+              prefix:
+              let
+                t1Ty = t1.getSubOptions prefix;
+                t2Ty = t2.getSubOptions prefix;
+                res = prev.trace ''
+                  either.getsuboptions:
+                  prefix = ${prev.showOption prefix}
+                  t1.name = ${show t1.name}
+                  t1.desc = ${show t1.description}
+                  t1Ty = ${show t1Ty}
+                  t2.name = ${show t2.name}
+                  t2.desc = ${show t2.description}
+                  t2Ty = ${show t2Ty}
+                '' (t1Ty // t2Ty);
+              in
+              if t1.name == "rawLua" || t1.name == "rawLua" then res else (t1Ty // t2Ty);
           };
 
-        eitherRecursive = t1: t2: (final.types.either t1 t2) // { getSubOptions = _: { }; };
+        eitherRecursive =
+          t1: t2:
+          let
+            left = final.types.either t1 t2;
+            right.getSubOptions =
+              prefix:
+              prev.trace ''
+                eitherRecursive.getSubOptions
+                prefix = ${prev.showOption prefix}
+                (no-op)
+              '' { };
+          in
+          left // right;
 
         oneOfRecursive =
-          ts:
+          ts':
           let
+            ts = prev.trace ''
+              oneOfRecursive:
+              ts = ${show ts}
+            '' ts';
+
             head' =
               if ts == [ ] then
                 throw "types.oneOfRecursive needs to get at least one type in its argument"
