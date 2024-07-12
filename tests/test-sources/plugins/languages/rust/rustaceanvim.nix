dc1559f25e419a5c1c2ac77aab466bae42fbc381
Austin Horstman <khaneliman12@gmail.com>
2024-07-12 09:59:09 -0500

    plugins/rustaceanvim: fix deprecation in test

--- tests/test-sources/plugins/languages/rust/rustaceanvim.nix
+++ tests/test-sources/plugins/languages/rust/rustaceanvim.nix
@@ -115,7 +115,7 @@
               return { 'rust-analyzer', '--log-file', RustaceanConfig.server.logfile }
             end
           '';
-          settings = ''
+          default_settings = ''
             function(project_root, default_settings)
               return require('rustaceanvim.config.server').load_rust_analyzer_settings(project_root, { default_settings = default_settings })
             end
