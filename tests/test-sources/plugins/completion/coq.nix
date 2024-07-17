daa94bd6c236538a71aa0c12216afd138dd517ed
traxys <quentin+dev@familleboyer.net>
2024-07-17 16:18:26 +0200

    tests: Disable coq-nvim defaults on x86_64 darwin

--- tests/test-sources/plugins/completion/coq.nix
+++ tests/test-sources/plugins/completion/coq.nix
@@ -4,16 +4,21 @@
   };
 
   nixvim-defaults = {
-    plugins.coq-nvim = {
-      enable = true;
+    module =
+      { pkgs, ... }:
+      {
+        plugins.coq-nvim = {
+          # It seems that the plugin has issues being executed in the same derivation
+          enable = !(pkgs.stdenv.isDarwin && pkgs.stdenv.isx86_64);
 
-      settings = {
-        xdg = true;
-        auto_start = true;
-        keymap.recommended = true;
-        completion.always = true;
+          settings = {
+            xdg = true;
+            auto_start = true;
+            keymap.recommended = true;
+            completion.always = true;
+          };
+        };
       };
-    };
   };
 
   artifacts = {
