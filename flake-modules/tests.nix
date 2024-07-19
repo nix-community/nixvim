b1576362a735e06f22445f1b44d17447fdaebbf1
traxys <quentin+dev@familleboyer.net>
2024-07-19 20:22:19 +0200

    tests: Remove the test link farm
    Fixes #1878

--- flake-modules/tests.nix
+++ flake-modules/tests.nix
@@ -10,42 +10,42 @@
       ...
     }:
     {
-      checks = {
-        tests = import ../tests {
+      checks =
+        {
+          extra-args-tests = import ../tests/extra-args.nix {
+            inherit pkgs;
+            inherit makeNixvimWithModule;
+          };
+
+          extend = import ../tests/extend.nix { inherit pkgs makeNixvimWithModule; };
+
+          extra-files = import ../tests/extra-files.nix { inherit pkgs makeNixvimWithModule; };
+
+          enable-except-in-tests = import ../tests/enable-except-in-tests.nix {
+            inherit pkgs makeNixvimWithModule;
+            inherit (self.lib.${system}.check) mkTestDerivationFromNixvimModule;
+          };
+
+          no-flake = import ../tests/no-flake.nix {
+            inherit system;
+            inherit (self.lib.${system}.check) mkTestDerivationFromNvim;
+            nixvim = "${self}";
+          };
+
+          lib-tests = import ../tests/lib-tests.nix {
+            inherit pkgs helpers;
+            inherit (pkgs) lib;
+          };
+
+          maintainers = import ../tests/maintainers.nix { inherit pkgs; };
+        }
+        // (import ../tests {
           inherit
             pkgs
             pkgsUnfree
             helpers
             makeNixvimWithModule
             ;
-        };
-
-        extra-args-tests = import ../tests/extra-args.nix {
-          inherit pkgs;
-          inherit makeNixvimWithModule;
-        };
-
-        extend = import ../tests/extend.nix { inherit pkgs makeNixvimWithModule; };
-
-        extra-files = import ../tests/extra-files.nix { inherit pkgs makeNixvimWithModule; };
-
-        enable-except-in-tests = import ../tests/enable-except-in-tests.nix {
-          inherit pkgs makeNixvimWithModule;
-          inherit (self.lib.${system}.check) mkTestDerivationFromNixvimModule;
-        };
-
-        no-flake = import ../tests/no-flake.nix {
-          inherit system;
-          inherit (self.lib.${system}.check) mkTestDerivationFromNvim;
-          nixvim = "${self}";
-        };
-
-        lib-tests = import ../tests/lib-tests.nix {
-          inherit pkgs helpers;
-          inherit (pkgs) lib;
-        };
-
-        maintainers = import ../tests/maintainers.nix { inherit pkgs; };
-      };
+        });
     };
 }
