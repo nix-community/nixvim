10cc58d497ab422801b68ec8cde036b521f76a50
Gaetan Lepage <gaetan@glepage.com>
2024-07-23 00:02:30 +0200

    plugins/lsp/pylsp: propagatedBuildInputs -> dependencies

--- plugins/lsp/language-servers/pylsp.nix
+++ plugins/lsp/language-servers/pylsp.nix
@@ -539,7 +539,7 @@ in
               (optional (isEnabled plugins.${pluginName}) (
                 nixPackage.overridePythonAttrs (old: {
                   # Get rid of the python-lsp-server dependency
-                  propagatedBuildInputs = filter (dep: dep.pname != "python-lsp-server") old.propagatedBuildInputs;
+                  dependencies = filter (dep: dep.pname != "python-lsp-server") old.dependencies;
 
                   # Skip testing because those naked dependencies will complain about missing pylsp
                   doCheck = false;
@@ -581,6 +581,9 @@ in
                     '';
                 });
                 rope = pylsp-rope.overridePythonAttrs (old: {
+                  # TODO: remove when https://github.com/NixOS/nixpkgs/pull/329137 reaches nixos-unstable
+                  build-system = [ setuptools ];
+
                   postPatch =
                     old.postPatch or ''''
                     + ''
