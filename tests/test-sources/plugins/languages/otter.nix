820f8d58eafd7121989fea3ae9e71f29699d856b
Gaetan Lepage <gaetan@glepage.com>
2024-07-29 00:14:36 +0200

    tests/otter: add treesitter to avoid warning

--- tests/test-sources/plugins/languages/otter.nix
+++ tests/test-sources/plugins/languages/otter.nix
@@ -1,38 +1,47 @@
 {
   empty = {
-    plugins.otter.enable = true;
+    plugins = {
+      otter.enable = true;
+      # Avoid the warning
+      treesitter.settings.highlight.enable = true;
+    };
   };
 
   defaults = {
-    plugins.otter = {
-      enable = true;
+    plugins = {
+      # Avoid the warning
+      treesitter.settings.highlight.enable = true;
 
-      settings = {
-        lsp = {
-          hover = {
-            border = [
-              "╭"
-              "─"
-              "╮"
-              "│"
-              "╯"
-              "─"
-              "╰"
-              "│"
-            ];
+      otter = {
+        enable = true;
+
+        settings = {
+          lsp = {
+            hover = {
+              border = [
+                "╭"
+                "─"
+                "╮"
+                "│"
+                "╯"
+                "─"
+                "╰"
+                "│"
+              ];
+            };
+            diagnostic_update_events = [ "BufWritePost" ];
           };
-          diagnostic_update_events = [ "BufWritePost" ];
+          buffers = {
+            set_filetype = false;
+            write_to_disk = false;
+          };
+          strip_wrapping_quote_characters = [
+            "'"
+            "\""
+            "\`"
+          ];
+          handle_leading_whitespace = false;
         };
-        buffers = {
-          set_filetype = false;
-          write_to_disk = false;
-        };
-        strip_wrapping_quote_characters = [
-          "'"
-          "\""
-          "\`"
-        ];
-        handle_leading_whitespace = false;
       };
     };
   };
