07fe8cdc43d9a28240568be5625566b651251149
Matt Sturgeon <matt@sturgeon.me.uk>
2024-08-04 21:54:17 +0100

    lib/modules: add `evalNixvim`
    Evaluates a nixvim config, by default also checking warnings and assertions.
    
    This used internally by the standalone wrapper.

--- lib/modules.nix
+++ lib/modules.nix
@@ -16,4 +16,39 @@ rec {
       defaultPkgs = pkgs;
     }
     // extraSpecialArgs;
+
+  # Evaluate nixvim modules
+  evalNixvim =
+    {
+      modules ? [ ],
+      extraSpecialArgs ? { },
+      isTopLevel ? true,
+      checkWarnings ? true,
+      checkAssertions ? true,
+    }:
+    let
+      result = lib.evalModules {
+        modules = [ (if isTopLevel then ../modules/top-level else ../modules) ] ++ modules;
+        specialArgs = specialArgsWith extraSpecialArgs;
+      };
+
+      failedAssertions = getAssertionMessages result.config.assertions;
+
+      checkWarningsFn = if checkWarnings then lib.showWarnings result.config.warnings else lib.id;
+
+      checkAssertionsFn =
+        if checkAssertions && failedAssertions != [ ] then
+          _: throw "\nFailed assertions:\n${lib.concatStringsSep "\n" (map (x: "- ${x}") failedAssertions)}"
+        else
+          lib.id;
+    in
+    checkWarningsFn (checkAssertionsFn result);
+
+  # Return the messages for all assertions that failed
+  getAssertionMessages =
+    assertions:
+    lib.pipe assertions [
+      (lib.filter (x: !x.assertion))
+      (lib.map (x: x.message))
+    ];
 }
