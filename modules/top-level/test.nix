903d597a3865f97650f472f2cda54fc6ebfbd309
Matt Sturgeon <matt@sturgeon.me.uk>
2024-08-05 14:25:44 +0100

    modules/test: check warnings/assertions

--- modules/top-level/test.nix
+++ modules/top-level/test.nix
@@ -42,20 +42,44 @@ in
         pkgs.docker-client
       ];
 
-      # We need to set HOME because neovim will try to create some files
-      #
-      # Because neovim does not return an exitcode when quitting we need to check if there are
-      # errors on stderr
-      # TODO: check warnings/assertions
-      buildPhase = lib.optionalString cfg.runNvim ''
-        mkdir -p .cache/nvim
+      # First check warnings/assertions, then run nvim
+      buildPhase =
+        let
+          inherit (config) warnings;
+          assertions = lib.nixvim.modules.getAssertionMessages config.assertions;
 
-        output=$(HOME=$(realpath .) nvim -mn --headless "+q" 2>&1 >/dev/null)
-        if [[ -n $output ]]; then
-          echo "ERROR: $output"
-          exit 1
-        fi
-      '';
+          showErr =
+            name: lines:
+            lib.optionalString (lines != [ ]) ''
+              Unexpected ${name}:
+              ${lib.concatStringsSep "\n" (lib.map (v: "- ${v}") lines)}
+            '';
+
+          errors = lib.foldlAttrs (
+            err: name: lines:
+            err + showErr name lines
+          ) "" { inherit assertions warnings; };
+        in
+        ''
+          ${lib.toShellVars { inherit errors; }}
+          if [ -n "$errors" ]; then
+            echo -n "$errors"
+            exit 1
+          fi
+        ''
+        # We need to set HOME because neovim will try to create some files
+        #
+        # Because neovim does not return an exitcode when quitting we need to check if there are
+        # errors on stderr
+        + lib.optionalString cfg.runNvim ''
+          mkdir -p .cache/nvim
+
+          output=$(HOME=$(realpath .) nvim -mn --headless "+q" 2>&1 >/dev/null)
+          if [[ -n $output ]]; then
+            echo "ERROR: $output"
+            exit 1
+          fi
+        '';
 
       # If we don't do this nix is not happy
       installPhase = ''
