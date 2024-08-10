f13bdef0bc697261c51eab686c28c7e2e7b7db3c
Gaetan Lepage <gaetan@glepage.com>
2024-08-10 10:30:51 +0200

    plugins/lsp/astro: fix package attribute path

--- plugins/lsp/language-servers/default.nix
+++ plugins/lsp/language-servers/default.nix
@@ -28,7 +28,10 @@ let
     {
       name = "astro";
       description = "astrols for Astro";
-      package = pkgs.nodePackages."@astrojs/language-server";
+      package =
+        pkgs.astro-language-server
+          # TODO remove the following line when https://github.com/NixOS/nixpkgs/pull/333736 reaches nixos-unstable
+          or pkgs.nodePackages."@astrojs/language-server";
       cmd = cfg: [
         "${cfg.package}/bin/astro-ls"
         "--stdio"
