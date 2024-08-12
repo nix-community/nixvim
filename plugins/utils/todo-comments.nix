9985956fbc3f24ad03de21190cd1302c6a314165
hansemschnokeloch <hansemschnokeloch@users.noreply.github.com>
2024-08-12 16:41:39 +0200

    Fix for obsolete nerdfont icons
    These two icons are removed from nerdfont.
    See https://www.nerdfonts.com/cheat-sheet

--- plugins/utils/todo-comments.nix
+++ plugins/utils/todo-comments.nix
@@ -76,8 +76,8 @@ in
               TODO = { icon = " "; color = "info"; };
               HACK = { icon = " "; color = "warning"; };
               WARN = { icon = " "; color = "warning"; alt = [ "WARNING" "XXX" ]; };
-              PERF = { icon = " "; alt = [ "OPTIM" "PERFORMANCE" "OPTIMIZE" ]; };
-              NOTE = { icon = " "; color = "hint"; alt = [ "INFO" ]; };
+              PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+              NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
               TEST = { icon = "⏲ "; color = "test"; alt = [ "TESTING" "PASSED" "FAILED" ]; };
             };
             ```
