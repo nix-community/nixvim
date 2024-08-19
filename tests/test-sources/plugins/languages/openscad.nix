ec9d29918321173c57a5fa8ca5a93106c373d12c
Austin Horstman <khaneliman12@gmail.com>
2024-08-19 16:09:45 -0500

    tests/plugins/openscad: re-enable on darwin
    Builds for me locally.

--- tests/test-sources/plugins/languages/openscad.nix
+++ tests/test-sources/plugins/languages/openscad.nix
@@ -1,7 +1,4 @@
-{ pkgs, ... }:
-# As of 2024-01-04, vimPlugins.openscad is broken on darwin
-# TODO: re-enable this test when fixed
-pkgs.lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+{
   empty = {
     plugins.openscad.enable = true;
   };
