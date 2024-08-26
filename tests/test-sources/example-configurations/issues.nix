d0564ce4cfd3d9eff700de66b86b943326b41946
Austin Horstman <khaneliman12@gmail.com>
2024-08-26 12:02:08 -0500

    tests/example-configuration: re-enable jsonls

--- tests/test-sources/example-configurations/issues.nix
+++ tests/test-sources/example-configurations/issues.nix
@@ -87,9 +87,7 @@
             installCargo = true;
             installRustc = true;
           };
-          # TODO: re-enable when pkg is fixed and available
-          # https://github.com/NixOS/nixpkgs/pull/335559
-          # jsonls.enable = true;
+          jsonls.enable = true;
         };
       };
 
