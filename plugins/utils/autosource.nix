475dca6895ed1e7f370f3a0c83df7cddc98c543a
Matt Sturgeon <matt@sturgeon.me.uk>
2024-09-08 13:59:19 +0100

    plugins/autosource: `mkBoolInt` -> `mkFlagInt`

--- plugins/utils/autosource.nix
+++ plugins/utils/autosource.nix
@@ -4,10 +4,6 @@
 }:
 let
   inherit (lib.nixvim) defaultNullOpts;
-  mkBoolInt = defaultNullOpts.mkEnum [
-    0
-    1
-  ];
 in
 lib.nixvim.vim-plugin.mkVimPlugin {
   name = "autosource";
@@ -18,12 +14,12 @@ lib.nixvim.vim-plugin.mkVimPlugin {
   maintainers = [ lib.nixvim.maintainers.refaelsh ];
 
   settingsOptions = {
-    prompt_for_new_file = mkBoolInt 1 ''
+    prompt_for_new_file = defaultNullOpts.mkFlagInt 1 ''
       The primary use-case of this option is to support automated testing.
       When set to false AutoSource will not prompt you when it detects a new file. The file will *NOT* be sourced.
     '';
 
-    prompt_for_changed_file = mkBoolInt 1 ''
+    prompt_for_changed_file = defaultNullOpts.mkFlagInt 1 ''
       The primary use-case of this option is to support automated testing.
       When set to false AutoSource will not prompt you when it detects when a file is changed. The file will NOT be sourced.
     '';
