f76051e06e40921cfb3eb0b4bc718880a6f1d9b1
Austin Horstman <khaneliman12@gmail.com>
2024-09-14 22:02:45 -0500

    plugins/compiler: init

---
+++ plugins/by-name/compiler/default.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  ...
+}:
+lib.nixvim.neovim-plugin.mkNeovimPlugin {
+  name = "compiler";
+  originalName = "compiler.nvim";
+  package = "compiler-nvim";
+
+  maintainers = [ lib.maintainers.khaneliman ];
+
+  description = ''
+    > [!Note]
+    > Some languages require you manually install their compilers in your machine, so `compiler.nvim` is able to call them.
+    > Please check [here], as the packages will be different depending your operative system.
+
+    [here]: https://github.com/Zeioth/Compiler.nvim/wiki/how-to-install-the-required-dependencies
+  '';
+}
---
+++ tests/test-sources/plugins/by-name/compiler/default.nix
@@ -0,0 +1,5 @@
+{
+  empty = {
+    plugins.compiler.enable = true;
+  };
+}
