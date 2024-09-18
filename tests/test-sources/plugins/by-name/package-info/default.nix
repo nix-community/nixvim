d4b1827606e57b417c882d1a525bd34d67999cfc
Austin Horstman <khaneliman12@gmail.com>
2024-09-17 23:37:33 -0500

    plugins/package-info: init

---
+++ tests/test-sources/plugins/by-name/package-info/default.nix
@@ -0,0 +1,31 @@
+{
+  empty = {
+    plugins.package-info.enable = true;
+  };
+
+  default = {
+    plugins.package-info = {
+      enable = true;
+
+      settings = {
+        colors = {
+          up_to_date = "#3C4048";
+          outdated = "#d19a66";
+          invalid = "#ee4b2b";
+        };
+        icons = {
+          enable = true;
+          style = {
+            up_to_date = "|  ";
+            outdated = "|  ";
+            invalid = "|  ";
+          };
+        };
+        autostart = true;
+        package_manager = "npm";
+        hide_up_to_date = false;
+        hide_unstable_versions = false;
+      };
+    };
+  };
+}
