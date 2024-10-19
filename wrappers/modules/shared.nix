606432a76d8cd2ac0b6eefac4319c4d26ea297c7
Matt Sturgeon <matt@sturgeon.me.uk>
2024-10-20 00:11:36 +0100

    modules/nixpkgs: add `useGlobalPackages` option

--- wrappers/modules/shared.nix
+++ wrappers/modules/shared.nix
@@ -3,6 +3,19 @@
   options = {
     enable = lib.mkEnableOption "nixvim";
 
+    nixpkgs.useGlobalPackages = lib.mkOption {
+      type = lib.types.bool;
+      default = true; # TODO: Added 2024-12-23; switch to false one release after adding a deprecation warning
+      defaultText = lib.literalMD ''`true`, but will change to `false` in a future version.'';
+      description = ''
+        Whether Nixvim should use the host configuration's `pkgs` instance.
+        When false, `pkgs` will be constructed from `nixpkgs.source`.
+
+        > [!TIP]
+        > The host configuration is usually home-manager, nixos, or nix-darwin.
+      '';
+    };
+
     meta.wrapper = {
       name = lib.mkOption {
         type = lib.types.str;
