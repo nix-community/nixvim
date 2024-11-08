c0742ca466dbb36dfdd67f5cbd55aa4df64e4307
bpatel347 <bpatel347@gatech.edu>
2024-11-08 07:58:58 -0500

    Add jupytext python3Dependency to jupytext plugin

--- plugins/by-name/jupytext/default.nix
+++ plugins/by-name/jupytext/default.nix
@@ -61,4 +61,21 @@ helpers.neovim-plugin.mkNeovimPlugin {
       };
     };
   };
+
+  extraOptions = {
+    python3Dependencies = mkOption {
+      type = with types; functionTo (listOf package);
+      default =
+        p: with p; [
+          jupytext
+        ];
+      defaultText = literalExpression ''
+        p: with p; [
+          jupytext
+        ]
+      '';
+      description = "Python packages to add to the `PYTHONPATH` of neovim.";
+    };
+  };
+
 }
