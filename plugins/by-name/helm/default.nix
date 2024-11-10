57068f532d5d42601fd74e2b531204fe1cd3a8f2
Gaetan Lepage <gaetan@glepage.com>
2024-11-10 23:15:04 +0100

    plugins/helm: add recommended autocmd for proper LSP behavior

--- plugins/by-name/helm/default.nix
+++ plugins/by-name/helm/default.nix
@@ -8,5 +8,22 @@ helpers.vim-plugin.mkVimPlugin {
   originalName = "vim-helm";
   package = "vim-helm";
 
+  description = ''
+    To ensure that `helm_ls` (and not `yamlls`) is used on helm files, add the following autocmd:
+
+    ```nix
+      autoCmd = [
+        {
+          event = "FileType";
+          pattern = "helm";
+          command = "LspRestart";
+        }
+      ];
+    ```
+
+    See [nix-community/nixvim#989](https://github.com/nix-community/nixvim/issues/989#issuecomment-2333728503)
+    for details.
+  '';
+
   maintainers = [ lib.maintainers.GaetanLepage ];
 }
