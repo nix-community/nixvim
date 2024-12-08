e2f81c8e8e8baa28b100e0e43b721f16de6299d8
Vincent Desjardins <vdesjardins@gmail.com>
2024-12-08 09:08:51 -0500

    plugins/schemastore: remove table wrapping
    Signed-off-by: Vincent Desjardins <vdesjardins@gmail.com>

--- plugins/by-name/schemastore/default.nix
+++ plugins/by-name/schemastore/default.nix
@@ -136,7 +136,7 @@ helpers.vim-plugin.mkVimPlugin {
     plugins.lsp.servers = {
       jsonls.settings = mkIf cfg.json.enable {
         schemas.__raw = ''
-          require('schemastore').json.schemas({${helpers.toLuaObject cfg.json.settings}})
+          require('schemastore').json.schemas(${helpers.toLuaObject cfg.json.settings})
         '';
 
         # The plugin recommends to enable this option in its README.
@@ -155,7 +155,7 @@ helpers.vim-plugin.mkVimPlugin {
         };
 
         schemas.__raw = ''
-          require('schemastore').yaml.schemas({${helpers.toLuaObject cfg.yaml.settings}})
+          require('schemastore').yaml.schemas(${helpers.toLuaObject cfg.yaml.settings})
         '';
       };
     };
