e60af13695cbbe2e0013ff26f3b3fa1150518aaa
Austin Horstman <khaneliman12@gmail.com>
2024-12-12 20:39:34 -0600

    plugins/lightline: extraConfiguLua -> luaConfig

--- plugins/by-name/lightline/default.nix
+++ plugins/by-name/lightline/default.nix
@@ -22,20 +22,20 @@ lib.nixvim.neovim-plugin.mkNeovimPlugin {
        settings.component_function = {
          readonly = "LightlineReadonly";
        };
-     };
 
-     extraConfigLua = '''
-       function LightlineReadonly()
-         local is_readonly = vim.bo.readonly == 1
-         local filetype = vim.bo.filetype
+       luaConfig.pre= '''
+         function LightlineReadonly()
+           local is_readonly = vim.bo.readonly == 1
+           local filetype = vim.bo.filetype
 
-         if is_readonly and filetype ~= "help" then
-           return "RO"
-         else
-           return ""
+           if is_readonly and filetype ~= "help" then
+             return "RO"
+           else
+             return ""
+           end
          end
-       end
-     ''';
+       ''';
+     };
   '';
 
   # TODO: Added 2024-08-23, remove after 24.11
