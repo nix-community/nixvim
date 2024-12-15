452c30f747b52d798d31deaa07f14b9833a8527f
Gaetan Lepage <gaetan@glepage.com>
2024-12-15 15:52:39 +0100

    plugins/commentary: switch to mkVimPlugin

--- plugins/by-name/commentary/default.nix
+++ plugins/by-name/commentary/default.nix
@@ -1,29 +1,16 @@
 {
   lib,
   helpers,
-  config,
-  pkgs,
   ...
 }:
-with lib;
-let
-  cfg = config.plugins.commentary;
-in
-{
+helpers.vim-plugin.mkVimPlugin {
+  name = "commentary";
+  packPathName = "vim-commentary";
+  package = "vim-commentary";
+
   # TODO Add support for additional filetypes. This requires autocommands!
 
-  options = {
-    plugins.commentary = {
-      enable = mkEnableOption "commentary";
+  maintainers = [ lib.maintainers.GaetanLepage ];
 
-      package = lib.mkPackageOption pkgs "commentary" {
-        default = [
-          "vimPlugins"
-          "vim-commentary"
-        ];
-      };
-    };
-  };
-
-  config = mkIf cfg.enable { extraPlugins = [ cfg.package ]; };
+  # This plugin has no config options
 }
