cb61e12bbac08fc7fd2c672974ccf2d12cdcf236
Gaetan Lepage <gaetan@glepage.com>
2024-12-15 18:04:49 +0100

    plugins/easyescape: switch to mkVimPlugin and add options

--- plugins/by-name/easyescape/default.nix
+++ plugins/by-name/easyescape/default.nix
@@ -1,26 +1,44 @@
 {
   lib,
-  helpers,
-  config,
-  pkgs,
   ...
 }:
-with lib;
 let
-  cfg = config.plugins.easyescape;
+  inherit (lib.nixvim) defaultNullOpts;
+  inherit (lib) types;
 in
-{
-  options = {
-    plugins.easyescape = {
-      enable = mkEnableOption "easyescape";
+lib.nixvim.vim-plugin.mkVimPlugin {
+  name = "easyescape";
+  packPathName = "vim-easyescape";
+  package = "vim-easyescape";
+  globalPrefix = "easyescape_";
 
-      package = lib.mkPackageOption pkgs "easyescape" {
-        default = [
-          "vimPlugins"
-          "vim-easyescape"
-        ];
+  maintainers = [ lib.maintainers.GaetanLepage ];
+
+  settingsOptions = {
+    chars = defaultNullOpts.mkAttrsOf' {
+      type = types.ints.unsigned;
+      pluginDefault = {
+        j = 1;
+        k = 1;
       };
+      example = {
+        j = 2;
+      };
+      description = ''
+        Which keys can be used to escape insert mode and how many times they need to be pressed.
+      '';
     };
+
+    timeout = defaultNullOpts.mkUnsignedInt 100 ''
+      The unit of timeout is in ms.
+
+      A very small timeout makes an input of real `jk` or `kj` possible (Python3 is required for
+      this feature)!
+    '';
   };
-  config = mkIf cfg.enable { extraPlugins = [ cfg.package ]; };
+
+  settingsExample = {
+    chars.j = 2;
+    timeout = 2000;
+  };
 }
--- tests/test-sources/plugins/by-name/easyescape/default.nix
+++ tests/test-sources/plugins/by-name/easyescape/default.nix
@@ -2,4 +2,29 @@
   empty = {
     plugins.easyescape.enable = true;
   };
+
+  defaults = {
+    plugins.easyescape = {
+      enable = true;
+
+      settings = {
+        chars = {
+          j = 1;
+          k = 1;
+        };
+        timeout = 100;
+      };
+    };
+  };
+
+  example = {
+    plugins.easyescape = {
+      enable = true;
+
+      settings = {
+        chars.j = 2;
+        timeout = 2000;
+      };
+    };
+  };
 }
