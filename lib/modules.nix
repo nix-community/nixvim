403d5e23c5fffe53395425234288f29bb5704bca
Matt Sturgeon <matt@sturgeon.me.uk>
2024-12-23 11:54:35 +0000

    lib/modules: remove assertion message for removed `check` arg

--- lib/modules.nix
+++ lib/modules.nix
@@ -21,12 +21,7 @@ in
     {
       modules ? [ ],
       extraSpecialArgs ? { },
-      check ? null, # TODO: Remove stub
-    }@args:
-    # TODO: `check` argument removed 2024-09-24
-    # NOTE: this argument was always marked as experimental
-    assert lib.assertMsg (!args ? "check")
-      "`evalNixvim`: passing `check` is no longer supported. Checks are now done when evaluating `config.build.package` and can be avoided by using `config.build.packageUnchecked` instead.";
+    }:
     # Ensure a suitable `lib` is used
     # TODO: offer a lib overlay that end-users could use to apply nixvim's extensions to their own `lib`
     assert lib.assertMsg (extraSpecialArgs ? lib -> extraSpecialArgs.lib ? nixvim) ''
