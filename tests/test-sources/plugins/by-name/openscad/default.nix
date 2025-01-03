9eb21bbdeab94831d8f4815be0d7575e72f31028
Gaetan Lepage <gaetan@glepage.com>
2025-01-03 18:15:43 +0100

    Revert "tests/openscad: disable on darwin (broken package)"
    This reverts commit 29fbcbc21bc54ed250cdb288bf63df811a013908.

--- tests/test-sources/plugins/by-name/openscad/default.nix
+++ tests/test-sources/plugins/by-name/openscad/default.nix
@@ -1,7 +1,4 @@
-{ lib, pkgs, ... }:
-# Ad of 2024-31-12, openscad is broken on darwin
-# TODO: re-enable when fixed
-lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+{
   empty = {
     plugins.openscad.enable = true;
   };
