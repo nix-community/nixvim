abcbd250b8a2c7aab1f4b2b9e01598ee24b42337
Gaetan Lepage <gaetan@glepage.com>
2025-01-19 11:39:16 +0100

    flake: use stable branch for nix-darwin

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
-      url = "github:lnl7/nix-darwin";
+      url = "github:lnl7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
