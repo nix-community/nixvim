c269e1b9670dbd8166152a9208e1e02ecbc8ccc4
Austin Horstman <khaneliman12@gmail.com>
2025-01-23 19:51:59 -0600

    plugins/markdown-preview: remove helpers

--- plugins/by-name/markdown-preview/default.nix
+++ plugins/by-name/markdown-preview/default.nix
@@ -1,10 +1,10 @@
 {
   lib,
-  helpers,
   ...
 }:
 let
   inherit (lib) types;
+  inherit (lib.nixvim) defaultNullOpts;
 in
 lib.nixvim.plugins.mkVimPlugin {
   name = "markdown-preview";
@@ -42,74 +42,74 @@ lib.nixvim.plugins.mkVimPlugin {
   ];
 
   settingsOptions = {
-    auto_start = helpers.defaultNullOpts.mkFlagInt 0 ''
+    auto_start = defaultNullOpts.mkFlagInt 0 ''
       Open the preview window after entering the markdown buffer.
     '';
 
-    auto_close = helpers.defaultNullOpts.mkFlagInt 1 ''
+    auto_close = defaultNullOpts.mkFlagInt 1 ''
       Auto close current preview window when change from markdown buffer to another buffer.
     '';
 
-    refresh_slow = helpers.defaultNullOpts.mkFlagInt 0 ''
+    refresh_slow = defaultNullOpts.mkFlagInt 0 ''
       Refresh markdown when save the buffer or leave from insert mode, default `0` is auto
       refresh markdown as you edit or move the cursor.
     '';
 
-    command_for_global = helpers.defaultNullOpts.mkFlagInt 0 ''
+    command_for_global = defaultNullOpts.mkFlagInt 0 ''
       Enable markdown preview for all files (by default, the plugin is only enabled for markdown
       files).
     '';
 
-    open_to_the_world = helpers.defaultNullOpts.mkFlagInt 0 ''
+    open_to_the_world = defaultNullOpts.mkFlagInt 0 ''
       Make the preview server available to others in your network.
       By default, the server listens on localhost (127.0.0.1).
     '';
 
-    open_ip = helpers.defaultNullOpts.mkStr "" ''
+    open_ip = defaultNullOpts.mkStr "" ''
       Custom IP used to open the preview page.
       This can be useful when you work in remote vim and preview on local browser.
       For more detail see: https://github.com/iamcco/markdown-preview.nvim/pull/9.
     '';
 
-    browser = helpers.defaultNullOpts.mkStr "" ''
+    browser = defaultNullOpts.mkStr "" ''
       The browser to open the preview page.
     '';
 
-    echo_preview_url = helpers.defaultNullOpts.mkFlagInt 0 ''
+    echo_preview_url = defaultNullOpts.mkFlagInt 0 ''
       Echo preview page url in command line when opening the preview page.
     '';
 
-    browserfunc = helpers.defaultNullOpts.mkStr "" ''
+    browserfunc = defaultNullOpts.mkStr "" ''
       A custom vim function name to open preview page.
       This function will receive url as param.
     '';
 
-    preview_options = helpers.mkNullOrOption (types.submodule {
+    preview_options = lib.nixvim.mkNullOrOption (types.submodule {
       freeformType = types.attrs;
 
       options = {
-        mkit = helpers.defaultNullOpts.mkListOf types.str [ ] ''
+        mkit = defaultNullOpts.mkListOf types.str [ ] ''
           `markdown-it` options for render.
         '';
 
-        katex = helpers.defaultNullOpts.mkListOf types.str [ ] ''
+        katex = defaultNullOpts.mkListOf types.str [ ] ''
           `katex` options for math.
         '';
 
-        uml = helpers.defaultNullOpts.mkListOf types.str [ ] ''
+        uml = defaultNullOpts.mkListOf types.str [ ] ''
           `markdown-it-plantuml` options.
         '';
 
-        maid = helpers.defaultNullOpts.mkListOf types.str [ ] ''
+        maid = defaultNullOpts.mkListOf types.str [ ] ''
           `mermaid` options.
         '';
 
-        disable_sync_scroll = helpers.defaultNullOpts.mkFlagInt 0 ''
+        disable_sync_scroll = defaultNullOpts.mkFlagInt 0 ''
           Disable sync scroll.
         '';
 
         sync_scroll_type =
-          helpers.defaultNullOpts.mkEnumFirstDefault
+          defaultNullOpts.mkEnumFirstDefault
             [
               "middle"
               "top"
@@ -122,62 +122,62 @@ lib.nixvim.plugins.mkVimPlugin {
               - "relative": The cursor position is always shown at the relative position of the preview page.
             '';
 
-        hide_yaml_meta = helpers.defaultNullOpts.mkFlagInt 1 ''
+        hide_yaml_meta = defaultNullOpts.mkFlagInt 1 ''
           Hide yaml metadata.
         '';
 
-        sequence_diagrams = helpers.defaultNullOpts.mkListOf types.str [ ] ''
+        sequence_diagrams = defaultNullOpts.mkListOf types.str [ ] ''
           `js-sequence-diagrams` options.
         '';
 
-        flowchart_diagrams = helpers.defaultNullOpts.mkListOf types.str [ ] ''
+        flowchart_diagrams = defaultNullOpts.mkListOf types.str [ ] ''
           `flowcharts` diagrams options.
         '';
 
-        content_editable = helpers.defaultNullOpts.mkFlagInt 0 ''
+        content_editable = defaultNullOpts.mkFlagInt 0 ''
           Content editable from the preview page.
         '';
 
-        disable_filename = helpers.defaultNullOpts.mkFlagInt 0 ''
+        disable_filename = defaultNullOpts.mkFlagInt 0 ''
           Disable filename header for the preview page.
         '';
 
-        toc = helpers.defaultNullOpts.mkListOf types.str [ ] ''
+        toc = defaultNullOpts.mkListOf types.str [ ] ''
           Toc options.
         '';
       };
     }) "Preview options";
 
-    markdown_css = helpers.defaultNullOpts.mkStr "" ''
+    markdown_css = defaultNullOpts.mkStr "" ''
       Custom markdown style.
       Must be an absolute path like `"/Users/username/markdown.css"` or
       `{__raw = "vim.fn.expand('~/markdown.css')";}`.
     '';
 
-    highlight_css = helpers.defaultNullOpts.mkStr "" ''
+    highlight_css = defaultNullOpts.mkStr "" ''
       Custom highlight style.
       Must be an absolute path like "/Users/username/highlight.css" or
       `{__raw = "vim.fn.expand('~/highlight.css')";}`.
     '';
 
-    port = helpers.defaultNullOpts.mkStr "" ''
+    port = defaultNullOpts.mkStr "" ''
       Custom port to start server or empty for random.
     '';
 
-    page_title = helpers.defaultNullOpts.mkStr "「\$\{name}」" ''
+    page_title = defaultNullOpts.mkStr "「\$\{name}」" ''
       Preview page title.
       `$${name}` will be replaced with the file name.
     '';
 
-    images_path = helpers.defaultNullOpts.mkStr "" ''
+    images_path = defaultNullOpts.mkStr "" ''
       Use a custom location for images.
     '';
 
-    filetypes = helpers.defaultNullOpts.mkListOf types.str [ "markdown" ] ''
+    filetypes = defaultNullOpts.mkListOf types.str [ "markdown" ] ''
       Recognized filetypes. These filetypes will have `MarkdownPreview...` commands.
     '';
 
-    theme = helpers.defaultNullOpts.mkEnum' {
+    theme = defaultNullOpts.mkEnum' {
       values = [
         "dark"
         "light"
@@ -188,13 +188,13 @@ lib.nixvim.plugins.mkVimPlugin {
       pluginDefault = lib.literalMD "chosen based on system preferences";
     };
 
-    combine_preview = helpers.defaultNullOpts.mkFlagInt 0 ''
+    combine_preview = defaultNullOpts.mkFlagInt 0 ''
       Combine preview window.
       If enable it will reuse previous opened preview window when you preview markdown file.
       Ensure to set `auto_close = 0` if you have enable this option.
     '';
 
-    combine_preview_auto_refresh = helpers.defaultNullOpts.mkFlagInt 1 ''
+    combine_preview_auto_refresh = defaultNullOpts.mkFlagInt 1 ''
       Auto refetch combine preview contents when change markdown buffer only when
       `combine_preview` is `1`.
     '';
