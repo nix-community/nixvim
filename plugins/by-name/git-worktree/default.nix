4ae6136d12e16990b07ba262c961ae994d9afed7
Austin Horstman <khaneliman12@gmail.com>
2025-01-23 23:29:41 -0600

    plugins/git-worktree: remove helpers

--- plugins/by-name/git-worktree/default.nix
+++ plugins/by-name/git-worktree/default.nix
@@ -1,6 +1,5 @@
 {
   lib,
-  helpers,
   config,
   pkgs,
   ...
@@ -26,28 +25,28 @@ in
 
       enableTelescope = lib.mkEnableOption "telescope integration";
 
-      changeDirectoryCommand = helpers.defaultNullOpts.mkStr "cd" ''
+      changeDirectoryCommand = lib.nixvim.defaultNullOpts.mkStr "cd" ''
         The vim command used to change to the new worktree directory.
         Set this to `tcd` if you want to only change the `pwd` for the current vim Tab.
       '';
 
-      updateOnChange = helpers.defaultNullOpts.mkBool true ''
+      updateOnChange = lib.nixvim.defaultNullOpts.mkBool true ''
         If set to true updates the current buffer to point to the new work tree if the file is found in the new project.
         Otherwise, the following command will be run.
       '';
 
-      updateOnChangeCommand = helpers.defaultNullOpts.mkStr "e ." ''
+      updateOnChangeCommand = lib.nixvim.defaultNullOpts.mkStr "e ." ''
         The vim command to run during the `update_on_change` event.
         Note, that this command will only be run when the current file is not found in the new worktree.
         This option defaults to `e .` which opens the root directory of the new worktree.
       '';
 
-      clearJumpsOnChange = helpers.defaultNullOpts.mkBool true ''
+      clearJumpsOnChange = lib.nixvim.defaultNullOpts.mkBool true ''
         If set to true every time you switch branches, your jumplist will be cleared so that you don't
         accidentally go backward to a different branch and edit the wrong files.
       '';
 
-      autopush = helpers.defaultNullOpts.mkBool false ''
+      autopush = lib.nixvim.defaultNullOpts.mkBool false ''
         When creating a new worktree, it will push the branch to the upstream then perform a `git rebase`.
       '';
     };
