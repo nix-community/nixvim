e1348ae87cf38396bf15f70eb088b0bce8de5c21
Matt Sturgeon <matt@sturgeon.me.uk>
2025-02-06 11:58:40 +0000

    plugins/blink-cmp-dictionary: use `mkBlinkPluginModule`

--- tests/test-sources/plugins/by-name/blink-cmp-dictionary/default.nix
+++ tests/test-sources/plugins/by-name/blink-cmp-dictionary/default.nix
@@ -1,9 +1,11 @@
 {
   empty = {
+    plugins.blink-cmp.enable = true;
     plugins.blink-cmp-dictionary.enable = true;
   };
 
   defaults = {
+    plugins.blink-cmp.enable = true;
     plugins.blink-cmp-dictionary = {
       enable = true;
     };
