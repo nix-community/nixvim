75900ccb1a812aeb3ef14467df8a61863296b734
Matt Sturgeon <matt@sturgeon.me.uk>
2025-02-06 10:49:07 +0000

    plugins/cmp/auto-enable: warn if used alongside `plugins.*.cmp`

--- plugins/cmp/auto-enable.nix
+++ plugins/cmp/auto-enable.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  options,
   ...
 }:
 let
@@ -25,6 +26,27 @@ let
         (map (lib.getAttr "name"))
         lib.unique
       ];
+
+  newStylePlugins = lib.pipe options.plugins [
+    # First, a manual blacklist
+    (lib.flip builtins.removeAttrs [
+      # lspkind has its own `cmp` options, but isn't a nvim-cmp source
+      "lspkind"
+    ])
+    builtins.attrValues
+    # Filter for non-options (all plugins are plain attrsets, not options)
+    # i.e. remove rename aliases
+    (builtins.filter (opt: !lib.isOption opt))
+    # Filter for plugins that have `cmp` enabled
+    (builtins.filter (opt: opt.cmp.enable.value or false))
+    # Collect the enable options' `loc`s
+    (builtins.catAttrs "enable")
+    (builtins.catAttrs "loc")
+    # Drop the `"enable"` part of the option-loc
+    (builtins.map (lib.lists.dropEnd 1))
+    # Render each plugin loc as an option string
+    (builtins.map lib.showOption)
+  ];
 in
 {
   options = {
@@ -55,9 +77,23 @@ in
     };
   };
 
-  config = lib.mkIf (cfg.enable && cfg.autoEnableSources) (
-    lib.mkMerge [
-      {
+  config = lib.mkMerge (
+    [
+      (lib.mkIf (cfg.enable && cfg.autoEnableSources && newStylePlugins != [ ]) {
+        # Warn when the new and old systems are used together.
+        # `autoEnableSources` is incompatible with the new `plugins.*.cmp` options
+        # TODO:
+        # - Have `autoEnableSources` default to no `plugins.*.cmp` options being enabled?
+        # - Maybe warn when `autoEnableSources` has highestPrio 1500?
+        # - I'm not sure how best to migrate to having `plugins.*.cmp.enable` default to true...
+        warnings = lib.nixvim.mkWarnings "plugins.cmp" ''
+          You have enabled `autoEnableSources` that tells Nixvim to automatically enable the source plugins with respect to the list of sources provided in `settings.sources`.
+          However, ${builtins.toString (builtins.length newStylePlugins)} plugins have cmp integration configured via `plugins.*.cmp`:${
+            lib.concatMapStrings (opt: "\n- `${opt}`") newStylePlugins
+          }
+        '';
+      })
+      (lib.mkIf (cfg.enable && cfg.autoEnableSources) {
         warnings = lib.nixvim.mkWarnings "plugins.cmp" [
           # TODO: expand this warning to ft & cmd sources lists and `showDefs` the offending definitions
           {
@@ -82,21 +118,42 @@ in
           }
         ];
 
-        # If the user has enabled the `foo` and `bar` sources, then `plugins` will look like:
-        # {
-        #   cmp-foo.enable = true;
-        #   cmp-bar.enable = true;
-        # }
-        plugins = lib.mapAttrs' (source: name: {
-          inherit name;
-          value.enable = lib.mkIf (lib.elem source enabledSources) true;
-        }) config.cmpSourcePlugins;
-      }
-      {
         plugins.lsp.capabilities = lib.mkIf (lib.elem "nvim_lsp" enabledSources) ''
           capabilities = vim.tbl_deep_extend("force", capabilities, require('cmp_nvim_lsp').default_capabilities())
         '';
-      }
+      })
+    ]
+    # If the user has enabled the `foo` and `bar` sources, then `plugins` will look like:
+    # {
+    #   cmp-foo.enable = true;
+    #   cmp-bar.enable = true;
+    # }
+    #
+    # To avoid inf-rec, we have to define _all_ declared plugins,
+    # outside of any conditional that depends on config.plugins.
+    # We can use mkIf _within_ definitions though.
+    ++ lib.pipe options.plugins [
+      (lib.flip builtins.removeAttrs [
+        # lspkind has its own `cmp` options, but isn't a nvim-cmp source
+        "lspkind"
+      ])
+      # Actual options are probably aliases, not plugins
+      (lib.filterAttrs (_: opt: !lib.isOption opt))
+      (lib.filterAttrs (_: opt: opt ? cmp))
+      builtins.attrNames
+      (builtins.map (
+        name:
+        let
+          inherit (config.plugins.${name}) cmp;
+        in
+        lib.mkIf (
+          cfg.enable
+          && cfg.autoEnableSources
+          # Avoid inf-rec by not auto-enabling sources that add themselves to sources
+          && !cmp.enable or false
+          && builtins.elem cmp.name enabledSources
+        ) { plugins.${name}.enable = true; }
+      ))
     ]
   );
 }
