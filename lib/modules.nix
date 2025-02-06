a9b720fcac3bb639dcfbe41b5288b94a7a8cf6ed
Matt Sturgeon <matt@sturgeon.me.uk>
2025-02-06 01:51:19 +0000

    lib/modules: init `mkCmpPluginModule`

--- lib/modules.nix
+++ lib/modules.nix
@@ -190,6 +190,159 @@ in
         })
       ];
     };
+
+  # Create a module configuring a plugin's integration with nvim-cmp and blink.cmp
+  mkCmpPluginModule =
+    {
+      # The plugin's option location-path
+      loc ? [
+        "plugins"
+        pluginName
+      ],
+      # Name of the plugin, used in documentation
+      pluginName,
+      # The nvim-cmp source name
+      # TODO: can we compute a sane default for sourceName?
+      sourceName,
+      # Defaults for the corresponding cmp options
+      enableDefault ? true,
+      enableCmdline ? { },
+      enabledFiletypes ? { },
+      # Whether to include a `blink` option at all
+      offerBlinkCompatibility ? true,
+      # Defaults for the blink compatibility option
+      enableBlinkProvider ? false,
+      enableBlinkDefault ? false,
+      enableBlinkCmdline ? false,
+      enabledBlinkFiletypes ? { },
+      # Whether the plugin's settings should be used as the blink provider's `opts`
+      usePluginSettingsForBlink ? false,
+      # The key to use with blink,
+      # i.e. the attr name for `sources.providers.<name>`
+      # TODO: should this default to pluginName or sourceName?
+      blinkProviderKey ? lib.strings.toLower pluginName,
+    }:
+    { config, options, ... }:
+    let
+      pluginOpt = lib.getAttrFromPath loc options;
+      pluginCfg = lib.getAttrFromPath loc config;
+      blinkOpt = pluginOpt.blink;
+      blinkCfg = pluginCfg.blink;
+      cfg = pluginCfg.cmp;
+      toSourceDef = v: lib.optionalAttrs (builtins.isAttrs v) v // { inherit (cfg) name; };
+      toSources = v: { sources = [ (toSourceDef v) ]; };
+    in
+    {
+      imports = lib.optionals offerBlinkCompatibility [
+        (lib.nixvim.modules.mkBlinkPluginModule {
+          inherit loc pluginName sourceName;
+          key = blinkProviderKey;
+          module = "blink.compat.source";
+          enableProvider = enableBlinkProvider;
+          enableDefault = enableBlinkDefault;
+          enableCmdline = enableBlinkCmdline;
+          enabledFiletypes = enabledBlinkFiletypes;
+          usePluginSettings = usePluginSettingsForBlink;
+        })
+        {
+          options = lib.setAttrByPath loc {
+            blink.settings.name = internal;
+          };
+          config = lib.mkIf (pluginCfg.enable && blinkCfg.enable) {
+            # Enable blink-compat if the plugin has `blink.enable = true`
+            plugins.blink-compat.enable = true;
+            # This warning will show if someone overrides `plugins.blink-compat.enable = mkForce false`
+            warnings = lib.nixvim.mkWarnings (lib.showOption loc) {
+              when = !config.plugins.blink-compat.enable;
+              message = ''
+                `${blinkOpt.enable}` is enabled, but `${options.plugins.blink-compat.enable}` is not.
+                This plugin is a nvim-cmp source, so it requires blink.compat when used with blink.cmp.
+              '';
+            };
+          };
+        }
+      ];
+      options = lib.setAttrByPath loc {
+        cmp = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            # FIXME: this should default to true, but it is incompatible with autoEnableCmpSources
+            default = false;
+            example = true;
+            description = ''
+              Whether to integrate this plugin with nvim-cmp.
+            '';
+          };
+          name = lib.mkOption {
+            type = lib.types.str;
+            default = sourceName;
+            description = "${pluginName}'s nvim-cmp source name.";
+            internal = true;
+          };
+          default = lib.mkOption {
+            type = with lib.types; either bool (attrsOf anything);
+            default = enableDefault;
+            example = !enableDefault;
+            description = ''
+              Whether to include this plugin in the `default` completion source list.
+
+              Can be defined as attrs to pass additional config to the source.
+            '';
+          };
+          cmdline = lib.mkOption {
+            type = with lib.types; attrsOf (either bool (attrsOf anything));
+            # Remove false attrs in the final value
+            apply = lib.filterAttrs (_: v: v != false);
+            default = enableCmdline;
+            # TODO: example
+            description = ''
+              Whether to include this plugin in the specific `cmdline` completion source lists.
+
+              Elements can be defined as attrs to pass additional config to the source.
+            '';
+          };
+          filetypes = lib.mkOption {
+            type = with lib.types; attrsOf (either bool (attrsOf anything));
+            # Remove false attrs in the final value
+            apply = lib.filterAttrs (_: v: v != false);
+            default = enabledFiletypes;
+            # TODO: example
+            description = ''
+              Whether to include this plugin in the specific `per_filetype` completion source lists.
+
+              Elements can be defined as attrs to pass additional config to the source.
+            '';
+          };
+        };
+      };
+      config = lib.mkMerge [
+        {
+          # Backwards compatibility with autoEnableCmpSources
+          cmpSourcePlugins.${sourceName} =
+            assert lib.assertMsg (builtins.length loc == 2 && builtins.head loc == "plugins")
+              "autoEnableCmpSources assumes all cmp source plugins are in the `plugins` namespace. Unsupported plugin `${lib.showOption loc}`.";
+            pluginName;
+        }
+        (lib.mkIf (pluginCfg.enable && cfg.enable) {
+          # FIXME: even though we have the mkIf, we also need optionalAttrs
+          # to avoid inf-recursion caused by `autoEnableSources`
+          plugins.cmp = lib.optionalAttrs cfg.enable {
+            # TODO: consider setting:
+            # autoEnableSources = lib.mkDefault false;
+            settings = lib.mkIf (cfg.default != false) (toSources cfg.default);
+            cmdline = lib.mkIf (cfg.cmdline != { }) (builtins.mapAttrs (_: toSources) cfg.cmdline);
+            filetype = lib.mkIf (cfg.filetypes != { }) (builtins.mapAttrs (_: toSources) cfg.filetypes);
+          };
+          warnings = lib.nixvim.mkWarnings (lib.showOption loc) {
+            when = !config.plugins.cmp.enable && options.plugins.cmp.enable.highestPrio == 1500;
+            message = ''
+              You have enabled the nvim-cmp source, but `plugins.cmp` is not enabled.
+              You can suppress this warning by explicitly setting `plugins.cmp.enable = false`.
+            '';
+          };
+        })
+      ];
+    };
 }
 // lib.mapAttrs (
   name: msg:
