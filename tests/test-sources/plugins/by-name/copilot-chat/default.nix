a3ab36e17a18d4c8460427cc9c8bd8960b3dc0b4
Gaetan Lepage <gaetan@glepage.com>
2025-02-20 21:23:54 +0100

    plugins/copilot-chat: wip

--- tests/test-sources/plugins/by-name/copilot-chat/default.nix
+++ tests/test-sources/plugins/by-name/copilot-chat/default.nix
@@ -8,116 +8,13 @@
     plugins.copilot-chat = {
       enable = true;
 
-      settings = {
-        debug = false;
-        proxy = null;
-        allow_insecure = false;
-        system_prompt = "require('CopilotChat.prompts').COPILOT_INSTRUCTIONS";
-        model = "gpt-4";
-        temperature = 0.1;
-        question_header = "## User ";
-        answer_header = "## Copilot ";
-        error_header = "## Error ";
-        separator = "───";
-        show_folds = true;
-        show_help = true;
-        auto_follow_cursor = true;
-        auto_insert_mode = false;
-        clear_chat_on_new_prompt = false;
-        highlight_selection = true;
-        context = null;
-        history_path.__raw = "vim.fn.stdpath('data') .. '/copilotchat_history'";
-        callback = null;
-        selection = ''
-          function(source)
-            local select = require('CopilotChat.select')
-            return select.visual(source) or select.line(source)
-          end
-        '';
-        prompts = {
-          Explain.prompt = "/COPILOT_EXPLAIN Write an explanation for the active selection as paragraphs of text.";
-          Review = {
-            prompt = "/COPILOT_REVIEW Review the selected code.";
-            callback = ''
-              function(response, source)
-                -- see config.lua for implementation
-              end
-            '';
-          };
-          Fix.prompt = "/COPILOT_GENERATE There is a problem in this code. Rewrite the code to show it with the bug fixed.";
-          Optimize.prompt = "/COPILOT_GENERATE Optimize the selected code to improve performance and readablilty.";
-          Docs.prompt = "/COPILOT_GENERATE Please add documentation comment for the selection.";
-          Tests.prompt = "/COPILOT_GENERATE Please generate tests for my code.";
-          FixDiagnostic = {
-            prompt = "Please assist with the following diagnostic issue in file:";
-            selection = "require('CopilotChat.select').diagnostics";
-          };
-          Commit = {
-            prompt = "Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.";
-            selection = "require('CopilotChat.select').gitdiff";
-          };
-          CommitStaged = {
-            prompt = "Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.";
-            selection = ''
-              function(source)
-                return select.gitdiff(source, true)
-              end
-            '';
-          };
-        };
-
-        window = {
-          layout = "vertical";
-          width = 0.5;
-          height = 0.5;
-          relative = "editor";
-          border = "single";
-          row = null;
-          col = null;
-          title = "Copilot Chat";
-          footer = null;
-          zindex = 1;
-        };
-        mappings = {
-          complete.insert = "<Tab>";
-          close = {
-            normal = "q";
-            insert = "<C-c>";
-          };
-          reset = {
-            normal = "<C-l>";
-            insert = "<C-l>";
-          };
-          submit_prompt = {
-            normal = "<CR>";
-            insert = "<C-s>";
-          };
-          toggle_sticky = {
-            detail = "Makes line under cursor sticky or deletes sticky line.";
-            normal = "gr";
-          };
-          accept_diff = {
-            normal = "<C-y>";
-            insert = "<C-y>";
-          };
-          jump_to_diff.normal = "gj";
-          quickfix_diffs.normal = "gq";
-          yank_diff = {
-            normal = "gy";
-            register = "\"";
-          };
-          show_diff.normal = "gd";
-          show_info.normal = "gi";
-          show_context.normal = "gc";
-          show_help.normal = "gh";
-        };
-      };
+      settings = import ./default-settings;
     };
   };
 
   example = {
     plugins.copilot-chat = {
-      enable = true;
+      enable = false;
 
       settings = {
         question_header = "## User ";
