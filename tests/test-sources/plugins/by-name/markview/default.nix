3feb4b4b4be47063d21cc6eef6bbe9dba7129dc9
Austin Horstman <khaneliman12@gmail.com>
2025-02-21 08:37:51 -0600

    plugins/markview: v25 migration
    Co-Authored-By: Matt Sturgeon <matt@sturgeon.me.uk>

--- tests/test-sources/plugins/by-name/markview/default.nix
+++ tests/test-sources/plugins/by-name/markview/default.nix
@@ -8,37 +8,20 @@
       enable = true;
 
       settings = {
-        buf_ignore = [ "nofile" ];
-        modes = [
-          "n"
-          "no"
-        ];
-        hybrid_modes = [ ];
-        callback = {
-          on_enable = # Lua
-            ''
-              function(buf, win)
-                vim.wo[window].conceallevel = 2;
-                vim.wo[window].concealcursor = "nc";
-              end
-            '';
-          on_disable = # Lua
-            ''
-              function(buf, win)
-                  vim.wo[window].conceallevel = 0;
-                  vim.wo[window].concealcursor = "";
-              end
-            '';
-          on_mode_change = # Lua
-            ''
-              function(buf, win, mode)
-                if vim.list_contains(markview.configuration.modes, mode) then
-                  vim.wo[window].conceallevel = 2;
-                else
-                  vim.wo[window].conceallevel = 0;
-                end
-              end
-            '';
+        preview = {
+          enable = true;
+          buf_ignore = [ ];
+          icon_provider = "internal";
+          filetypes = [ ];
+          hybrid_modes = [ ];
+          ignore_previews = [ ];
+          max_buf_lines = 1000;
+          modes = [ ];
+          render_distance = [
+            200
+            200
+          ];
+          splitview_winopts = { };
         };
       };
     };
