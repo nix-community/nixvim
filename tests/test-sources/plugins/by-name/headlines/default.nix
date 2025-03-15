5519b9d4136a8278e1886cfccec1f4bf891b4d23
Gaetan Lepage <gaetan@glepage.com>
2025-03-15 15:34:27 +0100

    tests/plugins/headlines: disable org-related tests

--- tests/test-sources/plugins/by-name/headlines/default.nix
+++ tests/test-sources/plugins/by-name/headlines/default.nix
@@ -168,55 +168,58 @@
             fat_headline_upper_string = "▃";
             fat_headline_lower_string = "🬂";
           };
-          org = {
-            query.__raw = ''
-              vim.treesitter.query.parse(
-                "org",
-                [[
-                    (headline (stars) @headline)
+          # Fails with:
+          # no parser for 'org' language, see :help treesitter-parsers
+          # TODO: re-enable when possible
+          # org = {
+          #   query.__raw = ''
+          #     vim.treesitter.query.parse(
+          #       "org",
+          #       [[
+          #           (headline (stars) @headline)
 
-                    (
-                        (expr) @dash
-                        (#match? @dash "^-----+$")
-                    )
+          #           (
+          #               (expr) @dash
+          #               (#match? @dash "^-----+$")
+          #           )
 
-                    (block
-                        name: (expr) @_name
-                        (#match? @_name "(SRC|src)")
-                    ) @codeblock
+          #           (block
+          #               name: (expr) @_name
+          #               (#match? @_name "(SRC|src)")
+          #           ) @codeblock
 
-                    (paragraph . (expr) @quote
-                        (#eq? @quote ">")
-                    )
-                ]]
-              )
-            '';
-            headline_highlights = [ "Headline" ];
-            bullet_highlights = [
-              "@org.headline.level1"
-              "@org.headline.level2"
-              "@org.headline.level3"
-              "@org.headline.level4"
-              "@org.headline.level5"
-              "@org.headline.level6"
-              "@org.headline.level7"
-              "@org.headline.level8"
-            ];
-            bullets = [
-              "◉"
-              "○"
-              "✸"
-              "✿"
-            ];
-            codeblock_highlight = "CodeBlock";
-            dash_highlight = "Dash";
-            dash_string = "-";
-            quote_highlight = "Quote";
-            quote_string = "┃";
-            fat_headlines = true;
-            fat_headline_upper_string = "▃";
-            fat_headline_lower_string = "🬂";
-          };
+          #           (paragraph . (expr) @quote
+          #               (#eq? @quote ">")
+          #           )
+          #       ]]
+          #     )
+          #   '';
+          #   headline_highlights = [ "Headline" ];
+          #   bullet_highlights = [
+          #     "@org.headline.level1"
+          #     "@org.headline.level2"
+          #     "@org.headline.level3"
+          #     "@org.headline.level4"
+          #     "@org.headline.level5"
+          #     "@org.headline.level6"
+          #     "@org.headline.level7"
+          #     "@org.headline.level8"
+          #   ];
+          #   bullets = [
+          #     "◉"
+          #     "○"
+          #     "✸"
+          #     "✿"
+          #   ];
+          #   codeblock_highlight = "CodeBlock";
+          #   dash_highlight = "Dash";
+          #   dash_string = "-";
+          #   quote_highlight = "Quote";
+          #   quote_string = "┃";
+          #   fat_headlines = true;
+          #   fat_headline_upper_string = "▃";
+          #   fat_headline_lower_string = "🬂";
+          # };
         };
       };
     };
