d81f37256d0a8691b837b74979d27bf89be8ecdd
Axel Karjalainen <axel@axka.fi>
2025-03-17 01:33:22 +0200

    plugins/aw-watcher: init

---
+++ tests/test-sources/plugins/by-name/aw-watcher/default.nix
@@ -0,0 +1,44 @@
+{
+  empty = {
+    # don't run tests as they try to access the network.
+    test.runNvim = false;
+    plugins.aw-watcher.enable = true;
+  };
+
+  defaults = {
+    # don't run tests as they try to access the network.
+    test.runNvim = false;
+    plugins.aw-watcher = {
+      enable = true;
+
+      settings = {
+        bucket = {
+          hostname = "nixvim";
+          name = "aw-watcher-neovim_nixvim";
+        };
+        aw_server = {
+          host = "127.0.0.1";
+          port = 5600;
+          ssl_enable = false;
+          pulsetime = 30;
+        };
+      };
+    };
+  };
+
+  example = {
+    # don't run tests as they try to access the network.
+    test.runNvim = false;
+    plugins.aw-watcher = {
+      enable = true;
+
+      settings = {
+        aw_server = {
+          host = "127.0.0.1";
+          port = 5600;
+          pulsetime = 60;
+        };
+      };
+    };
+  };
+}
