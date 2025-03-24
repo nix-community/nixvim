35ec52f1d98c379747a5c884a54822c9508da221
Gaetan Lepage <gaetan@glepage.com>
2025-03-24 08:26:39 +0100

    tests/plugins/texpresso: disable as the texpresso package is broken

--- tests/test-sources/plugins/by-name/texpresso/default.nix
+++ tests/test-sources/plugins/by-name/texpresso/default.nix
@@ -1,5 +1,8 @@
 {
   empty = {
-    plugins.texpresso.enable = true;
+    # TODO: added 2025-03-24
+    # pkgs.texpresso is broken
+    # https://github.com/NixOS/nixpkgs/issues/392649
+    plugins.texpresso.enable = false;
   };
 }
