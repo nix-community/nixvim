f795bd8284079433132a098162e61ada8fa5c456
Gaetan Lepage <gaetan@glepage.com>
2025-03-25 16:44:49 +0100

    TEST

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A neovim configuration system for NixOS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
