cb60b0d10a4db4420828513cc0c2e818947c78fe
Gaetan Lepage <gaetan@glepage.com>
2025-03-26 16:36:46 +0100

    TEST

--- flake/dev/flake.nix
+++ flake/dev/flake.nix
@@ -2,7 +2,7 @@
   description = "Private inputs for development purposes. These are used by the top level flake in the `dev` partition, but do not appear in consumers' lock files.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:GaetanLepage/nixpkgs/neovim";
 
     home-manager = {
       url = "github:nix-community/home-manager";
