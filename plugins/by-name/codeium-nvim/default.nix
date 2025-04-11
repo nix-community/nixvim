f046e5fd102c50629e0119b00079e5ab5c832cba
Gaetan Lepage <gaetan@glepage.com>
2025-04-11 15:44:57 +0200

    plugins/codeium-nvim: use dependencies

--- plugins/by-name/codeium-nvim/default.nix
+++ plugins/by-name/codeium-nvim/default.nix
@@ -1,7 +1,16 @@
 {
   lib,
+  config,
   ...
 }:
+let
+  inherit (lib)
+    getExe
+    getExe'
+    mkIf
+    mkDefault
+    ;
+in
 lib.nixvim.plugins.mkNeovimPlugin {
   name = "codeium-nvim";
   packPathName = "codeium.nvim";
@@ -12,20 +21,38 @@ lib.nixvim.plugins.mkNeovimPlugin {
     khaneliman
   ];
 
+  description = ''
+    By default, Nixvim will install the `curl`, `gzip`, `coreutils`, `util-linux` and `codeium` packages and populate settings.tools.*` accordingly.
+
+    You are free to override `dependencies.*.enable` and `dependencies.*.package` to respectively disable and customize this behavior.
+  '';
+
   # Register nvim-cmp association
   imports = [
     { cmpSourcePlugins.codeium = "codeium-nvim"; }
   ];
 
-  settingsExample = lib.literalExpression ''
-    {
-      enable_chat = true;
-      tools = {
-        curl = lib.getExe pkgs.curl;
-        gzip = lib.getExe pkgs.gzip;
-        uname = lib.getExe' pkgs.coreutils "uname";
-        uuidgen = lib.getExe' pkgs.util-linux "uuidgen";
+  settingsExample = {
+    enable_chat = true;
+  };
+
+  extraConfig = {
+    dependencies = lib.genAttrs [ "curl" "gzip" "coreutils" "util-linux" "codeium" ] (_: {
+      enable = lib.mkDefault true;
+    });
+
+    plugins.codeium-nvim.settings.tools =
+      let
+        depsCfg = config.dependencies;
+      in
+      {
+        curl = mkIf depsCfg.curl.enable (mkDefault (getExe depsCfg.curl.package));
+        gzip = mkIf depsCfg.gzip.enable (mkDefault (getExe depsCfg.gzip.package));
+        coreutils = mkIf depsCfg.coreutils.enable (mkDefault (getExe' depsCfg.coreutils.package "uname"));
+        util-linux = mkIf depsCfg.util-linux.enable (
+          mkDefault (getExe' depsCfg.util-linux.package "uuidgen")
+        );
+        language_server = mkIf depsCfg.codeium.enable (getExe depsCfg.codeium.package);
       };
-    }
-  '';
+  };
 }
