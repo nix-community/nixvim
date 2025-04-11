72ce6dbdf537d89556dc6721029ac9e34b391029
Gaetan Lepage <gaetan@glepage.com>
2025-04-11 15:42:21 +0200

    tests/modules/dependencies: test all package examples

--- tests/test-sources/modules/dependencies.nix
+++ tests/test-sources/modules/dependencies.nix
@@ -20,4 +20,29 @@
         enable = lib.meta.availableOn pkgs.stdenv.hostPlatform depOption.package.default;
       }) options.dependencies;
     };
+
+  all-examples =
+    {
+      lib,
+      pkgs,
+      options,
+      ...
+    }:
+    {
+      dependencies = lib.pipe options.dependencies [
+        (lib.filterAttrs (_: depOption: depOption.package ? example))
+        (lib.mapAttrs (
+          _: depOption:
+          let
+            packageName = depOption.package.example.text;
+            packagePath = lib.splitString "." packageName;
+            package = lib.attrByPath packagePath (throw "${packageName} not found in pkgs") pkgs;
+          in
+          {
+            enable = lib.meta.availableOn pkgs.stdenv.hostPlatform package;
+            inherit package;
+          }
+        ))
+      ];
+    };
 }
