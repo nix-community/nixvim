faa31d994cb5c2f320869dbb21252e21abcda3fc
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2025-04-27 10:39:40 +0300

    Revert "tests/modules-performance: add nvim-cmp to extraPlugins when necessary"
    This reverts commit bb5b0a26556017f1f3a4c07f8bd20dee88f6ffb0.
    
    This change defeats the purpose of the dependency test. Revert it.
    The test should verify that dependencies are correctly pulled. If
    upstream plugin doesn't have dependency anymore, then a suitable
    alternative should be used, not dependencies added manually.

--- tests/test-sources/modules/performance/combine-plugins.nix
+++ tests/test-sources/modules/performance/combine-plugins.nix
@@ -64,8 +64,6 @@ in
       extraPlugins = with pkgs.vimPlugins; [
         # Depends on nvim-cmp
         cmp-dictionary
-        # We have to manually add cmp-dictionary's dependence: nvim-cmp
-        nvim-cmp
         # Depends on telescope-nvim which itself depends on plenary-nvim
         telescope-undo-nvim
       ];
@@ -334,8 +332,6 @@ in
         telescope-nvim
         # Only its dependency (nvim-cmp) won't be combined, but not the plugin itself
         cmp-dictionary
-        # We have to manually add cmp-dictionary's dependence: nvim-cmp
-        nvim-cmp
         # More plugins
         gitsigns-nvim
         luasnip
