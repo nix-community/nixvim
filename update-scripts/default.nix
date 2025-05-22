7a4c70c55f5db77da15364d822dfd3660cf4bd0a
Matt Sturgeon <matt@sturgeon.me.uk>
2025-05-22 16:29:30 +0100

    update-scripts/version-info: init
    Writes a version-info.toml file.

--- update-scripts/default.nix
+++ update-scripts/default.nix
@@ -17,4 +17,5 @@ lib.fix (self: {
   none-ls-builtins = pkgs.callPackage ./none-ls.nix { };
   rust-analyzer-options = pkgs.callPackage ./rust-analyzer { };
   lspconfig-servers = pkgs.callPackage ./nvim-lspconfig { };
+  version-info = pkgs.callPackage ./version-info { };
 })
