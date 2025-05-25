2431cdc047e7d514e6b511aa73edcd4f95126f0e
Matt Sturgeon <matt@sturgeon.me.uk>
2025-05-25 17:56:54 +0100

    ci/check: group test-N derivations into one drv

--- flake/dev/tests.nix
+++ flake/dev/tests.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   self,
   inputs,
   helpers,
@@ -15,6 +16,23 @@
 
   # Output a build matrix for CI
   flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
-    inherit (self) checks;
+    checks = builtins.mapAttrs (
+      system:
+      lib.flip lib.pipe [
+        lib.attrsToList
+        # Group the "test-N" tests back into one drv
+        # FIXME: drop the entire test-grouping system
+        (builtins.groupBy ({ name, value }: if lib.strings.hasPrefix "test-" name then "test" else name))
+        (builtins.mapAttrs (
+          group: tests:
+          let
+            pkgs = inputs.nixpkgs.legacyPackages.${system};
+            singleton = (builtins.head tests).value;
+            joined = pkgs.linkFarmFromDrvs group (builtins.listToAttrs tests);
+          in
+          if builtins.length tests > 1 then joined else singleton
+        ))
+      ]
+    ) self.checks;
   };
 }
