9863e247a52deb0ff3da16e9b2a4aa86f3d2cc4a
Matt Sturgeon <matt@sturgeon.me.uk>
2025-05-25 21:44:27 +0100

    ci: filter out test-N checks for now

--- flake/dev/tests.nix
+++ flake/dev/tests.nix
@@ -17,22 +17,22 @@
   # Output a build matrix for CI
   flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
     checks = builtins.mapAttrs (
-      system:
-      lib.flip lib.pipe [
-        lib.attrsToList
-        # Group the "test-N" tests back into one drv
-        # FIXME: drop the entire test-grouping system
-        (builtins.groupBy ({ name, value }: if lib.strings.hasPrefix "test-" name then "test" else name))
-        (builtins.mapAttrs (
-          group: tests:
-          let
-            pkgs = inputs.nixpkgs.legacyPackages.${system};
-            singleton = (builtins.head tests).value;
-            joined = pkgs.linkFarm group (builtins.listToAttrs tests);
-          in
-          if builtins.length tests > 1 then joined else singleton
-        ))
-      ]
+      system: lib.filterAttrs (name: v: !lib.strings.hasPrefix "test-" name)
+      # lib.flip lib.pipe [
+      #   lib.attrsToList
+      #   # Group the "test-N" tests back into one drv
+      #   # FIXME: drop the entire test-grouping system
+      #   (builtins.groupBy ({ name, value }: if lib.strings.hasPrefix "test-" name then "test" else name))
+      #   (builtins.mapAttrs (
+      #     group: tests:
+      #     let
+      #       pkgs = inputs.nixpkgs.legacyPackages.${system};
+      #       singleton = (builtins.head tests).value;
+      #       joined = pkgs.linkFarm group (builtins.listToAttrs tests);
+      #     in
+      #     if builtins.length tests > 1 then joined else singleton
+      #   ))
+      # ]
     ) self.checks;
   };
 }
