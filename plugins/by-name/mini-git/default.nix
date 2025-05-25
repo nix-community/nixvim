c59fad7efb56fa7aef1501e3ff92baa72ea5ce23
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-05-25 15:47:26 -0300

    plugins/mini-git: init

---
+++ plugins/by-name/mini-git/default.nix
@@ -0,0 +1,15 @@
+{ lib, ... }:
+lib.nixvim.plugins.mkNeovimPlugin {
+  name = "mini-git";
+  moduleName = "mini.git";
+  packPathName = "mini.git";
+
+  maintainers = [ lib.maintainers.HeitorAugustoLN ];
+
+  dependencies = [ "git" ];
+
+  settingsExample = {
+    job.timeout = 20000;
+    command.split = "horizontal";
+  };
+}
