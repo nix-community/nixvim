2e4f383197d63008709e8b28229ce0cb67ed94c5
Alex Hamilton <1622250+Aehmlo@users.noreply.github.com>
2025-05-25 13:53:15 -0400

    flake/wrappers: Deprecate homeManagerModules output

--- flake/wrappers.nix
+++ flake/wrappers.nix
@@ -20,8 +20,14 @@
       nixvim = import ../wrappers/nixos.nix self;
       default = self.nixosModules.nixvim;
     };
-    # For backward compatibility
-    homeManagerModules = self.homeModules;
+    # Alias for backward compatibility
+    # Added 2025-05-25 in https://github.com/nix-community/nixvim/pull/3387
+    homeManagerModules =
+      let
+        cond = lib.trivial.oldestSupportedReleaseIsAtLeast 2505;
+        msg = "nixvim: flake output `homeManagerModules` has been renamed to `homeModules`.";
+      in
+      lib.warnIf cond msg self.homeModules;
     homeModules = {
       nixvim = import ../wrappers/hm.nix self;
       default = self.homeModules.nixvim;
