6b2e98bc471efb9e6a3796f2f77506dda160dc27
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-05-25 16:08:57 -0300

    plugins/mini-tabline: init

---
+++ tests/test-sources/plugins/by-name/mini-tabline/default.nix
@@ -0,0 +1,15 @@
+{
+  empty = {
+    plugins.mini-tabline.enable = true;
+  };
+
+  example = {
+    plugins.mini-tabline = {
+      enable = true;
+      settings = {
+        show_icons = false;
+        tabpage_section = "right";
+      };
+    };
+  };
+}
