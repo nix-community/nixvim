7d0ac00557dd97d4353cfcad8ffb65163ac1d782
Matt Sturgeon <matt@sturgeon.me.uk>
2025-05-30 17:37:23 +0100

    flake/ci: re-enable most tests on buildbot
    Most still run on all platforms, but many now only run on x86_64-linux.

--- flake/dev/tests.nix
+++ flake/dev/tests.nix
@@ -7,8 +7,15 @@
   perSystem =
     { pkgs, ... }:
     {
+      # TODO: consider whether all these tests are needed in the `checks` output
       checks = pkgs.callPackages ../../tests {
         inherit helpers self;
+      };
+
+      # TODO: consider whether all these tests are needed to be built by buildbot
+      ci.buildbot = pkgs.callPackages ../../tests {
+        inherit helpers self;
+        allSystems = false;
       };
     };
 }
