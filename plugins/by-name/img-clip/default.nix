6f4c9ad3fdb415e878bad4d528c79559f42f9c6d
saygo-png <saygo.mail@proton.me>
2025-07-16 21:19:44 +0200

    plugins/img-clip: init
    Signed-off-by: saygo-png <saygo.mail@proton.me>

---
+++ plugins/by-name/img-clip/default.nix
@@ -0,0 +1,23 @@
+{ lib, ... }:
+lib.nixvim.plugins.mkNeovimPlugin {
+  name = "img-clip";
+  packPathName = "img-clip.nvim";
+  package = "img-clip-nvim";
+  description = "Embed images into any markup language, like LaTeX, Markdown or Typst";
+  maintainers = [ lib.maintainers.saygo-png ];
+
+  settingsExample = {
+    default = {
+      dir_path = "assets";
+      file_name = "%y-%m-%d-%h-%m-%s";
+      use_absolute_path = false;
+      relative_to_current_file = false;
+      template = "$file_path";
+    };
+    filetypes = {
+      markdown = {
+        download_images = true;
+      };
+    };
+  };
+}
