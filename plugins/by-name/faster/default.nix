d438e42dfc2746fc64ead878594fbd601aab3067
saygo-png <saygo.mail@proton.me>
2025-07-21 20:58:40 +0200

    plugins/faster: init
    Signed-off-by: saygo-png <saygo.mail@proton.me>

---
+++ plugins/by-name/faster/default.nix
@@ -0,0 +1,43 @@
+{ lib, ... }:
+lib.nixvim.plugins.mkNeovimPlugin {
+  name = "faster";
+  packPathName = "faster.nvim";
+  package = "faster-nvim";
+  description = "Selectively disable some features when a big file is opened or macro is executed.";
+  maintainers = [ lib.maintainers.saygo-png ];
+
+  settingsExample = {
+    behaviours = {
+      bigfile = {
+        on = true;
+        features_disabled = [
+          "lsp"
+          "treesitter"
+        ];
+        filesize = 2;
+        pattern = "*";
+        extra_patterns = [
+          {
+            filesize = 1.1;
+            pattern = "*.md";
+          }
+          { pattern = "*.log"; }
+        ];
+      };
+      fastmacro = {
+        on = true;
+        features_disabled = [ "lualine" ];
+      };
+    };
+    features = {
+      lsp = {
+        on = true;
+        defer = false;
+      };
+      treesitter = {
+        on = true;
+        defer = false;
+      };
+    };
+  };
+}
