4499c5f67506cf928f52cffa87aff10d04f8e926
saygo-png <saygo.mail@proton.me>
2025-07-21 19:00:56 +0200

    tests/nvim-tree: update test
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- tests/test-sources/plugins/by-name/nvim-tree/default.nix
+++ tests/test-sources/plugins/by-name/nvim-tree/default.nix
@@ -7,253 +7,251 @@
   defaults = {
     plugins.web-devicons.enable = true;
     plugins.nvim-tree = {
-      enable = true;
-
-      disableNetrw = true;
-      hijackNetrw = false;
-
       openOnSetup = true;
       openOnSetupFile = true;
+      autoClose = true;
       ignoreBufferOnSetup = true;
       ignoreFtOnSetup = [ "tex" ];
-      autoClose = true;
-
-      autoReloadOnWrite = true;
-      sortBy = "name";
-      hijackUnnamedBufferWhenOpening = false;
-      hijackCursor = false;
-      rootDirs = [ ];
-      preferStartupRoot = false;
-      syncRootWithCwd = false;
-      reloadOnBufenter = false;
-      respectBufCwd = false;
-      hijackDirectories = {
-        enable = true;
-        autoOpen = true;
-      };
-      updateFocusedFile = {
-        enable = false;
-        updateRoot = false;
-        ignoreList = [ ];
-      };
-      systemOpen = {
-        cmd = "";
-        args = [ ];
-      };
-      diagnostics = {
-        enable = false;
-        debounceDelay = 50;
-        showOnDirs = false;
-        showOnOpenDirs = true;
-        icons = {
-          hint = "";
-          info = "";
-          warning = "";
-          error = "";
-        };
-        severity = {
-          min = "hint";
-          max = "error";
-        };
-      };
-      git = {
-        enable = true;
-        ignore = true;
-        showOnDirs = true;
-        showOnOpenDirs = true;
-        timeout = 400;
-      };
-      modified = {
-        enable = false;
-        showOnDirs = true;
-        showOnOpenDirs = true;
-      };
-      filesystemWatchers = {
-        enable = true;
-        debounceDelay = 50;
-        ignoreDirs = [ ];
-      };
-      onAttach = "default";
-      selectPrompts = false;
-      view = {
-        centralizeSelection = false;
-        cursorline = true;
-        debounceDelay = 15;
-        width = {
-          min = 30;
-          max = -1;
-          padding = 1;
-        };
-        side = "left";
-        preserveWindowProportions = false;
-        number = false;
-        relativenumber = false;
-        signcolumn = "yes";
-        float = {
-          enable = false;
-          quitOnFocusLoss = true;
-          openWinConfig = {
-            col = 1;
-            row = 1;
-            relative = "cursor";
-            border = "shadow";
-            style = "minimal";
-          };
-        };
-      };
-      renderer = {
-        addTrailing = false;
-        groupEmpty = false;
-        fullName = false;
-        highlightGit = false;
-        highlightOpenedFiles = "none";
-        highlightModified = "none";
-        rootFolderLabel = ":~:s?$?/..?";
-        indentWidth = 2;
-        indentMarkers = {
-          enable = false;
-          inlineArrows = true;
-          icons = {
-            corner = "└";
-            edge = "│";
-            item = "│";
-            bottom = "─";
-            none = " ";
-          };
-        };
-        icons = {
-          webdevColors = true;
-          gitPlacement = "before";
-          modifiedPlacement = "after";
-          padding = " ";
-          symlinkArrow = " ➛ ";
-          show = {
-            file = true;
-            folder = true;
-            folderArrow = true;
-            git = true;
-            modified = true;
-          };
-          glyphs = {
-            default = "";
-            symlink = "";
-            modified = "●";
-            folder = {
-              arrowClosed = "";
-              arrowOpen = "";
-              default = "";
-              open = "";
-              empty = "";
-              emptyOpen = "";
-              symlink = "";
-              symlinkOpen = "";
-            };
-            git = {
-              unstaged = "✗";
-              staged = "✓";
-              unmerged = "";
-              renamed = "➜";
-              untracked = "★";
-              deleted = "";
-              ignored = "◌";
-            };
-          };
-        };
-        specialFiles = [
-          "Cargo.toml"
-          "Makefile"
-          "README.md"
-          "readme.md"
-        ];
-        symlinkDestination = true;
-      };
-      filters = {
-        dotfiles = false;
-        gitClean = false;
-        noBuffer = false;
-        custom = [ ];
-        exclude = [ ];
-      };
-      actions = {
-        changeDir = {
+      settings = {
+        disable_netrw = true;
+        hijack_netrw = false;
+        auto_reload_on_write = true;
+        sort_by = "name";
+        hijack_unnamed_buffer_when_opening = false;
+        hijack_cursor = false;
+        root_dirs = [ ];
+        prefer_startup_root = false;
+        sync_root_with_cwd = false;
+        reload_on_bufenter = false;
+        respect_buf_cwd = false;
+        hijack_directories = {
           enable = true;
-          global = false;
-          restrictAboveCwd = false;
+          auto_open = true;
         };
-        expandAll = {
-          maxFolderDiscovery = 300;
+        update_focused_file = {
+          enable = false;
+          update_root = false;
+          ignore_list = [ ];
+        };
+        system_open = {
+          cmd = "";
+          args = [ ];
+        };
+        diagnostics = {
+          enable = false;
+          debounce_delay = 50;
+          show_on_dirs = false;
+          show_on_open_dirs = true;
+          icons = {
+            hint = "";
+            info = "";
+            warning = "";
+            error = "";
+          };
+          severity = {
+            min = "hint";
+            max = "error";
+          };
+        };
+        git = {
+          enable = true;
+          ignore = true;
+          show_on_dirs = true;
+          show_on_open_dirs = true;
+          timeout = 400;
+        };
+        modified = {
+          enable = false;
+          show_on_dirs = true;
+          show_on_open_dirs = true;
+        };
+        filesystem_watchers = {
+          enable = true;
+          debounce_delay = 50;
+          ignore_dirs = [ ];
+        };
+        on_attach = "default";
+        select_prompts = false;
+        view = {
+          centralize_selection = false;
+          cursorline = true;
+          debounce_delay = 15;
+          width = {
+            min = 30;
+            max = -1;
+            padding = 1;
+          };
+          side = "left";
+          preserve_window_proportions = false;
+          number = false;
+          relativenumber = false;
+          signcolumn = "yes";
+          float = {
+            enable = false;
+            quit_on_focus_loss = true;
+            open_win_config = {
+              col = 1;
+              row = 1;
+              relative = "cursor";
+              border = "shadow";
+              style = "minimal";
+            };
+          };
+        };
+        renderer = {
+          add_trailing = false;
+          group_empty = false;
+          full_name = false;
+          highlight_git = false;
+          highlight_opened_files = "none";
+          highlight_modified = "none";
+          root_folder_label = ":~:s?$?/..?";
+          indent_width = 2;
+          indent_markers = {
+            enable = false;
+            inline_arrows = true;
+            icons = {
+              corner = "└";
+              edge = "│";
+              item = "│";
+              bottom = "─";
+              none = " ";
+            };
+          };
+          icons = {
+            webdev_colors = true;
+            git_placement = "before";
+            modified_placement = "after";
+            padding = " ";
+            symlink_arrow = " ➛ ";
+            show = {
+              file = true;
+              folder = true;
+              folder_arrow = true;
+              git = true;
+              modified = true;
+            };
+            glyphs = {
+              default = "";
+              symlink = "";
+              modified = "●";
+              folder = {
+                arrow_closed = "";
+                arrow_open = "";
+                default = "";
+                open = "";
+                empty = "";
+                empty_open = "";
+                symlink = "";
+                symlink_open = "";
+              };
+              git = {
+                unstaged = "✗";
+                staged = "✓";
+                unmerged = "";
+                renamed = "➜";
+                untracked = "★";
+                deleted = "";
+                ignored = "◌";
+              };
+            };
+          };
+          special_files = [
+            "Cargo.toml"
+            "Makefile"
+            "README.md"
+            "readme.md"
+          ];
+          symlink_destination = true;
+        };
+        filters = {
+          dotfiles = false;
+          git_clean = false;
+          no_buffer = false;
+          custom = [ ];
           exclude = [ ];
         };
-        filePopup = {
-          openWinConfig = {
-            col = 1;
-            row = 1;
-            relative = "cursor";
-            border = "shadow";
-            style = "minimal";
+        actions = {
+          change_dir = {
+            enable = true;
+            global = false;
+            restrict_above_cwd = false;
+          };
+          expand_all = {
+            max_folder_discovery = 300;
+            exclude = [ ];
+          };
+          file_popup = {
+            open_win_config = {
+              col = 1;
+              row = 1;
+              relative = "cursor";
+              border = "shadow";
+              style = "minimal";
+            };
+          };
+          open_file = {
+            quit_on_open = false;
+            resize_window = true;
+          };
+          window_picker = {
+            enable = true;
+            picker = "default";
+            chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            exclude = {
+              filetype = [
+                "notify"
+                "packer"
+                "qf"
+                "diff"
+                "fugitive"
+                "fugitiveblame"
+              ];
+              buftype = [
+                "nofile"
+                "terminal"
+                "help"
+              ];
+            };
+          };
+          remove_file = {
+            close_window = true;
+          };
+          use_system_clipboard = true;
+        };
+        live_filter = {
+          prefix = "[FILTER]: ";
+          always_show_folders = true;
+        };
+        tab = {
+          sync = {
+            open = false;
+            close = false;
+            ignore = [ ];
           };
         };
-        openFile = {
-          quitOnOpen = false;
-          resizeWindow = true;
+        notify = {
+          threshold = "info";
         };
-        windowPicker = {
-          enable = true;
-          picker = "default";
-          chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-          exclude = {
-            filetype = [
-              "notify"
-              "packer"
-              "qf"
-              "diff"
-              "fugitive"
-              "fugitiveblame"
-            ];
-            buftype = [
-              "nofile"
-              "terminal"
-              "help"
-            ];
+        ui = {
+          confirm = {
+            remove = true;
+            trash = true;
           };
         };
-        removeFile = {
-          closeWindow = true;
-        };
-        useSystemClipboard = true;
-      };
-      liveFilter = {
-        prefix = "[FILTER]: ";
-        alwaysShowFolders = true;
-      };
-      tab = {
-        sync = {
-          open = false;
-          close = false;
-          ignore = [ ];
-        };
-      };
-      notify = {
-        threshold = "info";
-      };
-      ui = {
-        confirm = {
-          remove = true;
-          trash = true;
-        };
-      };
-      log = {
-        enable = false;
-        truncate = false;
-        types = {
-          all = false;
-          profile = false;
-          config = false;
-          copyPaste = false;
-          dev = false;
-          diagnostics = false;
-          git = false;
-          watcher = false;
+        log = {
+          enable = false;
+          truncate = false;
+          types = {
+            all = false;
+            profile = false;
+            config = false;
+            copy_paste = false;
+            dev = false;
+            diagnostics = false;
+            git = false;
+            watcher = false;
+          };
         };
       };
     };
