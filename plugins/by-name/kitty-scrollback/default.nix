8ada66c86dd2a8cc96704037708a12e7241a1d2a
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-08-10 13:04:11 +0200

    plugins/kitty-scrollback: add settingsExample
    Co-authored-by: Matt Sturgeon <matt@sturgeon.me.uk>

--- plugins/by-name/kitty-scrollback/default.nix
+++ plugins/by-name/kitty-scrollback/default.nix
@@ -4,4 +4,13 @@ lib.nixvim.plugins.mkNeovimPlugin {
   package = "kitty-scrollback-nvim";
 
   maintainers = [ lib.maintainers.nim65s ];
+
+  settingsExample = lib.literalExpression ''
+    {
+      # create your config
+      myconfig.kitty_get_text.ansi = false;
+      # or extend a builtin one
+      ksb_builtin_last_cmd_output.ansi = false;
+    }
+  '';
 }
