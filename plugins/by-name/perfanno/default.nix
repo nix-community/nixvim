29544e5f510b454c97492741901f37c48595f391
Frede Braendstrup <frederikbraendstrup@gmail.com>
2025-08-26 17:27:55 +0200

    plugins/perfanno: add perfanno-nvim plugin

---
+++ plugins/by-name/perfanno/default.nix
@@ -0,0 +1,43 @@
+{ lib, ... }:
+
+lib.nixvim.plugins.mkNeovimPlugin {
+  name = "perfanno";
+  packPathName = "perfanno.nvim";
+  package = "perfanno-nvim";
+  description = "NeoVim lua plugin that annotates source code with profiling information from perf, LuaJIT, or other profilers.";
+  # TODO: Remove after https://github.com/NixOS/nixpkgs/pull/438707
+  url = "https://github.com/t-troebst/perfanno.nvim";
+
+  maintainers = [ lib.maintainers.fredeb ];
+
+  settingsExample = {
+    line_highlights.__raw = ''util.make_bg_highlights(nil, "#CC3300", 10)'';
+    vt_highlight.__raw = ''util.make_fg_highlight("#CC3300")'';
+    formats = [
+      {
+        percent = true;
+        format = "%.2f%%";
+        minimum = 0.5;
+      }
+      {
+        percent = false;
+        format = "%d";
+        minimum = 1;
+      }
+    ];
+
+    annotate_after_load = true;
+    annotate_on_open = true;
+    telescope = {
+      enabled.__raw = ''pcall(require, "telescope")'';
+      annotate = true;
+    };
+
+    ts_function_patterns = {
+      default = [
+        "function"
+        "method"
+      ];
+    };
+  };
+}
