d1c122ac44df24ef93b6df34b12b7cf0444e6423
Matt Sturgeon <matt@sturgeon.me.uk>
2025-09-09 23:04:19 +0100

    plugins: fix duplicate URL definitions

--- plugins/by-name/earthly/default.nix
+++ plugins/by-name/earthly/default.nix
@@ -3,7 +3,6 @@ lib.nixvim.plugins.mkVimPlugin {
   name = "earthly";
   packPathName = "earthly.vim";
   package = "earthly-vim";
-  url = "https://github.com/earthly/earthly.vim";
   description = "Earthfile syntax highlighting for vim";
   maintainers = [ lib.maintainers.DataHearth ];
 }
