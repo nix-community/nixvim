2bcf7d00c7a603d47de7c75e17e65076f3407e19
saygo-png <saygo.mail@proton.me>
2025-09-11 03:15:01 +0200

    plugins/colorschemes: add warning to gruvbox-material
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- plugins/colorschemes/gruvbox-material.nix
+++ plugins/colorschemes/gruvbox-material.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, config, ... }:
 lib.nixvim.plugins.mkVimPlugin {
   name = "gruvbox-material";
   isColorscheme = true;
@@ -18,5 +18,16 @@ lib.nixvim.plugins.mkVimPlugin {
       ];
     };
     show_eob = 0;
+  };
+
+  extraConfig = cfg: {
+    warnings = lib.nixvim.mkWarnings "colorschemes.gruvbox-material" {
+      when = (cfg.settings.better_performance or 0 == 1) && !config.impureRtp;
+      message = ''
+        You have enabled 'better_performance', but the top-level option 'impureRtp' is false.
+        The performance option generates syntax files at runtime that cannot be accessed with impureRtp disabled.
+        This breaks the plugin. Either disable 'better_performance' or enable 'impureRtp'.
+      '';
+    };
   };
 }
