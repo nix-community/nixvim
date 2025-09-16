c0bc3b500905a15471b5f2664817c6e35ae4762d
Austin Horstman <khaneliman12@gmail.com>
2025-09-16 18:06:20 -0500

    lensline: add module
    Modular nvim codelens support with inline references, git blame and more

---
+++ tests/test-sources/plugins/by-name/lensline/default.nix
@@ -0,0 +1,58 @@
+{
+  empty = {
+    plugins.lensline.enable = true;
+  };
+
+  defaults = {
+    plugins.lensline = {
+      enable = true;
+      settings = {
+        profiles = [
+          {
+            name = "default";
+            providers = [
+              {
+                name = "references";
+                enabled = true;
+                quiet_lsp = true;
+              }
+              {
+                name = "last_author";
+                enabled = true;
+                cache_max_files = 50;
+              }
+              {
+                name = "diagnostics";
+                enabled = false;
+                min_level = "WARN";
+              }
+              {
+                name = "complexity";
+                enabled = false;
+                min_level = "L";
+              }
+            ];
+            style = {
+              separator = " • ";
+              highlight = "Comment";
+              prefix = "┃ ";
+              placement = "above";
+              use_nerdfont = true;
+              render = "all";
+            };
+          }
+        ];
+        limits = {
+          exclude = {
+          };
+          exclude_gitignored = true;
+          max_lines = 1000;
+          max_lenses = 70;
+        };
+        debounce_ms = 500;
+        focused_debounce_ms = 150;
+        debug_mode = false;
+      };
+    };
+  };
+}
