76f97572c57a303862aba9d689fceb0952865a56
Austin Horstman <khaneliman12@gmail.com>
2025-09-16 21:06:46 -0500

    patterns: add module
    A simple pattern viewer for Neovim with support for Lua patterns &
    regex.

---
+++ plugins/by-name/patterns/default.nix
@@ -0,0 +1,47 @@
+{ lib, ... }:
+lib.nixvim.plugins.mkNeovimPlugin {
+  name = "patterns";
+  packPathName = "patterns.nvim";
+  package = "patterns-nvim";
+
+  maintainers = [ lib.maintainers.khaneliman ];
+
+  settingsExample = {
+    preferred_regex_matcher = "lua";
+    update_delay = 300;
+
+    keymaps = {
+      explain_input = {
+        "<Esc>" = {
+          callback = "close";
+        };
+        "K" = {
+          callback = "lang_prev";
+        };
+        "J" = {
+          callback = "lang_next";
+        };
+      };
+      hover = {
+        "e" = {
+          callback = "edit";
+        };
+      };
+    };
+
+    windows = {
+      hover = lib.nixvim.nestedLiteralLua ''
+        function(pos, side)
+          return {
+            width = math.floor(vim.o.columns * 0.8),
+            height = math.floor(vim.o.lines * 0.6),
+            border = "single",
+            footer = {
+              { "󰛪 Patterns ", "FloatTitle" },
+            }
+          }
+        end
+      '';
+    };
+  };
+}
