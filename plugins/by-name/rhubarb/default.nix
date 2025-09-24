47b2ce062aadabbb6a0bded6e02386a1ba08bb19
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-24 17:50:30 +0545

    rhubarb: init

---
+++ plugins/by-name/rhubarb/default.nix
@@ -0,0 +1,20 @@
+{ lib, config, ... }:
+lib.nixvim.plugins.mkVimPlugin {
+  name = "rhubarb";
+  packPathName = "vim-rhubarb";
+  package = "vim-rhubarb";
+  description = "Rhubarb is a GitHub extension for fugitive.vim.";
+
+  maintainers = [ lib.maintainers.santosh ];
+
+  dependencies = [ "git" ];
+
+  extraConfig = {
+    assertions = lib.nixvim.mkAssertions "plugins.rhubarb" [
+      {
+        assertion = config.plugins.fugitive.enable;
+        message = "You must enable `plugins.fugitive` when using `rhubarb`.";
+      }
+    ];
+  };
+}
