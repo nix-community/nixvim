167ea865e519667258dc94b69ef7c1593dda4e77
Matt Sturgeon <matt@sturgeon.me.uk>
2025-09-28 22:36:42 +0100

    modules/lsp: port `packageFallback` option from plugins.lsp
    Added to `plugins.lsp` in 6a054de04dd2b0a3cc5fed1adab8e9f76c050b45

--- tests/test-sources/modules/lsp.nix
+++ tests/test-sources/modules/lsp.nix
@@ -28,4 +28,54 @@
       };
     };
   };
+
+  package-fallback =
+    { lib, config, ... }:
+    {
+      test.buildNixvim = false;
+
+      lsp = {
+        servers = {
+          nil_ls.enable = true;
+          rust_analyzer = {
+            enable = true;
+            packageFallback = true;
+          };
+          hls = {
+            enable = true;
+            packageFallback = true;
+          };
+        };
+      };
+
+      assertions =
+        let
+          assertPrefix = name: pkg: [
+            {
+              assertion = lib.all (x: x == pkg) config.extraPackages;
+              message = "Expected `${name}` to be in extraPackages";
+            }
+            {
+              assertion = lib.any (x: x != pkg) config.extraPackagesAfter;
+              message = "Expected `${name}` not to be in extraPackagesAfter";
+            }
+          ];
+          assertSuffix = name: pkg: [
+            {
+              assertion = lib.all (x: x != pkg) config.extraPackages;
+              message = "Expected `${name}` not to be in extraPackages";
+            }
+            {
+              assertion = lib.any (x: x == pkg) config.extraPackagesAfter;
+              message = "Expected `${name}` to be in extraPackagesAfter";
+            }
+          ];
+        in
+        with config.lsp.servers;
+        (
+          assertPrefix "nil" nil_ls.package
+          ++ assertSuffix "rust-analyzer" rust_analyzer.package
+          ++ assertSuffix "haskell-language-server" hls.package
+        );
+    };
 }
