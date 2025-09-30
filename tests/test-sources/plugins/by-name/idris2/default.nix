523444bf996b5ad3463032fbcd1b2ec76c7d8498
Gaetan Lepage <gaetan@glepage.com>
2025-10-01 00:10:19 +0200

    tests: disable test relying on the idris2 plugin (using deprecated lspconfig API)

--- tests/test-sources/plugins/by-name/idris2/default.nix
+++ tests/test-sources/plugins/by-name/idris2/default.nix
@@ -1,5 +1,9 @@
 {
   empty = {
+    # TODO 2025-10-01
+    # Calls `require("lspconfig")` which is deprecated, producing a warning
+    test.runNvim = false;
+
     plugins.idris2.enable = true;
   };
 }
