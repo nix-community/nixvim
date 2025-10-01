1d4a02d7e92d4df10081943e847f4de5ee13b59a
Gaetan Lepage <gaetan@glepage.com>
2025-10-01 14:43:59 +0200

    tests/plugins/neogit: update defaults

--- tests/test-sources/plugins/by-name/neogit/default.nix
+++ tests/test-sources/plugins/by-name/neogit/default.nix
@@ -13,67 +13,110 @@
       enable = true;
 
       settings = {
-        filewatcher = {
-          enabled = true;
-        };
-        graph_style = "ascii";
+        use_default_keymaps = true;
         disable_hint = false;
         disable_context_highlighting = false;
         disable_signs = false;
+        prompt_force_push = true;
+        graph_style = "ascii";
+        commit_date_format = null;
+        log_date_format = null;
+        process_spinner = false;
+        filewatcher.enabled = true;
+        telescope_sorter.__raw = ''
+          function()
+            return nil
+          end
+        '';
         git_services = {
-          "github.com" = "https://github.com/$\{owner}/$\{repository}/compare/$\{branch_name}?expand=1";
-          "bitbucket.org" =
-            "https://bitbucket.org/$\{owner}/$\{repository}/pull-requests/new?source=$\{branch_name}&t=1";
-          "gitlab.com" =
-            "https://gitlab.com/$\{owner}/$\{repository}/merge_requests/new?merge_request[source_branch]=$\{branch_name}";
+          "github.com" = {
+            pull_request = "https://github.com/$\{owner}/$\{repository}/compare/$\{branch_name}?expand=1";
+            commit = "https://github.com/$\{owner}/$\{repository}/commit/$\{oid}";
+            tree = "https://$\{host}/$\{owner}/$\{repository}/tree/$\{branch_name}";
+          };
+          "bitbucket.org" = {
+            pull_request = "https://bitbucket.org/\${owner}/$\{repository}/pull-requests/new?source=$\{branch_name}&t=1";
+            commit = "https://bitbucket.org/$\{owner}/$\{repository}/commits/$\{oid}";
+            tree = "https://bitbucket.org/$\{owner}/$\{repository}/branch/$\{branch_name}";
+          };
+          "gitlab.com" = {
+            pull_request = "https://gitlab.com/$\{owner}/$\{repository}/merge_requests/new?merge_request[source_branch]=$\{branch_name}";
+            commit = "https://gitlab.com/$\{owner}/$\{repository}/-/commit/$\{oid}";
+            tree = "https://gitlab.com/$\{owner}/$\{repository}/-/tree/$\{branch_name}?ref_type=heads";
+          };
+          "azure.com" = {
+            pull_request = "https://dev.azure.com/$\{owner}/_git/$\{repository}/pullrequestcreate?sourceRef=$\{branch_name}&targetRef=$\{target}";
+            commit = "";
+            tree = "";
+          };
         };
-        telescope_sorter = null;
+        highlight = { };
         disable_insert_on_commit = "auto";
         use_per_project_settings = true;
         remember_settings = true;
-        auto_refresh = true;
+        fetch_after_checkout = false;
         sort_branches = "-committerdate";
+        commit_order = "topo";
         kind = "tab";
+        floating = {
+          relative = "editor";
+          width = 0.8;
+          height = 0.7;
+          style = "minimal";
+          border = "rounded";
+        };
+        initial_branch_name = "";
         disable_line_numbers = true;
+        disable_relative_line_numbers = true;
         console_timeout = 2000;
         auto_show_console = true;
+        auto_show_console_on = "output";
+        auto_close_console = true;
+        notification_icon = "󰊢";
         status = {
+          show_head_commit_hash = true;
           recent_commit_count = 10;
+          HEAD_padding = 10;
+          HEAD_folded = false;
+          mode_padding = 3;
+          mode_text = {
+            M = "modified";
+            N = "new file";
+            A = "added";
+            D = "deleted";
+            C = "copied";
+            U = "updated";
+            R = "renamed";
+            T = "changed";
+            DD = "unmerged";
+            AU = "unmerged";
+            UD = "unmerged";
+            UA = "unmerged";
+            DU = "unmerged";
+            AA = "unmerged";
+            UU = "unmerged";
+            "?" = "";
+          };
         };
         commit_editor = {
-          kind = "auto";
-        };
-        commit_select_view = {
           kind = "tab";
+          show_staged_diff = true;
+          staged_diff_split_kind = "split";
+          spell_check = true;
         };
+        commit_select_view.kind = "tab";
         commit_view = {
           kind = "vsplit";
-          verify_commit = "os.execute('which gpg') == 0";
+          verify_commit.__raw = "vim.fn.executable('gpg') == 1";
         };
-        log_view = {
-          kind = "tab";
-        };
-        rebase_editor = {
-          kind = "auto";
-        };
-        reflog_view = {
-          kind = "tab";
-        };
-        merge_editor = {
-          kind = "auto";
-        };
-        description_editor = {
-          kind = "auto";
-        };
-        tag_editor = {
-          kind = "auto";
-        };
-        preview_buffer = {
-          kind = "split";
-        };
-        popup = {
-          kind = "split";
-        };
+        log_view.kind = "tab";
+        rebase_editor.kind = "auto";
+        reflog_view.kind = "tab";
+        merge_editor.kind = "auto";
+        preview_buffer.kind = "floating_console";
+        popup.kind = "split";
+        stash.kind = "tab";
+        refs_view.kind = "tab";
         signs = {
           hunk = [
             ""
@@ -91,10 +134,16 @@
         integrations = {
           telescope = null;
           diffview = null;
-          fzf-lua = null;
+          fzf_lua = null;
+          mini_pick = null;
+          snacks = null;
         };
         sections = {
           sequencer = {
+            folded = false;
+            hidden = false;
+          };
+          bisect = {
             folded = false;
             hidden = false;
           };
@@ -139,14 +188,7 @@
             hidden = false;
           };
         };
-        ignored_settings = [
-          "NeogitPushPopup--force-with-lease"
-          "NeogitPushPopup--force"
-          "NeogitPullPopup--rebase"
-          "NeogitCommitPopup--allow-empty"
-          "NeogitRevertPopup--no-edit"
-        ];
-
+        ignored_settings = [ ];
         mappings = {
           commit_editor = {
             q = "Close";
@@ -190,8 +232,10 @@
             "<c-p>" = "Previous";
             "<down>" = "Next";
             "<up>" = "Previous";
-            "<tab>" = "MultiselectToggleNext";
-            "<s-tab>" = "MultiselectTogglePrevious";
+            "<tab>" = "InsertCompletion";
+            "<c-y>" = "CopySelection";
+            "<space>" = "MultiselectToggleNext";
+            "<s-space>" = "MultiselectTogglePrevious";
             "<c-j>" = "NOP";
             "<ScrollWheelDown>" = "ScrollWheelDown";
             "<ScrollWheelUp>" = "ScrollWheelUp";
@@ -200,6 +244,7 @@
             "<LeftMouse>" = "MouseClick";
             "<2-LeftMouse>" = "NOP";
           };
+          refs_view.x = "DeleteBranch";
           popup = {
             "?" = "HelpPopup";
             A = "CherryPickPopup";
@@ -216,31 +261,43 @@
             c = "CommitPopup";
             f = "FetchPopup";
             l = "LogPopup";
+            L = "MarginPopup";
             m = "MergePopup";
             p = "PullPopup";
             r = "RebasePopup";
             v = "RevertPopup";
           };
           status = {
+            j = "MoveDown";
+            k = "MoveUp";
+            o = "OpenTree";
             q = "Close";
             I = "InitRepo";
             "1" = "Depth1";
             "2" = "Depth2";
             "3" = "Depth3";
             "4" = "Depth4";
+            Q = "Command";
             "<tab>" = "Toggle";
+            za = "Toggle";
+            zo = "OpenFold";
+            zc = "CloseFold";
+            zC = "Depth1";
+            zO = "Depth4";
             x = "Discard";
             s = "Stage";
             S = "StageUnstaged";
             "<c-s>" = "StageAll";
             u = "Unstage";
             K = "Untrack";
+            R = "Rename";
             U = "UnstageStaged";
             y = "ShowRefs";
             "$" = "CommandHistory";
             Y = "YankSelected";
             "<c-r>" = "RefreshBuffer";
             "<cr>" = "GoToFile";
+            "<s-cr>" = "PeekFile";
             "<c-v>" = "VSplitOpen";
             "<c-x>" = "SplitOpen";
             "<c-t>" = "TabOpen";
@@ -248,14 +305,11 @@
             "}" = "GoToNextHunkHeader";
             "[c" = "OpenOrScrollUp";
             "]c" = "OpenOrScrollDown";
+            "<c-k>" = "PeekUp";
+            "<c-j>" = "PeekDown";
+            "<c-n>" = "NextSection";
+            "<c-p>" = "PreviousSection";
           };
-        };
-        notification_icon = "󰊢";
-        use_default_keymaps = true;
-        highlight = {
-          italic = true;
-          bold = true;
-          underline = true;
         };
       };
     };
