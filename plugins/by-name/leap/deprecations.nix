35b7251d83dfea09648c7c35764e38edfdd31046
Austin Horstman <khaneliman12@gmail.com>
2025-10-04 11:54:59 -0500

    plugins/leap: migrate to mkNeovimPlugin

---
+++ plugins/by-name/leap/deprecations.nix
@@ -0,0 +1,38 @@
+{
+  # TODO: added 2025-10-04
+  deprecateExtraOptions = true;
+  optionsRenamedToSettings = [
+    "maxPhaseOneTargets"
+    "highlightUnlabeledPhaseOneTargets"
+    "maxHighlightedTraversalTargets"
+    "caseSensitive"
+    "equivalenceClasses"
+    "substituteChars"
+    "safeLabels"
+    "labels"
+    [
+      "specialKeys"
+      "nextTarget"
+    ]
+    [
+      "specialKeys"
+      "prevTarget"
+    ]
+    [
+      "specialKeys"
+      "nextGroup"
+    ]
+    [
+      "specialKeys"
+      "prevGroup"
+    ]
+    [
+      "specialKeys"
+      "multiAccept"
+    ]
+    [
+      "specialKeys"
+      "multiRevert"
+    ]
+  ];
+}
