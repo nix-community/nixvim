9bdedc05109b44f97cdf757b3910fda8f86bc28d
Austin Horstman <khaneliman12@gmail.com>
2025-10-06 14:46:34 -0500

    plugins/ollama: migrate to mkNeovimPlugin

---
+++ plugins/by-name/ollama/deprecations.nix
@@ -0,0 +1,30 @@
+{
+  # TODO: introduced 2025-10-06
+  deprecateExtraOptions = true;
+  optionsRenamedToSettings = [
+    "model"
+    "prompts"
+    "action"
+    "url"
+    [
+      "serve"
+      "onStart"
+    ]
+    [
+      "serve"
+      "command"
+    ]
+    [
+      "serve"
+      "args"
+    ]
+    [
+      "serve"
+      "stopCommand"
+    ]
+    [
+      "serve"
+      "stopArgs"
+    ]
+  ];
+}
