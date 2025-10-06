9d44460d028a062ec6e6b18c09bde9e93cd97225
Matt Sturgeon <matt@sturgeon.me.uk>
2025-10-06 10:48:18 +0100

    modules/lsp: auto-import per-server custom modules
    Adding a module in the following places will import the module into that
    specific server's submodule.
    
    This allows creating server-specific options.
    
    Added a test case to ensure all custom modules correspond to an actual
    server option to avoid accidental dead code.

--- tests/test-sources/modules/lsp.nix
+++ tests/test-sources/modules/lsp.nix
@@ -221,4 +221,37 @@
           ++ assertSuffix "haskell-language-server" hls.package
         );
     };
+
+  # Assert that we don't have any redundant custom modules
+  custom-server-modules =
+    { lib, options, ... }:
+    let
+      rootPrefix = toString ../../../. + "/";
+      customDir = ../../../modules/lsp/servers/custom;
+      serverOptions = (opt: opt.type.getSubOptions opt.loc) options.lsp.servers;
+      modules = lib.pipe customDir [
+        builtins.readDir
+        (lib.filterAttrs (name: _: !(serverOptions ? ${lib.strings.removeSuffix ".nix" name})))
+        (lib.mapAttrsToList (
+          name: type: customDir + "/${name}" + lib.optionalString (type == "directory") "/default.nix"
+        ))
+        (builtins.filter (lib.strings.hasSuffix ".nix"))
+        (builtins.filter lib.pathExists)
+        (map (module: lib.strings.removePrefix rootPrefix (toString module)))
+      ];
+    in
+    {
+      test.buildNixvim = false;
+
+      assertions = [
+        {
+          assertion = modules == [ ];
+          message = ''
+            The following custom modules do not correspond to an LSP server option:${
+              lib.concatMapStrings (module: "\n- ${module}") modules
+            }
+          '';
+        }
+      ];
+    };
 }
