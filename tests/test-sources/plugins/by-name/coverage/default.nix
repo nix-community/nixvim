5fbec45014235b4ff075820c205cda0fb3861e74
FKouhai <frandres00@gmail.com>
2025-10-07 08:36:42 +0200

    plugins/coverage: migrate to mkNeovim

--- tests/test-sources/plugins/by-name/coverage/default.nix
+++ tests/test-sources/plugins/by-name/coverage/default.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   empty = {
     plugins.coverage.enable = true;
@@ -7,97 +8,87 @@
     plugins.coverage = {
       enable = true;
 
-      keymapsSilent = true;
-      keymaps = {
-        coverage = "<leader>a";
-        load = "<leader>b";
-        show = "<leader>c";
-        hide = "<leader>d";
-        toggle = "<leader>e";
-        clear = "<leader>f";
-        summary = "<leader>g";
-        loadLcov = "<leader>h";
+      settings = {
+        commands = true;
+        autoReload = false;
+        autoReloadTimeoutMs = 500;
+        highlights = {
+          covered = {
+            fg = "#B7F071";
+          };
+          uncovered = {
+            fg = "#F07178";
+          };
+          partial = {
+            fg = "#AA71F0";
+          };
+          summaryBorder = {
+            link = "FloatBorder";
+          };
+          summaryNormal = {
+            link = "NormalFloat";
+          };
+          summaryCursorLine = {
+            link = "CursorLine";
+          };
+          summaryHeader = {
+            style = "bold,underline";
+            sp = "bg";
+          };
+          summaryPass = {
+            link = "CoverageCovered";
+          };
+          summaryFail = {
+            link = "CoverageUncovered";
+          };
+        };
+        loadCoverageCb = lib.nixvim.mkRaw ''
+          function (ftype)
+            vim.notify("Loaded " .. ftype .. " coverage")
+          end
+        '';
+        signs = {
+          covered = {
+            hl = "CoverageCovered";
+            text = "▎";
+          };
+          uncovered = {
+            hl = "CoverageUncovered";
+            text = "▎";
+          };
+          partial = {
+            hl = "CoveragePartial";
+            text = "▎";
+          };
+        };
+        signGroup = "coverage";
+        summary = {
+          widthPercentage = 0.7;
+          heightPercentage = 0.5;
+          borders = {
+            topleft = "╭";
+            topright = "╮";
+            top = "─";
+            left = "│";
+            right = "│";
+            botleft = "╰";
+            botright = "╯";
+            bot = "─";
+            highlight = "Normal:CoverageSummaryBorder";
+          };
+          minCoverage = 80;
+        };
+        lang = {
+          python = {
+            coverage_file = ".coverage";
+            coverage_command = "coverage json --fail-under=0 -q -o -";
+          };
+          ruby = {
+            coverage_file = "coverage/coverage.json";
+          };
+        };
+        lcovFile = null;
       };
-
-      autoReload = false;
-      autoReloadTimeoutMs = 500;
-      commands = true;
-      highlights = {
-        covered = {
-          fg = "#B7F071";
-        };
-        uncovered = {
-          fg = "#F07178";
-        };
-        partial = {
-          fg = "#AA71F0";
-        };
-        summaryBorder = {
-          link = "FloatBorder";
-        };
-        summaryNormal = {
-          link = "NormalFloat";
-        };
-        summaryCursorLine = {
-          link = "CursorLine";
-        };
-        summaryHeader = {
-          style = "bold,underline";
-          sp = "bg";
-        };
-        summaryPass = {
-          link = "CoverageCovered";
-        };
-        summaryFail = {
-          link = "CoverageUncovered";
-        };
-      };
-      loadCoverageCb = ''
-        function (ftype)
-          vim.notify("Loaded " .. ftype .. " coverage")
-        end
-      '';
-      signs = {
-        covered = {
-          hl = "CoverageCovered";
-          text = "▎";
-        };
-        uncovered = {
-          hl = "CoverageUncovered";
-          text = "▎";
-        };
-        partial = {
-          hl = "CoveragePartial";
-          text = "▎";
-        };
-      };
-      signGroup = "coverage";
-      summary = {
-        widthPercentage = 0.7;
-        heightPercentage = 0.5;
-        borders = {
-          topleft = "╭";
-          topright = "╮";
-          top = "─";
-          left = "│";
-          right = "│";
-          botleft = "╰";
-          botright = "╯";
-          bot = "─";
-          highlight = "Normal:CoverageSummaryBorder";
-        };
-        minCoverage = 80;
-      };
-      lang = {
-        python = {
-          coverage_file = ".coverage";
-          coverage_command = "coverage json --fail-under=0 -q -o -";
-        };
-        ruby = {
-          coverage_file = "coverage/coverage.json";
-        };
-      };
-      lcovFile = null;
     };
   };
 }
