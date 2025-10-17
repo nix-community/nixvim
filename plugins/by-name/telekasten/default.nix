f7a8a6b5212d60f7d3159e8ca2d243d41f369b60
saygo-png <saygo.mail@proton.me>
2025-10-18 01:56:04 +0200

    plugins/telekasten: remove obsolete dependencies
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- plugins/by-name/telekasten/default.nix
+++ plugins/by-name/telekasten/default.nix
@@ -1,7 +1,5 @@
 {
-  config,
   lib,
-  pkgs,
   ...
 }:
 lib.nixvim.plugins.mkNeovimPlugin {
@@ -13,24 +11,5 @@ lib.nixvim.plugins.mkNeovimPlugin {
 
   settingsExample = {
     home.__raw = ''vim.fn.expand("~/zettelkasten")'';
-  };
-
-  # TODO: Remove once nixpkgs #349346 lands, since it will have plenary-nvim as
-  # a dependency.
-  extraOptions = {
-    plenaryPackage = lib.mkPackageOption pkgs.vimPlugins "plenary-nvim" { nullable = true; };
-  };
-
-  # TODO: Remove once nixpkgs #349346 lands, since it will have telescope-nvim
-  # as a dependency.
-  extraConfig = cfg: {
-    assertions = lib.nixvim.mkAssertions "plugins.telekasten" {
-      assertion = config.plugins.telescope.enable;
-      message = ''
-        You have to enable `plugins.telescope` as `enableTelescope` is activated.
-      '';
-    };
-
-    extraPlugins = [ cfg.plenaryPackage ];
   };
 }
