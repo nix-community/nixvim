910e0debcb918c9b78fd600faf9d796c6721743b
Gaetan Lepage <gaetan@glepage.com>
2025-10-24 16:00:56 +0200

    tests/plugins/compiler: disable runNvim (unconditional warning)
    https://github.com/stevearc/overseer.nvim/pull/448

--- tests/test-sources/plugins/by-name/compiler/default.nix
+++ tests/test-sources/plugins/by-name/compiler/default.nix
@@ -1,5 +1,10 @@
 {
   empty = {
+    # TODO: shows a warning
+    # > ERROR: ATTN: overseer.nvim will experience breaking changes soon. Pin to version v1.6.0 or earlier to avoid disruption.
+    # > See: https://github.com/stevearc/overseer.nvim/pull/448
+    test.runNvim = false;
+
     plugins.compiler.enable = true;
   };
 }
