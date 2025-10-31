ac2221d64da8929b043949049c80f06103ae7787
saygo-png <saygo.mail@proton.me>
2025-10-31 19:55:38 +0100

    plugins/dap-go: remove settings declarations
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- plugins/by-name/dap-go/default.nix
+++ plugins/by-name/dap-go/default.nix
@@ -2,42 +2,12 @@
   lib,
   ...
 }:
-let
-  inherit (lib) types;
-  inherit (lib.nixvim) defaultNullOpts;
-
-  dapHelpers = import ../dap/dapHelpers.nix { inherit lib; };
-in
 lib.nixvim.plugins.mkNeovimPlugin {
   name = "dap-go";
   package = "nvim-dap-go";
   description = "An extension for nvim-dap providing configurations for launching go debugger.";
 
   maintainers = [ lib.maintainers.khaneliman ];
-
-  settingsOptions = {
-    dap_configurations = lib.nixvim.mkNullOrOption (types.listOf dapHelpers.configurationType) ''
-      Additional dap configurations.
-      See `:h dap-configuration` for more detail.
-    '';
-
-    delve = {
-      path = defaultNullOpts.mkStr "dlv" "The path to the executable dlv which will be used for debugging.";
-
-      initialize_timeout_sec = defaultNullOpts.mkUnsignedInt 20 "Time to wait for delve to initialize the debug session.";
-
-      port = defaultNullOpts.mkStr "$${port}" ''
-        A string that defines the port to start delve debugger.
-
-        Defaults to string "$${port}" which instructs dap
-        to start the process in a random available port.
-      '';
-
-      args = lib.nixvim.mkNullOrOption (types.listOf types.str) "Additional args to pass to dlv.";
-
-      build_flags = defaultNullOpts.mkStr "" "Build flags to pass to dlv.";
-    };
-  };
 
   # Manually supplied to nvim-dap config module
   callSetup = false;
