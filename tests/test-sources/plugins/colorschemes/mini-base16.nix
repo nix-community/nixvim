192565c5f4e36c3601c2f59f792983c9b58c63a0
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-11-01 23:27:31 -0300

    colorschemes/mini-base16: init

---
+++ tests/test-sources/plugins/colorschemes/mini-base16.nix
@@ -0,0 +1,27 @@
+{
+  defaults = {
+    colorschemes.mini-base16 = {
+      enable = true;
+      settings = {
+        palette = {
+          base00 = "#112641";
+          base01 = "#3a475e";
+          base02 = "#606b81";
+          base03 = "#8691a7";
+          base04 = "#d5dc81";
+          base05 = "#e2e98f";
+          base06 = "#eff69c";
+          base07 = "#fcffaa";
+          base08 = "#ffcfa0";
+          base09 = "#cc7e46";
+          base0A = "#46a436";
+          base0B = "#9ff895";
+          base0C = "#ca6ecf";
+          base0D = "#42f7ff";
+          base0E = "#ffc4ff";
+          base0F = "#00a5c5";
+        };
+      };
+    };
+  };
+}
