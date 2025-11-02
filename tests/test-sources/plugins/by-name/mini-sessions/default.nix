3e669baf95b1456f632de7767de7794585bc7380
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-11-02 02:13:33 -0300

    plugins/mini-sessions: init

---
+++ tests/test-sources/plugins/by-name/mini-sessions/default.nix
@@ -0,0 +1,43 @@
+{ lib, ... }:
+{
+  empty = {
+    plugins.mini-sessions.enable = true;
+  };
+
+  defaults = {
+    plugins.mini-sessions = {
+      enable = true;
+      settings = {
+        autoread = false;
+        autowrite = true;
+        file = "Session.vim";
+
+        force = {
+          read = false;
+          write = true;
+          delete = false;
+        };
+
+        hooks = {
+          pre = {
+            read = lib.nixvim.mkRaw "nil";
+            write = lib.nixvim.mkRaw "nil";
+            delete = lib.nixvim.mkRaw "nil";
+          };
+
+          post = {
+            read = lib.nixvim.mkRaw "nil";
+            write = lib.nixvim.mkRaw "nil";
+            delete = lib.nixvim.mkRaw "nil";
+          };
+        };
+
+        verbose = {
+          read = false;
+          write = true;
+          delete = true;
+        };
+      };
+    };
+  };
+}
