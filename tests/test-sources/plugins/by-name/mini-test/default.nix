aa93ed0257ecd91aee1b936ffd195edc7ada7ea6
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-11-02 02:31:29 -0300

    plugins/mini-test: init

---
+++ tests/test-sources/plugins/by-name/mini-test/default.nix
@@ -0,0 +1,31 @@
+{ lib, ... }:
+{
+  empty = {
+    plugins.mini-test.enable = true;
+  };
+
+  defaults = {
+    plugins.mini-test = {
+      enable = true;
+      settings = {
+        collect = {
+          emulate_busted = true;
+          find_files = lib.nixvim.mkRaw ''
+            function()
+              return vim.fn.globpath('tests', '**/test_*.lua', true, true)
+            end
+          '';
+          filter_cases = lib.nixvim.mkRaw "function(case) return true end";
+        };
+
+        execute = {
+          reporter = lib.nixvim.mkRaw "nil";
+          stop_on_error = false;
+        };
+
+        script_path = "scripts/minitest.lua";
+        silent = false;
+      };
+    };
+  };
+}
