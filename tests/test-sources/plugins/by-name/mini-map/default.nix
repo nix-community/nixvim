7d3057302d53be2f03baa985783b3a31404f7858
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-11-02 01:49:37 -0300

    plugins/mini-map: init

---
+++ tests/test-sources/plugins/by-name/mini-map/default.nix
@@ -0,0 +1,30 @@
+{ lib, ... }:
+{
+  empty = {
+    plugins.mini-map.enable = true;
+  };
+
+  defaults = {
+    plugins.mini-map = {
+      enable = true;
+      settings = {
+        integrations = lib.nixvim.mkRaw "nil";
+
+        symbols = {
+          encode = lib.nixvim.mkRaw "nil";
+          scroll_line = "█";
+          scroll_view = "┃";
+        };
+
+        window = {
+          focusable = false;
+          side = "right";
+          show_integration_count = true;
+          width = 10;
+          winblend = 25;
+          zindex = 10;
+        };
+      };
+    };
+  };
+}
