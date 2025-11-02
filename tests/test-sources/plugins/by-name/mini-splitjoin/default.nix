d0f392f9856af17b1dd3d170e1eaa673e672273d
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-11-02 02:25:28 -0300

    plugins/mini-splitjoin: init

---
+++ tests/test-sources/plugins/by-name/mini-splitjoin/default.nix
@@ -0,0 +1,35 @@
+{ lib, ... }:
+{
+  empty = {
+    plugins.mini-splitjoin.enable = true;
+  };
+
+  defaults = {
+    plugins.mini-splitjoin = {
+      enable = true;
+      settings = {
+        mappings = {
+          toggle = "gS";
+          split = "";
+          join = "";
+        };
+
+        detect = {
+          brackets = lib.nixvim.mkRaw "nil";
+          separator = ",";
+          exclude_regions = lib.nixvim.mkRaw "nil";
+        };
+
+        split = {
+          hooks_pre = lib.nixvim.emptyTable;
+          hooks_post = lib.nixvim.emptyTable;
+        };
+
+        join = {
+          hooks_pre = lib.nixvim.emptyTable;
+          hooks_post = lib.nixvim.emptyTable;
+        };
+      };
+    };
+  };
+}
