4cc7ab6c6ae1f354a7794248bf5fb2a1c2b80a4a
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-11-02 01:30:43 -0300

    plugins/mini-indentscope: init

---
+++ tests/test-sources/plugins/by-name/mini-indentscope/default.nix
@@ -0,0 +1,35 @@
+{ lib, ... }:
+{
+  empty = {
+    plugins.mini-indentscope.enable = true;
+  };
+
+  defaults = {
+    plugins.mini-indentscope = {
+      enable = true;
+      settings = {
+        draw = {
+          delay = 100;
+          predicate = lib.nixvim.mkRaw "function (scope) return not scope.body.is_incomplete end";
+          priority = 2;
+        };
+
+        mappings = {
+          object_scope = "ii";
+          object_scope_with_border = "ai";
+          goto_top = "[i";
+          goto_bottom = "]i";
+        };
+
+        options = {
+          border = "both";
+          indent_at_cursor = true;
+          n_lines = 10000;
+          try_as_border = false;
+        };
+
+        symbol = "╎";
+      };
+    };
+  };
+}
