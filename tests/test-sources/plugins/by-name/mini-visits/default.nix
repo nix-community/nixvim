1eb80d497dfd9d2b2a4d1480664e90dc7b9756ae
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-11-02 02:35:32 -0300

    plugins/mini-visits: init

---
+++ tests/test-sources/plugins/by-name/mini-visits/default.nix
@@ -0,0 +1,31 @@
+{ lib, ... }:
+{
+  empty = {
+    plugins.mini-visits.enable = true;
+  };
+
+  defaults = {
+    plugins.mini-visits = {
+      enable = true;
+      settings = {
+        list = {
+          filter = lib.nixvim.mkRaw "nil";
+          sort = lib.nixvim.mkRaw "nil";
+        };
+
+        silent = false;
+
+        store = {
+          autowrite = true;
+          normalize = lib.nixvim.mkRaw "nil";
+          path = lib.nixvim.mkRaw "vim.fn.stdpath('data') .. '/mini-visits-index'";
+        };
+
+        track = {
+          event = "BufEnter";
+          delay = 1000;
+        };
+      };
+    };
+  };
+}
