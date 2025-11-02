b64735ffc6e88a3fb72db1e9b9954f7c89d769d7
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-11-02 02:03:49 -0300

    plugins/mini-operators: init

---
+++ tests/test-sources/plugins/by-name/mini-operators/default.nix
@@ -0,0 +1,38 @@
+{ lib, ... }:
+{
+  empty = {
+    plugins.mini-operators.enable = true;
+  };
+
+  defaults = {
+    plugins.mini-operators = {
+      enable = true;
+      settings = {
+        evaluate = {
+          prefix = "g=";
+          func = lib.nixvim.mkRaw "nil";
+        };
+
+        exchange = {
+          prefix = "gx";
+          reindent_linewise = true;
+        };
+
+        multiply = {
+          prefix = "gm";
+          func = lib.nixvim.mkRaw "nil";
+        };
+
+        replace = {
+          prefix = "gr";
+          reindent_linewise = true;
+        };
+
+        sort = {
+          prefix = "gs";
+          func = lib.nixvim.mkRaw "nil";
+        };
+      };
+    };
+  };
+}
