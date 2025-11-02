2d9f0214307cc6bafb7963227903cc5264837228
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-11-02 00:04:21 -0300

    plugins/mini-comment: init

---
+++ tests/test-sources/plugins/by-name/mini-comment/default.nix
@@ -0,0 +1,30 @@
+{ lib, ... }:
+{
+  empty = {
+    plugins.mini-comment.enable = true;
+  };
+
+  defaults = {
+    plugins.mini-comment = {
+      enable = true;
+      settings = {
+        options = {
+          custom_commentstring = lib.nixvim.mkRaw "nil";
+          ignore_blank_line = false;
+          start_of_line = false;
+          pad_comment_parts = true;
+        };
+        mappings = {
+          comment = "gc";
+          comment_line = "gcc";
+          comment_visual = "gc";
+          textobject = "gc";
+        };
+        hooks = {
+          pre = lib.nixvim.mkRaw "function() end";
+          post = lib.nixvim.mkRaw "function() end";
+        };
+      };
+    };
+  };
+}
