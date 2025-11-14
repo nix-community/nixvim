3832b56fb6ccd34c6ab8722a8e69de570bd181bd
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-11-14 15:12:08 -0300

    plugins/blink-indent: init

---
+++ tests/test-sources/plugins/by-name/blink-indent/default.nix
@@ -0,0 +1,76 @@
+{ lib, ... }:
+{
+  empty = {
+    plugins.blink-indent.enable = true;
+  };
+
+  defaults = {
+    plugins.blink-indent = {
+      enable = true;
+
+      settings = {
+        blocked = {
+          buftypes.include_defaults = true;
+          filetypes.include_defaults = true;
+        };
+
+        mappings = {
+          border = "both";
+          object_scope = "ii";
+          object_scope_with_border = "ai";
+          goto_top = "[i";
+          goto_bottom = "]i";
+        };
+
+        static = {
+          enabled = true;
+          char = "▎";
+          whitespace_char = lib.nixvim.mkRaw "nil";
+          priority = 1;
+          highlights = [ "BlinkIndent" ];
+        };
+
+        scope = {
+          enabled = true;
+          char = "▎";
+          priority = 1000;
+
+          highlights = [
+            "BlinkIndentOrange"
+            "BlinkIndentViolet"
+            "BlinkIndentBlue"
+          ];
+
+          underline = {
+            enabled = false;
+
+            highlights = [
+              "BlinkIndentOrangeUnderline"
+              "BlinkIndentVioletUnderline"
+              "BlinkIndentBlueUnderline"
+            ];
+          };
+        };
+      };
+    };
+  };
+
+  example = {
+    plugins.blink-indent = {
+      enable = true;
+
+      settings = {
+        static.highlights = [
+          "BlinkIndentRed"
+          "BlinkIndentOrange"
+          "BlinkIndentYellow"
+          "BlinkIndentGreen"
+          "BlinkIndentViolet"
+          "BlinkIndentCyan"
+        ];
+
+        scope.underline.enable = true;
+      };
+    };
+  };
+}
