d6e9b258dae41e319cb80dc5fae762d89af8cc19
Matt Sturgeon <matt@sturgeon.me.uk>
2025-11-24 09:31:31 +0000

    tests/extended-lib: omit `helpers`

--- tests/test-sources/extended-lib.nix
+++ tests/test-sources/extended-lib.nix
@@ -1,16 +1,11 @@
 let
   module =
-    { lib, helpers, ... }:
+    { lib, ... }:
     {
       assertions = [
         {
           assertion = lib ? nixvim;
           message = "lib.nixvim should be defined";
-        }
-        {
-          # NOTE: evaluating `helpers` here prints an eval warning
-          assertion = builtins.attrNames lib.nixvim == builtins.attrNames helpers;
-          message = "lib.nixvim and helpers should be aliases";
         }
       ];
     };
