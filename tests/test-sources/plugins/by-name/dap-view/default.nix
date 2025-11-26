7bce9d28eaa1db7c65ecbc2b932de2e001b4b505
Gaetan Lepage <gaetan@glepage.com>
2025-11-26 23:11:29 +0100

    tests/plugins/dap-view: remove deprecated settings.winbar.base_sections.*.action

--- tests/test-sources/plugins/by-name/dap-view/default.nix
+++ tests/test-sources/plugins/by-name/dap-view/default.nix
@@ -26,81 +26,41 @@
               keymap = "B";
               label = "Breakpoints [B]";
               short_label = " [B]";
-              action.__raw = ''
-                function()
-                    require("dap-view.views").switch_to_view("breakpoints")
-                end
-              '';
             };
             scopes = {
               keymap = "S";
               label = "Scopes [S]";
               short_label = "󰂥 [S]";
-              action.__raw = ''
-                function()
-                  require("dap-view.views").switch_to_view("scopes")
-                end
-              '';
             };
             exceptions = {
               keymap = "E";
               label = "Exceptions [E]";
               short_label = "󰢃 [E]";
-              action.__raw = ''
-                function()
-                    require("dap-view.views").switch_to_view("exceptions")
-                end
-              '';
             };
             watches = {
               keymap = "W";
               label = "Watches [W]";
               short_label = "󰛐 [W]";
-              action.__raw = ''
-                function()
-                  require("dap-view.views").switch_to_view("watches")
-                end
-              '';
             };
             threads = {
               keymap = "T";
               label = "Threads [T]";
               short_label = "󱉯 [T]";
-              action.__raw = ''
-                function()
-                  require("dap-view.views").switch_to_view("threads")
-                end
-              '';
             };
             repl = {
               keymap = "R";
               label = "REPL [R]";
               short_label = "󰯃 [R]";
-              action.__raw = ''
-                function()
-                  require("dap-view.repl").show()
-                end
-              '';
             };
             sessions = {
               keymap = "K";
               label = "Sessions [K]";
               short_label = " [K]";
-              action.__raw = ''
-                function()
-                  require("dap-view.views").switch_to_view("sessions")
-                end
-              '';
             };
             console = {
               keymap = "C";
               label = "Console [C]";
               short_label = "󰆍 [C]";
-              action.__raw = ''
-                function()
-                  require("dap-view.term").show()
-                end
-              '';
             };
           };
           custom_sections.__empty = { };
