814894ba73f95a73ce2827567a1263994af085a9
Gaetan Lepage <gaetan@glepage.com>
2025-12-13 19:39:22 +0100

    plugins/lsp/servers/omnisharp: drop settingsOptions

--- plugins/lsp/language-servers/default.nix
+++ plugins/lsp/language-servers/default.nix
@@ -45,53 +45,6 @@ let
     };
     omnisharp = {
       settings = cfg: { omnisharp = cfg; };
-      settingsOptions = {
-        enableEditorConfigSupport = lib.nixvim.defaultNullOpts.mkBool true ''
-          Enables support for reading code style, naming convention and analyzer settings from
-          `.editorconfig`.
-        '';
-
-        enableMsBuildLoadProjectsOnDemand = lib.nixvim.defaultNullOpts.mkBool false ''
-          If true, MSBuild project system will only load projects for files that were opened in the
-          editor.
-          This setting is useful for big C# codebases and allows for faster initialization of code
-          navigation features only for projects that are relevant to code that is being edited.
-          With this setting enabled OmniSharp may load fewer projects and may thus display
-          incomplete reference lists for symbols.
-        '';
-
-        enableRoslynAnalyzers = lib.nixvim.defaultNullOpts.mkBool false ''
-          If true, MSBuild project system will only load projects for files that were opened in the
-          editor.
-          This setting is useful for big C# codebases and allows for faster initialization of code
-          navigation features only for projects that are relevant to code that is being edited.
-          With this setting enabled OmniSharp may load fewer projects and may thus display
-          incomplete reference lists for symbols.
-        '';
-
-        organizeImportsOnFormat = lib.nixvim.defaultNullOpts.mkBool false ''
-          Specifies whether 'using' directives should be grouped and sorted during document
-          formatting.
-        '';
-
-        enableImportCompletion = lib.nixvim.defaultNullOpts.mkBool false ''
-          Enables support for showing unimported types and unimported extension methods in
-          completion lists.
-          When committed, the appropriate using directive will be added at the top of the current
-          file.
-          This option can have a negative impact on initial completion responsiveness, particularly
-          for the first few completion sessions after opening a solution.
-        '';
-
-        sdkIncludePrereleases = lib.nixvim.defaultNullOpts.mkBool true ''
-          Specifies whether to include preview versions of the .NET SDK when determining which
-          version to use for project loading.
-        '';
-
-        analyzeOpenDocumentsOnly = lib.nixvim.defaultNullOpts.mkBool true ''
-          Only run analyzers against open files when 'enableRoslynAnalyzers' is true.
-        '';
-      };
     };
     pylsp = {
       settings = cfg: { pylsp = cfg; };
