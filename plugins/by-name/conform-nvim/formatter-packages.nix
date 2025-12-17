ff68d318f7f8ed5e38250cf699f534d9f38c2d0e
Gaetan Lepage <gaetan@glepage.com>
2025-12-17 13:46:12 +0100

    plugins/conform-nvim/formatter-packages: mark oxfmt as unpackaged

--- plugins/by-name/conform-nvim/formatter-packages.nix
+++ plugins/by-name/conform-nvim/formatter-packages.nix
@@ -65,6 +65,7 @@ in
     mojo_format = states.unpackaged;
     nomad_fmt = states.unpackaged;
     npm-groovy-lint = states.unpackaged;
+    oxfmt = states.unpackaged;
     packer_fmt = states.unpackaged;
     pangu = states.unpackaged;
     perlimports = states.unpackaged;
