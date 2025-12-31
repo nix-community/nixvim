95af4dd0d1b6532cfa5e55688b5dcc67d1a74357
Gaetan Lepage <gaetan@glepage.com>
2025-12-31 14:04:45 +0100

    plugins/vscode-diff: init

---
+++ plugins/by-name/vscode-diff/default.nix
@@ -0,0 +1,29 @@
+{ lib, ... }:
+lib.nixvim.plugins.mkNeovimPlugin {
+  name = "vscode-diff";
+  package = "vscode-diff-nvim";
+
+  maintainers = [ lib.maintainers.GaetanLepage ];
+
+  settingsExample = {
+    highlights = {
+      line_insert = "#2a3325";
+      line_delete = "#362c2e";
+      char_insert = "#3d4f35";
+      char_delete = "#4d3538";
+    };
+    keymaps = {
+      view = {
+        next_hunk = "]c";
+        prev_hunk = "[c";
+        next_file = "]f";
+        prev_file = "[f";
+      };
+      explorer = {
+        select = "<CR>";
+        hover = "K";
+        refresh = "R";
+      };
+    };
+  };
+}
