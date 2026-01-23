e504e7f415eb75fe726c2419aecf91f857c1aa01
Austin Horstman <khaneliman12@gmail.com>
2026-01-23 09:41:06 -0600

    tests/plugins/indent-tools: skip tests
    Upstream needs to be updated to be compatible with latest
    nvim-treesitter-textobjects main branch.

--- tests/test-sources/plugins/by-name/indent-tools/default.nix
+++ tests/test-sources/plugins/by-name/indent-tools/default.nix
@@ -1,9 +1,15 @@
 {
   empty = {
+    # TODO: upstream needs update to support nvim-treesitter-textobjects namespace:
+    # local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
+    test.runNvim = false;
     plugins.indent-tools.enable = true;
   };
 
   defaults = {
+    # TODO: upstream needs update to support nvim-treesitter-textobjects namespace:
+    # local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
+    test.runNvim = false;
     plugins.indent-tools = {
       enable = true;
 
@@ -22,6 +28,9 @@
   };
 
   example = {
+    # TODO: upstream needs update to support nvim-treesitter-textobjects namespace:
+    # local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
+    test.runNvim = false;
     plugins.indent-tools = {
       enable = true;
 
