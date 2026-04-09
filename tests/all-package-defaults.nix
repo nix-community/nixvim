66af000867b62de0ba4468b48018680419bc7ae6
Austin Horstman <khaneliman12@gmail.com>
2026-04-09 11:28:34 -0500

    tests/all-package-defaults: disable fortitude on darwin

--- tests/all-package-defaults.nix
+++ tests/all-package-defaults.nix
@@ -75,6 +75,9 @@ let
     "rustaceanvim"
   ]
   ++ lib.optionals hostPlatform.isDarwin [
+    # 2026-04-09: build failure
+    "fortitude"
+
     # 2026-02-04 dependency llvmPackages_22.llvm is broken
     "ameba"
     "crystal"
