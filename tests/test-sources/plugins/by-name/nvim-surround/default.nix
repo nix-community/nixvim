61876f3936fa25d6520dbc95c4595e38c2d649a0
Austin Horstman <khaneliman12@gmail.com>
2026-04-09 11:17:20 -0500

    tests/nvim-surround: drop removed keymaps setting
    default> As of nvim-surround v4, keymaps are no longer set up using the
    setup function. Please see `:h nvim-surround.migrating.v3_to_v4`.

--- tests/test-sources/plugins/by-name/nvim-surround/default.nix
+++ tests/test-sources/plugins/by-name/nvim-surround/default.nix
@@ -8,20 +8,6 @@
       enable = true;
 
       settings = {
-        keymaps = {
-          insert = "<C-g>s";
-          insert_line = "<C-g>S";
-          normal = "ys";
-          normal_cur = "yss";
-          normal_line = "yS";
-          normal_cur_line = "ySS";
-          visual = "S";
-          visual_line = "gS";
-          delete = "ds";
-          change = "cs";
-          change_line = "cS";
-        };
-
         surrounds = {
           "(" = {
             add = [
