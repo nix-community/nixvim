1b4eef9c6b1d6d36ec6d046b9495d547ce7fa259
Austin Horstman <khaneliman12@gmail.com>
2026-04-10 08:19:42 -0500

    plugins/treesitter-textobjects: keymaps update
    Breaking changes recently introduced in all the nvim-treesitter rewrite.
    Just make it more known that the `settings` keymaps will have no effect
    in newer versions.

--- tests/test-sources/plugins/by-name/treesitter-textobjects/default.nix
+++ tests/test-sources/plugins/by-name/treesitter-textobjects/default.nix
@@ -17,15 +17,6 @@
             enable = true;
             disable.__empty = { };
             lookahead = true;
-            keymaps = {
-              af = "@function.outer";
-              "if" = "@function.inner";
-              ac = "@class.outer";
-              ic = {
-                query = "@class.inner";
-                desc = "Select inner part of a class region";
-              };
-            };
             selection_modes = {
               "@parameter.outer" = "v";
               "@function.outer" = "V";
@@ -36,47 +27,15 @@
           swap = {
             enable = true;
             disable.__empty = { };
-            swap_next = {
-              "<leader>a" = "@parameter.inner";
-            };
-            swap_previous = {
-              "<leader>A" = "@parameter.inner";
-            };
           };
           move = {
             enable = true;
             disable.__empty = { };
             set_jumps = true;
-            goto_next_start = {
-              "]m" = "@function.outer";
-              "]]" = "@class.outer";
-            };
-            goto_next_end = {
-              "]M" = "@function.outer";
-              "][" = "@class.outer";
-            };
-            goto_previous_start = {
-              "[m" = "@function.outer";
-              "[[" = "@class.outer";
-            };
-            goto_previous_end = {
-              "[M" = "@function.outer";
-              "[]" = "@class.outer";
-            };
-            goto_next = {
-              "]d" = "@conditional.outer";
-            };
-            goto_previous = {
-              "[d" = "@conditional.outer";
-            };
           };
           lsp_interop = {
             enable = true;
             border = "none";
-            peek_definition_code = {
-              "<leader>df" = "@function.outer";
-              "<leader>dF" = "@class.outer";
-            };
             floating_preview_opts.__empty = { };
           };
         };
