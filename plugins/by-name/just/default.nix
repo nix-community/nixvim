0075f9cbfd3315e8629c0a79860243f5bcd8e14a
Austin Horstman <khaneliman12@gmail.com>
2026-05-19 22:31:55 -0500

    plugins/just: add module
    Closes #4204

---
+++ plugins/by-name/just/default.nix
@@ -0,0 +1,57 @@
+{ lib, pkgs, ... }:
+lib.nixvim.plugins.mkNeovimPlugin {
+  name = "just";
+  package = "just-nvim";
+  description = "Lightweight task runner for `just` recipe files in Neovim.";
+
+  maintainers = [ lib.maintainers.khaneliman ];
+
+  dependencies = [
+    "just"
+  ];
+
+  settingsExample = {
+    fidget_message_limit = 64;
+    play_sound = true;
+    open_qf_on_run = false;
+    telescope_borders = {
+      prompt = [
+        " "
+        " "
+        " "
+        " "
+        " "
+        " "
+        " "
+        " "
+      ];
+      results = [
+        " "
+        " "
+        " "
+        " "
+        " "
+        " "
+        " "
+        " "
+      ];
+      preview = [
+        " "
+        " "
+        " "
+        " "
+        " "
+        " "
+        " "
+        " "
+      ];
+    };
+  };
+
+  extraPlugins = [
+    pkgs.vimPlugins."plenary-nvim"
+    pkgs.vimPlugins."telescope-nvim"
+    pkgs.vimPlugins."fidget-nvim"
+    pkgs.vimPlugins."nvim-notify"
+  ];
+}
