c3a6528ec73e4c46b7adc577c1d0a0038d3806cd
Austin Horstman <khaneliman12@gmail.com>
2026-05-19 22:31:55 -0500

    plugins/vectorcode: add module
    Closes #3326

---
+++ plugins/by-name/vectorcode/default.nix
@@ -0,0 +1,17 @@
+{ lib, pkgs, ... }:
+lib.nixvim.plugins.mkNeovimPlugin {
+  name = "vectorcode";
+  package = "vectorcode-nvim";
+  description = "Code-aware local retrieval and completion integration for Neovim.";
+  callSetup = "optional";
+
+  maintainers = [ lib.maintainers.khaneliman ];
+
+  settingsExample = {
+    async_backend = "lsp";
+    n_query = 5;
+    sync_log_env_var = true;
+  };
+
+  extraPackages = [ pkgs.vectorcode ];
+}
