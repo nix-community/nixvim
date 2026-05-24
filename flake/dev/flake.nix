860304dd630b69bce65b08bc38aeefd01ccb6a70
Matt Sturgeon <matt@sturgeon.me.uk>
2026-05-24 16:23:21 +0100

    flake/dev: drop dev-nixpkgs for nixvim path ref
    flake/dev/flake.lock updates:
    • Removed input 'dev-nixpkgs'
    • Updated input 'devshell/nixpkgs':
        follows 'dev-nixpkgs'
      → follows 'nixvim/nixpkgs'
    • Updated input 'git-hooks/nixpkgs':
        follows 'dev-nixpkgs'
      → follows 'nixvim/nixpkgs'
    • Updated input 'home-manager/nixpkgs':
        follows 'dev-nixpkgs'
      → follows 'nixvim/nixpkgs'
    • Updated input 'nix-darwin/nixpkgs':
        follows 'dev-nixpkgs'
      → follows 'nixvim/nixpkgs'
    • Added input 'nixvim':
        'path:../..'
    • Added input 'nixvim/flake-parts':
        'github:hercules-ci/flake-parts/f7c1a2d347e4c52d5fb8d10cb4d94b5884e546fb?narHash=sha256-m1Yf0wZ8j1OHjTc2UwHwyQRSnNeSgLJOd7q5Y45hzi4%3D' (2026-05-13)
    • Added input 'nixvim/flake-parts/nixpkgs-lib':
        follows 'nixvim/nixpkgs'
    • Added input 'nixvim/nixpkgs':
        'github:NixOS/nixpkgs/d99b013d5d1931ad77fe3912ed218170dec5d9a4?narHash=sha256-7DKWmH23hL2eYdkxCKeqj2i%2ByljTKuU%2B3Nk1UPHOnxc%3D' (2026-05-20)
    • Added input 'nixvim/systems':
        'github:nix-systems/default/da67096a3b9bf56a91d16901293e51ba5b49a27e?narHash=sha256-Vy1rq5AaRuLzOxct8nz4T6wlgyUR7zLU309k9mBC768%3D' (2023-04-09)
    • Updated input 'nuschtosSearch/nixpkgs':
        follows 'dev-nixpkgs'
      → follows 'nixvim/nixpkgs'
    • Updated input 'treefmt-nix/nixpkgs':
        follows 'dev-nixpkgs'
      → follows 'nixvim/nixpkgs'

--- flake/dev/flake.nix
+++ flake/dev/flake.nix
@@ -2,10 +2,10 @@
   description = "Private inputs for development purposes. These are used by the top level flake in the `dev` partition, but do not appear in consumers' lock files.";
 
   inputs = {
-    # NOTE: Use a different name to the root flake's inputs.nixpkgs to avoid shadowing it.
-    # NOTE: The only reason we specify a nixpkgs input at all here, is so the other inputs can follow it.
-    # TODO: Once nix 2.26 is more prevalent, follow the root flake's inputs using a "path:../.." input.
-    dev-nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # Nix 2.26 improved support for relative path flake inputs.
+    # This dev-flake is only evaluated by flake-parts' flake-compat,
+    # and is only managed by our update CI, so we can safely rely on it.
+    nixvim.url = ../..;
 
     # flake-compat is used by the root `default.nix` to allow non-flake users to import nixvim
     flake-compat.url = "github:NixOS/flake-compat";
@@ -13,34 +13,34 @@
     # keep-sorted start block=yes newline_separated=yes
     devshell = {
       url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "dev-nixpkgs";
+      inputs.nixpkgs.follows = "nixvim/nixpkgs";
     };
 
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "dev-nixpkgs";
+      inputs.nixpkgs.follows = "nixvim/nixpkgs";
       inputs.flake-compat.follows = "flake-compat";
     };
 
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "dev-nixpkgs";
+      inputs.nixpkgs.follows = "nixvim/nixpkgs";
     };
 
     nix-darwin = {
       url = "github:lnl7/nix-darwin";
-      inputs.nixpkgs.follows = "dev-nixpkgs";
+      inputs.nixpkgs.follows = "nixvim/nixpkgs";
     };
 
     nuschtosSearch = {
       # TODO: newer versions fail with: TimeoutError: The operation was aborted due to timeout
       url = "github:NuschtOS/search/b6f77b88e9009bfde28e2130e218e5123dc66796";
-      inputs.nixpkgs.follows = "dev-nixpkgs";
+      inputs.nixpkgs.follows = "nixvim/nixpkgs";
     };
 
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "dev-nixpkgs";
+      inputs.nixpkgs.follows = "nixvim/nixpkgs";
     };
 
     # keep-sorted end
