047bffb020ac73ee90ce45dc0154bd6caa9bfc46
Matt Sturgeon <matt@sturgeon.me.uk>
2026-06-15 19:09:07 +0100

    plugins/coq-nvim: remove `settings.auto_start` option
    It's removed & deprecated in upstream's v2 plugin.

--- plugins/by-name/coq-nvim/deprecation.nix
+++ plugins/by-name/coq-nvim/deprecation.nix
@@ -15,5 +15,10 @@ in
       assertion = !(cfg.settings ? xdg);
       message = "xdg has been removed in v2. The field is ignored — delete it from `${opts.settings}`.";
     }
+    # TODO: Added 2026-06-15; remove after 27.11
+    {
+      assertion = !(cfg.settings ? auto_start);
+      message = "auto_start has been removed in v2. The field is ignored — delete it from `${opts.settings}`.";
+    }
   ];
 }
