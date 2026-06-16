d43c763fd9fae0912bdb4103cd842f26fea5b0ed
Austin Horstman <khaneliman12@gmail.com>
2026-06-16 11:56:41 -0500

    plugins/efmls-configs: map available packagings
    Move newly available efmls formatters and linters from unpackaged to
    explicit package mappings.

--- plugins/by-name/efmls-configs/packages.nix
+++ plugins/by-name/efmls-configs/packages.nix
@@ -5,21 +5,15 @@ in
 {
   # efmls-configs tools that have no corresponding nixpkgs package
   unpackaged = [
-    "alex" # was removed from nixpkgs as it is unmaintained
-    "blade_formatter"
-    "cljstyle"
-    "cspell"
+    # keep-sorted start block=yes newline_separated=no
     "dartanalyzer"
     "debride"
     "deno_fmt"
     "fecs"
-    "fixjson"
     "forge_fmt"
-    "gersemi"
     "gleam_format"
     "js_standard"
     "jsonlint"
-    "kdlfmt"
     "markuplint"
     "mix"
     "pint"
@@ -32,16 +26,16 @@ in
     "slim_lint"
     "solhint"
     "sorbet"
-    "swiftformat"
-    "swiftlint"
     "typstfmt"
     "xo"
+    # keep-sorted end
   ];
 
   # Mapping from a efmls-configs tool name to the corresponding nixpkgs package
   packaged =
     # Top-level packages
     topLevel [
+      # keep-sorted start block=yes newline_separated=no
       "actionlint"
       "alejandra"
       "ameba"
@@ -109,53 +103,64 @@ in
       "vale"
       "yamllint"
       "yapf"
+      # keep-sorted end
     ]
     # Scoped packages
     // scoped {
+      # keep-sorted start block=yes newline_separated=no
+      haskellPackages = [
+        "fourmolu"
+      ];
+      luaPackages = [
+        "luacheck"
+      ];
+      phpPackages = [
+        "phan"
+        "psalm"
+      ];
       python3.pkgs = [
         "autopep8"
         "flake8"
         "mdformat"
         "vulture"
       ];
-      phpPackages = [
-        "phan"
-        "psalm"
-      ];
-      luaPackages = [
-        "luacheck"
-      ];
-      haskellPackages = [
-        "fourmolu"
-      ];
+      # keep-sorted end
     }
     # Packages where the name is different
     // {
+      # keep-sorted start block=yes newline_separated=no
+      alex = "alex";
       ansible_lint = "ansible-lint";
+      blade_formatter = "blade-formatter";
       chktex = "texliveMedium";
       clang_format = "clang-tools";
       clang_tidy = "clang-tools";
       clj_kondo = "clj-kondo";
+      cljstyle = "cljstyle";
       cmake_lint = "cmake-format";
+      cspell = "cspell";
       dartfmt = "dart";
       dotnet_format = "dotnet-runtime";
       fish_indent = "fish";
+      fixjson = "fixjson";
+      gersemi = "gersemi";
+      go_revive = "revive";
       gofmt = "go";
       goimports = "go-tools";
       golangci_lint = "golangci-lint";
       google_java_format = "google-java-format";
-      go_revive = "revive";
+      kdlfmt = "kdlfmt";
       latexindent = "texliveMedium";
       lua_format = "luaformatter";
       markdownlint = "markdownlint-cli";
       mcs = "mono";
-      phpcbf = [
-        "phpPackages"
-        "php-codesniffer"
-      ];
       php_cs_fixer = [
         "phpPackages"
         "php-cs-fixer"
+      ];
+      phpcbf = [
+        "phpPackages"
+        "php-codesniffer"
       ];
       phpcs = [
         "phpPackages"
@@ -164,10 +169,13 @@ in
       prettier_d = "prettierd";
       slither = "slither-analyzer";
       staticcheck = "go-tools";
+      swiftformat = "swiftformat";
+      swiftlint = "swiftlint";
       terraform_fmt = "terraform";
       vint = "vim-vint";
       write_good = "write-good";
       yq = "yq-go";
       zlint = "zig-zlint";
+      # keep-sorted end
     };
 }
