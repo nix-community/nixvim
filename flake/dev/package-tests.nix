2718c1099ba2390c5f5d113dcce31d8bf16420ea
Matt Sturgeon <matt@sturgeon.me.uk>
2026-07-01 02:41:16 +0100

    ci: buildbot-nix → nixbot
    Migrate flake outputs, config files, etc from buildbot-nix to nixbot.
    
    This is not urgent, because nixbot falls back to buildbot-nix.toml when
    nixbot.toml is missing.

--- flake/dev/package-tests.nix
+++ flake/dev/package-tests.nix
@@ -6,11 +6,11 @@
       # Test that all packages build fine when running `nix flake check`.
       checks = config.packages;
 
-      # Test that all packages build when running buildbot
+      # Test that all packages build when running nixbot
       # FIXME: we want to build most platforms on all systems,
       # but building the docs is often too expensive.
       # For now, we restrict the whole packages output to one platform.
       # TODO: move building the docs to GHA
-      ci.buildbot = lib.mkIf (system == "x86_64-linux") config.packages;
+      ci.nixbot = lib.mkIf (system == "x86_64-linux") config.packages;
     };
 }
