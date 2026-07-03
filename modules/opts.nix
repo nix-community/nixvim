92af88fb005497de5b5b94f119dcd67585c8a440
belamu <26257151+belamu@users.noreply.github.com>
2026-07-03 21:26:54 +0200

    modules/opts: docs on difference between opts, global opts and local opts
    These differences are clear for users that are well-versed in neovim
    configuration but need a while to figure out for neovim newcomers.
    
    Therefore I hope that they can help.

--- modules/opts.nix
+++ modules/opts.nix
@@ -5,21 +5,42 @@ let
       prettyName = "options";
       luaVariableName = "options";
       luaApi = "opt";
-      description = "The configuration options, e.g. line numbers (`vim.opt.*`)";
+      description = ''
+        The configuration options, e.g. line numbers (`vim.opt.*`)
+
+        These options are set as global and local.
+
+        See [:h option-list](https://neovim.io/doc/user/quickref/#option-list) for all available options.
+      '';
     };
 
     globalOpts = {
       prettyName = "global options";
       luaVariableName = "global_options";
       luaApi = "opt_global";
-      description = "The configuration global options (`vim.opt_global.*`)";
+      description = ''
+        The configuration global options (`vim.opt_global.*`)
+
+        These options are overridden for buffers that have the same option set as a local option.
+        If a configuration set in `globalOpts` seems to have no effect, check with `:setglobal <option>?`, `:setlocal <option>?`
+        and move the option declaration into `opts`.
+
+        See [:h option-list](https://neovim.io/doc/user/quickref/#option-list) for all available options.
+      '';
     };
 
     localOpts = {
       prettyName = "local options";
       luaVariableName = "local_options";
       luaApi = "opt_local";
-      description = "The configuration local options (`vim.opt_local.*`)";
+      description = ''
+        The configuration local options (`vim.opt_local.*`)
+
+        These options are applied to the buffer on startup of neovim with the global options (`opt_global`) as a fallback.
+        When opening a new buffer, e.g. with `:new`, the global options are used, making `localOpts` irrelevant for the new buffer.
+
+        See [:h option-list](https://neovim.io/doc/user/quickref/#option-list) for all available options.
+      '';
     };
 
     globals = {
