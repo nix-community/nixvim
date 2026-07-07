5410981588e63c82f603e6f7a623706da64c8955
Guelakais <koroyeldiores@gmail.com>
2026-07-07 16:53:49 +0200

    Update modules/ui_two.nix
    Co-authored-by: Matt Sturgeon <matt@sturgeon.me.uk>

--- modules/ui_two.nix
+++ modules/ui_two.nix
@@ -9,7 +9,7 @@ in
 {
   options.ui2.enable = lib.mkEnableOption "Neovim's [experimental UI2](https://neovim.io/doc/user/lua/#ui2)";
 
-  config = lib.mkIf (config.ui_two.enable) {
+  config = lib.mkIf cfg.enable {
     extraConfigLua = ''
       require('vim._core.ui2').enable(${lib.nixvim.toLuaObject cfg.settings})
     '';
