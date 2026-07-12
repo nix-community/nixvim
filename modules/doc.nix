56b509b769e5cd6a20728c43573bb8563057a951
Matt Sturgeon <matt@sturgeon.me.uk>
2026-07-12 12:57:12 +0100

    modules/doc: disable man-docs on darwin
    We cannot build docs on Darwin because pandoc is currently built without
    lua support: https://github.com/NixOS/nixpkgs/issues/540900

--- modules/doc.nix
+++ modules/doc.nix
@@ -1,10 +1,17 @@
-{ lib, options, ... }:
+{
+  lib,
+  pkgs,
+  options,
+  ...
+}:
 {
   options.enableMan = lib.mkOption {
     type = lib.types.bool;
     # TODO: make `build.manDocsPackage` available in non-flake evals
-    default = options.flake.isDefined;
-    defaultText = lib.literalExpression "options.${options.flake}.isDefined";
+    # TODO: 2026-07-12 cannot build docs on darwin due to pandoc missing lua support:
+    #       https://github.com/NixOS/nixpkgs/issues/540900
+    default = options.flake.isDefined && !pkgs.stdenv.hostPlatform.isDarwin;
+    defaultText = lib.literalExpression "options.${options.flake}.isDefined && !isDarwin";
     description = "Install the man pages for Nixvim options.";
   };
 }
