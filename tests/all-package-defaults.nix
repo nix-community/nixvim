7404e74b7f49b9db8d3ce0c766a02407c9ce3669
Matt Sturgeon <matt@sturgeon.me.uk>
2026-07-12 13:44:47 +0100

    tests/all-package-defaults: disable godot on darwin

--- tests/all-package-defaults.nix
+++ tests/all-package-defaults.nix
@@ -104,6 +104,9 @@ let
     "rustaceanvim"
   ]
   ++ lib.optionals hostPlatform.isDarwin [
+    # 2026-07-12 build failure
+    "godot"
+
     # 2026-07-12 transitive build failure: qtspeech
     "sioyek"
 
